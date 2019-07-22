444a276b14ecaf72d4b04affafad51a6ef7dd368
sergio rivera <telesforociriaco@gmail.com>
2019-07-22 18:18:49 -0500

    practica try catch

---
+++ jul_22/Sergio-practica-try-catch.js
@@ -0,0 +1,45 @@
+//Agregando a un nuevo array elementos de otro pero sin conocer su contenido
+//arry = [0,,4,"choto",7];
+//hola = [];
+//    try {
+//        for (let i = 0; i < arry.length; i++) {
+//        if(arry[i]){
+//            throw "alto ahi indefinido;"
+//        }
+//            hola[i] = arry[i];
+//        }
+//    } catch (error) {
+//        console.log(error);
+//        return error;
+//        
+//    }
+//
+//console.log(hola);
+
+
+function JhonaSeLa(toda){
+            let frase = "";
+            let i = 0;
+            let terminado = true;
+            while ( terminado ){
+                
+                try {
+                    if(typeof toda[i] != String){
+                    throw "basura"
+                    
+                }
+                    frase += toda[i]+" ";
+                } catch (error) {
+                    console.log("esta resibiendo infinito");
+                    
+                }
+                
+                if(i == toda.length){
+                    terminado = false;
+                }
+                i++;
+            }
+            return frase;
+        }
+
+console.log(JhonaSeLa(["el","Jhona","sela","come",7]));
