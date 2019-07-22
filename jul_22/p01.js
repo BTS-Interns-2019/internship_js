ed81afd907090d4d369ebb27445b32099dbf0c21
Miguel <miguelv.bts@gmail.com>
2019-07-22 18:17:38 -0500

    Aqui estan dos de los ejercicios, todavia no estan terminados, asi que recibiran modificaciones

---
+++ jul_22/p01.js
@@ -0,0 +1,17 @@
+console.log("BIENVENIDO");
+var opc = t;
+try{
+    switch(opc){
+        case 1://Saludo
+            console.log("Hola");
+            break;
+        case 2://Despedida
+            console.log("Adios");
+            break;
+    }
+}catch(ReferenceError){
+    console.error(e);
+}
+
+
+//for while return finally typeof
