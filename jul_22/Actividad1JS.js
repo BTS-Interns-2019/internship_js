2bae579a638ffdac2e463817a8c97104e06e209f
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-07-22 20:38:53 -0500

    Actividad uno completa

--- jul_22/Actividad1JS.js
+++ jul_22/Actividad1JS.js
@@ -16,11 +16,16 @@ function printArray(array) {
     }
 }
 
-console.log(printArray([25,56,78]));
+console.log(printArray());
 
 /////
 function question(respuesta) {
-    console.log("2+2= 4, Si o No");
+    console.log(respuesta);
+
+    if (typeof(respuesta) !="string"){
+        throw new Error({message:"Escribe solo un parametro"});
+    }else{
+        console.log("2+2= 4, Si o No");
 
         switch (respuesta) {
             case "Si":
@@ -34,10 +39,34 @@ function question(respuesta) {
                 console.log("Esas opciones no estan hijo!!");
                 break;
         }
+    }
 }
 
 try {
-    console.log(question(si,no));
+    console.log(question(25));
 } catch (error) {
-    console.log("Solo un parametro por favor")
+    console.log("Solo texto por favor")
 }
+
+
+function testWhile(number) {
+    console.log(typeof(number));
+    if (typeof(number)!='number'){
+        console.log("Solo numeros");
+    }else{
+        while (number<10) {
+            number++;
+        console.log(number);
+       
+        }
+    }
+}
+
+try {
+    console.log(testWhile(defaultStatus));
+} catch (error) {
+    console.log(error+"\nLa variable no esta definida");
+}
+
+
+
