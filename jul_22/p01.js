c177d3ac964534a5f557bb6116c9d7571934dd5c
Miguel <miguelv.bts@gmail.com>
2019-07-22 19:53:11 -0500

    Programa 1 modificado, solucionado el error no estar instanciado el opc

--- jul_22/p01.js
+++ jul_22/p01.js
@@ -1,17 +1,20 @@
-console.log("BIENVENIDO");
-var opc = t;
-try{
-    switch(opc){
-        case 1://Saludo
-            console.log("Hola");
-            break;
-        case 2://Despedida
-            console.log("Adios");
-            break;
+try{//Lanza instrucciones para evitar errores
+    console.log("BIENVENIDO");//Mensaje de bienvenida
+    var opc = 2;//Variable de opciones del switch
+    if(opc === undefined){//Condicion que evita que marque un error al faltar opc
+        throw e;
     }
-}catch(ReferenceError){
+    switch(opc){//Si el valor de opc es
+        case 1://1 es Saludo
+            console.log("Hola");//Mensaje
+            break;
+        case 2://2 es Despedida
+            console.log("Adios");//Mensaje
+            break;
+        default://Caso donde no sea ni uno ni dos
+            console.log("Solo elige uno o dos");//Mensaje
+    }
+}catch(e){//Captura errores del codigo
     console.error(e);
 }
 
-
-//for while return finally typeof
