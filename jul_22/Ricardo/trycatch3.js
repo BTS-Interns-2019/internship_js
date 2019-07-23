592295b1f52e9bb4e292ab098ca45b87584be7fe
Ricardo Martín del campo Loza <ricer.loza@gmail.com>
2019-07-22 22:10:48 -0500

    Commit con el trabajo de clase terminado

--- jul_22/Ricardo/trycatch3.js
+++ jul_22/Ricardo/trycatch3.js
@@ -1,3 +1,13 @@
-function funcion (a) {
-    
+function calcularIVA (a) {
+if(typeof a == "number"){
+    return "Gracias por ingresar un número";
+}else{
+    throw new Error("Tienes que poner un numero")
 }
+}
+try{
+    console.log(calcularIVA("A"));
+}
+ catch(e){
+     console.error(e.message)
+ }
