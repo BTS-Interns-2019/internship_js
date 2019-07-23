068d6ecbe625b7311ca8b11579ee896e10a1638f
Viridiana <vdlre1994@gmail.com>
2019-07-22 23:48:15 -0500

    Ejercicio en clase modificado y terminado

--- jul_22/Ejercicio1.js
+++ jul_22/Ejercicio1.js
@@ -1,36 +1,76 @@
-let myVariable = prompt('Give me a number');
+/*Escribir 3 ejemplos usando try...catch que incluyan 
+ switch
+ for
+ while-
+ return-
+ finally-
+ typeof-
+*/
 
-try {
-  if (isNaN(+myVariable)) {
-    throw new TypeError('Not a number!');
-  }
-  console.log('Good choice', myVariable);
-} catch (e) {
-  if (e.name === 'TypeError') {
-    console.log(e);
-  } else {
-    throw e;
-  }
+//Ejemplo 1 con try.. catch typeof y return
+function Numeros(a, b) {
 
-
-
-
-
-(function() {
   try {
-    try {
-      throw new Error('oops');
+    
+    if (typeof a !== 'number') {
+      throw TypeError('El primer argumento no es un numero');
     }
-    catch (ex) {
-      console.error('inner', ex.message);
-      throw ex;
+
+    if (typeof b !== 'number') {
+      throw TypeError('El segundo argumento no es un numero');
     }
-    finally {
-      console.log('finally');
-      return;
-    }
+
+    return a + b;
+  } catch (error) {
+    return error.message;
   }
-  catch (ex) {
-    console.error('outer', ex.message);
+}
+
+Numeros('string', 1);
+
+
+
+//Try ... catch  while finally
+
+function ejemplo(n) {
+ i = 1; 
+  while (n < 5) {
+    n++;
+    if (n == 3) {
+      continue;
+    }
+    i += n;
+    return i;
   }
-})();
+} try {
+  ejemplo(3)
+  console.log(i)
+}
+catch (e) {
+  console.log(e)
+} finally {
+  console.log("Se realizo correctamente")
+}
+
+//try....catch switch
+
+try{
+  var comida = 'Pan';
+  switch (comida) {
+    case 'Pan':
+    case 'Leche':
+    case 'Cafe':
+    case 'Lechuga':
+      console.log('Está comida es saludable');
+      break;
+    default:
+      throw(console.log(''));
+  }
+  
+  }catch(e){
+    console.log('Eso no existe:'+ e);
+  
+  }
+  
+
+
