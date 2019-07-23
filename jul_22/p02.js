9e41edf5e3dc3baab6aac2da0e3e9e247c62116d
Miguel <miguelv.bts@gmail.com>
2019-07-22 19:38:22 -0500

    Programa 2 modificado, adicion de comentarios y arreglados algunos errores

--- jul_22/p02.js
+++ jul_22/p02.js
@@ -1,16 +1,17 @@
-function sumar(a,b){
+function sumar(a,b){//Funcion
     return a+b;
 }
-try{
-    var primero=1;
-    var segundo='t';
-    for(segundo;segundo<ciclo;segundo++){
-        if(typeof primero === 'number'){
-            if(typeof segundo === 'number'){
-                console.log(sumar(primero,segundo));
+try{//Lanza instrucciones para evitar errores
+    var primero=1;//Numero
+    var segundo=3;//Numero
+    var ciclo=10;//Numero de iteraciones
+    for(segundo;segundo<ciclo;segundo++){//Ciclo for
+        if(typeof primero === 'number'){//Condicion
+            if(typeof segundo === 'number'){//Condicion
+                console.log(sumar(primero,segundo));//Mensaje con funcion
             }
         }
     }
-}catch(e){
+}catch(e){//Captura errores del codigo
     console.error(e);
 }
