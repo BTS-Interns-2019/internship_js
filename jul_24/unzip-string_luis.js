b1d35e2a4d0bc3638132881b4533a0568c797c77
Luis G <luicarlosg.bts@gmail.com>
2019-07-24 13:47:16 -0700

    zip by luis

---
+++ jul_24/unzip-string_luis.js
@@ -0,0 +1,20 @@
+function unzipString(zip){
+    let unzip="";
+    let n = 1;
+    for(let car of zip){
+        if(car>=0&&car<=9){
+            n = car;
+        }else{
+            unzip+= car.repeat(n);
+        }
+    }
+    return unzip;
+}
+
+console.log(unzipString('a2bc'));
+console.log(unzipString('3D2a5d2f'));
+console.log(unzipString('3d332f2a'));
+console.log(unzipString('abcd'));
+console.log(unzipString(''));
+
+module.exports = unzipString;
