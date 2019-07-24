ca8571f2a709dd48c3cd424757dc6955c4e887d1
silva-david <silvad.bts@gmail.com>
2019-07-24 17:42:51 -0500

    david expenses-parser exercise

---
+++ jul_24/expenses-parser_david.js
@@ -0,0 +1,58 @@
+//const input = "1000.00 127 Video 7.45 128 Gasoline 16.10";
+//console.log(expensesParser(input));
+
+function expensesParser(input) {
+    array = input.split(' ');
+    if (array.length == 0) {
+
+    } else {
+        n = (array.length-1)/3;
+
+        var output = new Object();
+        output.initialBalance = array[0];
+        output.expenses = [];
+
+        let tE = 0;
+        for (i = 3; i < array.length; i+=3) {
+            tE += Number(array[i]);
+        }
+        output.totalExpense = tE;
+        output.averageExpense = tE/n;
+        output.finalBalance = array[0]-tE;
+
+        k = 1;
+        iB = Number(array[0]);
+        for (i = 0; i < n; i++) {
+            output.expenses[i] = {};
+            for (j =1; j < 5; j++) {
+                switch (j) {
+                    case 1:
+                        output.expenses[i].id = array[k]; 
+                        k++;
+                        break;
+
+                    case 2:
+                        output.expenses[i].description = array[k];
+                        k++;
+                        break;
+
+                    case 3:
+                        output.expenses[i].cost = Number(array[k]);
+                        k++;
+                        break;
+
+                    case 4:
+                        iB = iB - Number(array[k-1]);
+                        output.expenses[i].balance = Number(iB.toFixed(2));
+                        break;
+                }        
+            }
+        }
+    }
+    return output
+}
+
+module.exports = expensesParser;
+
+
+
---
+++ jul_24/unzip-string_david.js
@@ -0,0 +1,30 @@
+zip = 'a2bc';
+// function unzipString(zip) {
+    
+//     return unzip
+// }
+
+array = zip.split('');
+if (array.length == 0) {
+    unzip = "''";
+} else {
+    unzip = '';
+    for (i = 0; i < array.length; i++) {
+        if (Number(array[i])) {
+            for (j = i+1; j < array.length; j++) {
+                if (Number(array[j])) {
+                    break;
+                } else {
+                    n = Number(array[i]);
+                    unzip += array[j].repeat(n)
+                }            
+            }
+        } else {
+
+            unzip += array[i]        
+        }
+        console.log(unzip);
+    }
+}
+
+console.log(unzip);
