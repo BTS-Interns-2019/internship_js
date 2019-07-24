088e5ff83638949715d6280c2fa19717ca0b986f
cristianliljon <52710484+cristianliljon@users.noreply.github.com>
2019-07-24 08:25:01 -0500

    Update Segundo.js

--- jul_22/Segundo.js
+++ jul_22/Segundo.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 //Codigo que funciona solo en chrome para tenr interaccion con el usuario
-=======
->>>>>>> 82104f29dc46c8c5bf63256cf7f6b8c8c7c999bf
 try{
     var nombre = prompt("Dame tu nombre");
     alert("Hola " + nombre);
@@ -13,7 +10,6 @@ try{
         switch(t)
         {
             case "1":
-<<<<<<< HEAD
                 var v = parseInt( prompt("Ingresa tu edad") );
                 if(!isNaN(v)){
                     alert("Edad " + v + " añadida");
@@ -21,11 +17,6 @@ try{
                 }else{
                     throw "Caracter invalido";
                 }
-=======
-                var v = prompt("Ingresa tu edad");
-                alert("Edad añadida");
-                edad = v;    
->>>>>>> 82104f29dc46c8c5bf63256cf7f6b8c8c7c999bf
                 break;
             case "2":
                 var n = prompt("Nuevo nombre");
@@ -39,17 +30,10 @@ try{
                     throw new error("Ingresarte un caracter invalido");
                 } break;
             case "3":
-<<<<<<< HEAD
                 var m = parseInt( prompt("Dame un nuero para mostrar su tabla de multiplicar") );
                 if(!isNaN(m))
                 {
                     var e ="";
-=======
-                var m = prompt("Dame un nuero para mostrar su tabla de multiplicar");
-                if(typeof m == "number")
-                {
-                    var e;
->>>>>>> 82104f29dc46c8c5bf63256cf7f6b8c8c7c999bf
                     for(var i=1;i<=10;i++)
                     {
                         e += m + " * " + i + " = " + (m*i) + "\n"
@@ -65,11 +49,7 @@ try{
     }while(t != 4)
     
 }catch(e){
-<<<<<<< HEAD
     alert(e);
-=======
-    alert(e.message);
->>>>>>> 82104f29dc46c8c5bf63256cf7f6b8c8c7c999bf
 }
 finally{
     alert("Gracias por su visita");
