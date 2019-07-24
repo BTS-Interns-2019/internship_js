b3e6774e62bd62a0f8eb372ae5c536fdccfc246d
cristianliljon <52710484+cristianliljon@users.noreply.github.com>
2019-07-24 10:56:22 -0500

    Update homework_cristianp.js
    Ejercicio terminado

--- jul_23/homework_cristianp.js
+++ jul_23/homework_cristianp.js
@@ -47,27 +47,98 @@ let matrix = [
     [1,2,3,4,5,6,7,8,9] ];
 
 var i=0;
-var j=0;
 while(i<9)
 {
+    var j=0;
     //console.log(matrix[i][j]);
     while(j<9)
     {
-        console.log(matrix[i][j]);
-        if(matrix[i][j] != i)
+        if(matrix[i][j] != i+1)
         {
             matrix[i][j] = 0;
         }
         j++;
     }
-    j=0;
     i++;
 }
-console.log(matrix);
 
+for(var i=0; i<9;i++)
+{
+    for(var j = 0; j<9;j++)
+    {
+        console.log(matrix[i][j]);
+    }
+}
 /*for(var l=0;l<=9;l++){
-            if(matrix[i][j] != i)
-            {
-                matrix[i][j] = 0;
-            }
-        }*/
+    if(matrix[i][j] != i)
+    {
+        matrix[i][j] = 0;
+    }
+}*/
+
+console.log(15/0);
+console.log(50/0);
+console.log(6/0);
+
+
+//
+var matriz = [
+    [1,0,0,0,0,0,0,0,0],
+    [0,2,0,0,0,0,0,0,0],
+    [0,0,3,0,0,0,0,0,0],
+    [0,0,0,4,0,0,0,0,0],
+    [0,0,0,0,5,0,0,0,0],
+    [0,0,0,0,0,6,0,0,0],
+    [0,0,0,0,0,0,7,0,0],
+    [0,0,0,0,0,0,0,8,0],
+    [0,0,0,0,0,0,0,0,9]
+];
+var i=0;
+while(i<9)
+{
+    var j=0;
+    //console.log(matrix[i][j]);
+    while(j<9)
+    {
+        if(matrix[i][j] == i+1)
+        {
+            delete matriz[i][j];
+        }
+        j++;
+    }
+    i++;
+}
+
+for(var i=0; i<9;i++)
+{
+    for(var j = 0; j<9;j++)
+    {
+        //console.log(matriz[i][j]);
+        if(i==j)
+        {
+            console.log(matriz[i][j] in matriz);
+        }
+    }
+}
+
+/*
+console.log(matriz[0][0] in matriz);
+console.log(matriz[1][1] in matriz);
+console.log(matriz[2][2] in matriz);
+console.log(matriz[3][3] in matriz);
+console.log(matriz[4][4] in matriz);
+console.log(matriz[5][5] in matriz);
+console.log(matriz[6][6] in matriz);
+console.log(matriz[7][7] in matriz);
+console.log(matriz[8][8] in matriz);
+//*/
+
+//
+for(var a = 0; a<=100;a++)
+{
+    ostia = a;
+    if((ostia%5)>=2 && (ostia%5)<4)
+    {
+        console.log(a);
+    }
+}
