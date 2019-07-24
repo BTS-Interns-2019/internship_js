01e8ad04130e2a84157848dfc12baef064d33bb3
Ricardo Martin del Campo Loza <ricardom.bts@gmail.com>
2019-07-24 18:30:44 -0500

    Commit con el primer ejercicio unzip

---
+++ jul_24/unzip-string_ricardo.js
@@ -0,0 +1,22 @@
+index = 0;
+numero = 1;
+unziped = "";
+//var arreglo = "2e2a";
+
+function unzipString(str){
+    while(index < str.length){
+        if(!isNaN(str[index])){
+            //console.log("Numero");
+            numero = str[index];
+            //console.log(numero);
+        }else{
+            //console.log("No numero");
+            for(i = 0; i < numero ; i++ ){
+                unziped+=str[index];
+            }
+        }
+    index++;
+}
+return unziped;
+}
+console.log(unzipString("221a2b"));
