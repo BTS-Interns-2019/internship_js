6cb352e0d61594d4d032a055c74b294896039b77
Miguel <miguelv.bts@gmail.com>
2019-07-25 18:53:16 -0500

    Cuatro pruebas realizadas con exito para el ejercicio de unzip-string

---
+++ jul_24/unzip-string_miguel.test.js
@@ -0,0 +1,14 @@
+const unzipString = require('./unzip-string_miguel.js');
+
+test("Prueba c2a5f2b",function(){
+    expect(unzipString('c2a5f2b')).toBe('caafffffbb');
+})
+test("Prueba 5cos2n6x",function(){
+    expect(unzipString('5cos2n6x')).toBe('cccccooooosssssnnxxxxxx');
+})
+test("Prueba 2la6mi2c1e",function(){
+    expect(unzipString('2la6mi2c1e')).toBe('llaammmmmmiiiiiicce');
+})
+test("Prueba abc2xyz",function(){
+    expect(unzipString('abc2xyz')).toBe('abcxxyyzz');
+})
