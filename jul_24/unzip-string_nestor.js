5f3e4b1f56e0e0cb9a013447726f79853461f173
rabbin <rabbin_cbta@hotmail.com>
2019-07-24 19:50:59 -0500

    funcion de unzip v0.1

--- jul_24/unzip-string_nestor.js
+++ jul_24/unzip-string_nestor.js
@@ -4,12 +4,16 @@ function unzipString(data){
     for(let i = 0; i < data.length; i++){        
         if(!isNaN(data[i])){
             if(!isNaN(data[i]) == isNaN(data[i+1])){
-                res += data[i+1].repeat(data[i]);                    
+                res += data[i+1].repeat(data[i]);
+                i++;
             }
+        }else{
+                res += data[i]
         }
     }
+    
     return res;
 }
-console.log(unzipString("2a3v5bl"));
+console.log(unzipString("1233abc"));
 
 module.exports = unzipString;    
