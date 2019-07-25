d40efa685dd31a838f7c5ee095030785c8ee601c
estefaniacr <estefaniac.bts@gmail.com>
2019-07-24 19:25:04 -0500

    avance del ejercicio expenses-parser

---
+++ jul_24/expenses-parser_estefania.js
@@ -0,0 +1,40 @@
+function expensesParser(datos){
+
+JSON.stringify({initialBalance: Number, 
+expenses:[{'id':Number, 'description': String, 'cost':Number }]})
+
+
+}
+
+
+var datos = new Object()
+datos.initialBalance = Number;
+datos.expenses = Array;
+datos.totalExpense = Number;
+datos.averageExpense = Number;
+datos. finalBalance = Number;
+
+
+
+const multi= `1000.00
+127 Video 7.45
+128 Gasoline 16.10`
+
+expensesParser(`1000.00
+127 Video 7.45
+128 Gasoline 16.10`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = expensesParser;
