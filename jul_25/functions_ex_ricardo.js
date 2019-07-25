43265f7026e04f90c5bf8df0b9094fdecd4eac3d
Ricardo Martin del Campo Loza <ricardom.bts@gmail.com>
2019-07-25 12:20:43 -0500

    Commit con el primer ejercicio del dia

---
+++ jul_25/functions_ex_ricardo.js
@@ -0,0 +1,61 @@
+/*
+num = 0;
+if(num == 0){
+    func = function (){return "Ajalas Bistec";};
+}
+console.log(func);
+*/
+var objeto;
+
+var persona = {
+    nombre : "",
+    direccion:"",
+    numero:2,
+    ocupacion:"",
+    edad:2,
+    comidaFavorita:[],
+    fraseFavorita:"",
+};
+
+
+
+function name(objeto, valor){
+    objeto.nombre = valor;
+}
+
+function adress(objeto, valor){
+    objeto.direccion = valor;
+}
+
+function number (objeto, valor){
+    objeto.numero = valor;
+}
+
+function ocupation(objeto, valor){
+    objeto.ocupacion = valor;
+}
+
+function age(objeto, valor){
+    if(valor > 18) objeto.edad = function(){return "hurray!";}
+    else{objeto.edad = valor;}
+    
+}
+
+function favoriteFood(objeto, valor){
+    objeto.comidaFavorita = valor;
+}
+
+function favoriteFrase(objeto, valor){
+    objeto.fraseFavorita = valor;
+}
+
+name(persona, "Ricardo");
+adress(persona, "Granada 155");
+number("7813032");
+ocupation(persona, "Developer");
+age(persona, 22);
+favoriteFood(persona, ["papasALaFrancesa", "Pizza", "Tacos"]);
+favoriteFrase(persona, "Ajalas bistec");
+
+console.log(persona);
+
