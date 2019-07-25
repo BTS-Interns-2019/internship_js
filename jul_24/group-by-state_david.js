840d7d3df05c3105bff5d379ac85ed60c34381f1
silva-david <silvad.bts@gmail.com>
2019-07-24 20:30:54 -0500

    david unzip-string exercise

---
+++ jul_24/group-by-state_david.js
@@ -0,0 +1,34 @@
+// function groupByState() {
+
+// }
+const input = `John Snow, 341 Kings Road, KL
+Arya Stark, 22 No Where St, WF
+Jaime Lannister, 73 1st Street, KL`
+
+a = [];
+b = input.split(/\n/);
+for (i = 0; i < b.length; i++) {
+    a[i] = b[i].split(',');   
+}
+
+name = [];
+address = [];
+state = [];
+for (i = 0; i < a.length; i++) {
+    name[i] = a[i][0];
+    address[i] = a[i][1];
+    state[i] = a[i][2];
+}
+// console.log(name);
+// console.log(address);
+// console.log(state);
+
+var output = new Object();
+for (i = 0; i < state.length; i++) {
+    output[state[i]] = {};
+}
+
+console.log(output);
+
+// }
+//module.exports = groupByState;
