27c7c6fa8f9427a58c31976bf1e913d488213581
Marlon Torres <marlont.bts@gmail.com>
2019-07-25 18:55:50 -0500

    pruebas a averageSpeed

---
+++ jul_25/functions_ex_marlont.js
@@ -0,0 +1,60 @@
+var persona = {};
+
+function addNombre(obj, valor){
+  return obj.nombre = valor;
+}
+
+function addDireccion(obj, valor){
+    return obj.direccion = valor;
+}
+
+function addNumero(obj, valor){
+    return obj.numero = valor;
+}
+
+function addOcupacion(obj, valor){
+    return obj.ocupacion = valor;
+}
+
+function addComidaFavorita(obj, valor){
+    return obj.comidaFavorita = valor;
+}
+
+function addFraseFavorita(obj, valor){
+    return obj.fraseFavorita = valor;
+}
+
+function addEdad(obj, valor){
+    if(valor > 18){
+      return obj.edad = function(){return "hurray";};
+    } else {
+      return obj.edad = valor;
+    }
+}
+
+// Crear un contador de velocidad promedio
+// Nombre de la funcion padre averageSpeed
+// Regresa una funcion funcionHija que recibe un solo parametro numerico, que es la velocidad
+//   Esa funcion regresa la velocidad promedio
+//   Cada vez que se mande llamar la funcion hija debe tomar el parametro de la velocidad y calcular
+//   la nueva velocidad.
+
+var contador = 0;
+var sumaVelocidades = 0;
+var velocidadPromedio = 0;
+
+function averageSpeed(velocidad){
+  function funcionHija(velocidad){
+    if(isNaN(velocidad)){
+      return velocidadPromedio;
+    } else {
+      contador ++;
+      sumaVelocidades = sumaVelocidades + velocidad;
+      velocidadPromedio = sumaVelocidades / contador;
+      return velocidadPromedio;
+    }
+  }
+  return funcionHija(velocidad);
+}
+
+module.exports = averageSpeed;
---
+++ jul_25/jest_marlont.test.js
@@ -0,0 +1,57 @@
+//function sum(primero, segundo){
+//  return primero + segundo;
+//}
+
+//test('Suma de 2+2', function() {
+//  expect(sum(2,2)).toBe(4);
+//})
+
+//test('Suma de 4+3', function() {
+//  expect(sum(4, 3)).toBe(7);
+//})
+
+//test('Suma de 4+9', function() {
+//    expect(sum(4, 9)).toBe(13);
+//})
+
+//test('Suma de 4+4', function() {
+//    expect(sum(4, 4)).toBe(8);
+//})
+
+//test('Suma de 4+5', function() {
+//    expect(sum(4, 5)).toBe(9);
+//})
+
+//test('Suma de 6+6', function() {
+//    expect(sum(6, 6)).toBe(12);
+//})
+
+test('Probando 1 entrada', function(){
+    expect(averageSpeed(100)).toBe(100);
+})
+
+test('Probando 2 entradas', function(){
+    expect(averageSpeed(50)).toBe(75);
+})
+
+test('Probando entrada string', function(){
+    expect(averageSpeed('m')).toBe(75);
+})
+
+test('Probando 3 entradas: 100 + 50 + 150', function(){
+  expect(averageSpeed(150)).toBe(100);
+})
+
+test('Probando 4 entradas: 100 + 50 + 150 + 100', function(){
+  expect(averageSpeed(100)).toBe(100);
+})
+
+test('Probando 5 entradas: 100 + 50 + 150 + 100 + 200', function(){
+  expect(averageSpeed(200)).toBe(120);
+})
+
+////////////////////////////////////////////////////////////////////////
+
+const averageSpeed = require('./functions_ex_marlont');
+
+
