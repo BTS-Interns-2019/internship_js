4692cd4d365346af4dd6ead06e1f308bf5523344
sergio rivera <telesforociriaco@gmail.com>
2019-07-24 19:25:47 -0500

    ya se resta del balance total , pero no hace lo de multilinea

---
+++ jul_24/expenses-parser_sergio.js
@@ -0,0 +1,69 @@
+function expensesParser(input) {
+    input = input.split(" ");
+    let initialBalance = input[0];
+    console.log(initialBalance);
+    let totalExpense = 0;
+    let idYeso = {id:0,description:"nada",cost:2,balance:2};
+    var objetoRes = {initialBalance:initialBalance,expenses:[],totalExpense:totalExpense,averageExpense:0.000,finalBalance:initialBalance};
+    
+    
+    
+    let pos = 0;
+    for (let i = 1; i < input.length; i++) {
+        
+            //console.log("aqui");
+            for (key in idYeso) {
+                if( key === "id" ){idYeso[key] = input[1+pos];}
+                if( key === "description" ){idYeso[key] = input[2+pos];}
+                if( key === "cost" ){
+                    idYeso[key] = input[3+pos];
+                    console.log(totalExpense);
+                    
+                    totalExpense += input[3+pos];
+                    console.log(totalExpense);
+                    
+                }
+                if( key === "balance" ){idYeso[key] = (objetoRes["finalBalance"]-=input[3+pos]).toFixed(2)}
+                //console.log(key);
+                //console.log(idYeso[key]);   
+                totalExpense = parseInt(totalExpense);
+            }
+            pos+=3;
+            i+=3;
+        
+        console.log(idYeso);
+        console.log(objetoRes);
+        objetoRes["expenses"].push(idYeso);
+        objetoRes["totalExpense"].push(totalExpense)
+        
+        
+    }
+
+    
+    //console.log(objetoRes);
+    
+    
+}
+
+console.log(expensesParser("1000.00 127 Video 7.45 128 Gasoline 16.10"));
+
+// {
+//     initialBalance: 1000.00,
+//     expenses: [
+//       {
+//         id: 127
+//         description: 'Video',
+//         cost: 7.45,
+//         balance: 992.55
+//       },
+//       {
+//         id: 128
+//         description: 'Gasoline',
+//         cost: 16.10,
+//         balance: 976.45
+//       }
+//     ],
+//     totalExpense: 23.55,
+//     averageExpense: 11.775,
+//     finalBalance: 976.45
+//   }
