a2248f4cdf61283b1e73cec7e527e8c3ad2923b4
estefaniacr <estefaniac.bts@gmail.com>
2019-07-25 14:27:56 -0500

    ejercicio tres final

--- jul_25/functions_ex_estefania.js
+++ jul_25/functions_ex_estefania.js
@@ -1,3 +1,17 @@
+//In JavaScript, a function can be defined based on a condition. For example, the following function definition defines myFunc only if num equals 0
+
+var laFuncion;
+let num = 0;
+if (num === 0) {
+  laFuncion = function(algo) {
+    return "Hola " + algo;
+  }
+}
+
+let x = laFuncion("mundo")
+x
+
+
 // objeto persona con propiedades
 
 const persona = {};
@@ -46,3 +60,29 @@ food(persona, ['Arroz con leche', 'tacos de suadero']);
 phrase(persona, 'Los sueños pueden cumplirse o no, pero lo importante es luchar por ellos');
 
 console.log(persona);
+
+
+/*crea un contador de velocidad promedio
+
+nombre de la funcion padre 'averageSpeed'
+regresa una funcion hija 'funcionHija'
+
+*/
+
+
+
+function averageSpeed() {
+    let suma = 0;
+    let i = 0;
+
+    return function funcionHija(speed) {
+            i++
+            suma += speed
+            return suma / i
+        }      
+    }
+
+let average = averageSpeed();
+console.log(average(100));
+console.log(average(50))
+console.log(average(75))
