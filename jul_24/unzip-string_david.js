c57af8799edf1294d1973258d4dd81c7e46e36db
silva-david <silvad.bts@gmail.com>
2019-07-24 20:30:54 -0500

    david unzip-string exercise

--- jul_24/unzip-string_david.js
+++ jul_24/unzip-string_david.js
@@ -1,30 +1,54 @@
-zip = '';
-// function unzipString(zip) {
-    
-//     return unzip
-// }
+//zip = 'a2bc';
+//zip = '3D2a5d2f';
+//zip = '3d332f2a';
+//zip = '';
+//console.log(unzipString(zip));
 
-array = zip.split('');
-if (array.length == 0) {
-    unzip = "''";
-} else {
-    unzip = '';
-    for (i = 0; i < array.length; i++) {
-        if (Number(array[i])) {
-            for (j = i+1; j < array.length; j++) {
-                if (Number(array[j])) {
-                    break;
-                } else {
-                    n = Number(array[i]);
-                    unzip += array[j].repeat(n);
-                }            
+function unzipString(zip) {
+    x = zip.split('');
+    if (x.length==0) {
+        unzip = "''";
+    } else {
+        unzip = '';
+        y = false;
+        for (i = 0; i < x.length; i++) {
+            //y = false;
+            if (Number(x[i])) {
+                for (j = i+1; j < x.length; j++) {
+                    if (Number(x[j])) {
+                        break;
+                    } else {
+                        n = Number(x[i]);
+                        unzip += x[j].repeat(n)
+                        y = true;
+                    }
+                }
             }
-        } else {
 
-            unzip += array[i]        
+
+            // if (y) {
+            //     i+=1;
+            // } else if (Number(x[i+1])) {
+            //     continue;        
+            // } else {
+            //     unzip += x[i]
+            // }
+
+            if (y) {
+                i+=1;
+            } else if (!Number(x[i])) {
+                unzip += x[i];             
+            } else if (Number(x[i+1])) {
+                unzip += x[i]
+                continue;       
+            }
+
+            
         }
-        console.log(unzip);
     }
+   
+    return unzip
 }
 
-console.log(unzip);
+// console.log(unzip);
+module.exports = unzipString;
