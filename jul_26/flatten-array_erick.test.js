118cb628a846e9829680349e93b88b1a180e11ce
Erick Vazquez <erickbardo.bts@gmail.com>
2019-07-26 18:47:14 -0500

    Corregido flatten con Throw

--- jul_26/flatten-array_erick.test.js
+++ jul_26/flatten-array_erick.test.js
@@ -2,18 +2,17 @@ const arr1 = [];
 const arr2 = [1, 2, ['a', 'b'],3 ,4];
 const arr3 = [1, 2, ['a', ['b', 'x']],3 ,4];
 const arr4 = [1, 2, ['a', ['b', ['x']]],3 ,4];
-const arr5 = [1, 2, [{foo: 'bar'}, [b]],3, 4];
+const arr5 = [1, 2, [{foo: 'bar'}, ["b"]],3, 4];
 
 
 function flattenArray(arr){
+  if(!Array.isArray(arr)){
+  throw new Error("This is not Array")
+  }
   const res = []
   let pass = arr
   function foo(){
-    if(!Array.isArray(pass)){
-      throw "This is not Array"
-    }
-    console.log(res)
-      for(let i of pass){
+    for(let i of pass){
         if(Array.isArray(i)){
           pass = i
           foo(pass)         
@@ -21,8 +20,10 @@ function flattenArray(arr){
           res.push(i)
         } 
       }
-      console.log(res)
+      return res
     }
     return foo()
 }
-console.log(flattenArray(arr4))
+console.log(flattenArray(arr5))
+
+module.exports = flattenArray.js
