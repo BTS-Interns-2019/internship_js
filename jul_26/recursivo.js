a30c1d538e767ff64a566f4042a50834f8bcc5dc
emmanuelr.bts@gmail.com <emmanuel.lv.97@gmail.com>
2019-07-26 12:22:56 -0500

    primer función recursiva

---
+++ jul_26/recursivo.js
@@ -0,0 +1,40 @@
+obj={
+    name: "chicorita",
+    children: [
+        {
+        name: "lv 2",
+        children: [{
+            name: "lv 3",
+            children: [{
+                name: "lv4",
+                children: [{
+                    name: "lv5",
+                    children: []
+                }]
+            }]
+        }]
+    }, {
+        name: "x lv2",
+        children: [{
+            name: "xk l3",
+            children: []
+        }]
+    }, {
+        name: "y lv2",
+        children: [{
+            name: "yb lv3",
+            children: []
+        }]
+    }, {
+        name: "z lv2",
+        children: [{
+            name: "zv lv3",
+            children: []
+        }]
+    }]
+}
+function rec(obj){
+    console.log(obj.name);
+    if(obj.children!=0)obj.children.forEach(hijo => {rec(hijo)});
+}
+rec(obj);
