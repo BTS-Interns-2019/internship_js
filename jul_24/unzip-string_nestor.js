49ea53a2db15efcbd3055f2f779927138bd95ba0
rabbin <rabbin_cbta@hotmail.com>
2019-07-25 19:13:19 -0500

    unfinish homework of testing

--- jul_24/unzip-string_nestor.js
+++ jul_24/unzip-string_nestor.js
@@ -1,19 +1,21 @@
 function unzipString(data){
     data = data.split("");
     var res ='';
-    for(let i = 0; i < data.length; i++){        
-        if(!isNaN(data[i])){
-            if(!isNaN(data[i]) == isNaN(data[i+1])){
-                res += data[i+1].repeat(data[i]);
-                i++;
-            }
-        }else{
-                res += data[i]
+    for (let i = 0; i < data.length; i++) {
+        if(isNaN(data[i])){
+
         }
+        
     }
-    
     return res;
 }
-console.log(unzipString("1233abc"));
+console.log(unzipString("1233abc")); // aaabbbccc
+✕ console.log(unzipString("3abc")) //=> aaabbbccc (5ms)
+  ✕ console.log(unzipString("8abc")) // => aaaaaaaabbbbbbbbcccccccc
+  ✕ console.log(unzipString("2abc")) //=> aabbcc (1ms)
+  ✕ console.log(unzipString("abcd")) //=> abcd
+  ✓ console.log(unzipString(""))
+  ✕ console.log(unzipString("1233abc")) // => aaabbbccc
+  ✕ console.log(unzipString("22abc1d")) // => aabbccd (1ms)
 
 module.exports = unzipString;    
