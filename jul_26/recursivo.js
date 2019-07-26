b67fcd6550def9076ce09f9f2134ea1ad06e5348
estefaniacr <estefaniac.bts@gmail.com>
2019-07-26 13:37:55 -0500

    ejercicio de recursividad

---
+++ jul_26/recursivo.js
@@ -0,0 +1,61 @@
+
+var arbol = {"name": "chicorita",
+"children": [
+    {
+        "name": "lv 2",
+        "children": [
+            {
+                "name": "lv 3",
+                "children": [
+                    {
+                        "name": "lv4",
+                        "children": [
+                            {
+                                "name": "lv5",
+                                "children": []
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+            },
+            {
+                "name": "x lv2",
+                "children": [
+                    {
+                        "name": "xk l3",
+                        "children": []
+                    }
+                ]
+            },
+            {
+                "name": "y lv2",
+                "children": [
+                    {
+                        "name": "yb lv3",
+                        "children": []
+                    }
+                ]          
+            },
+            {
+                "name": "z lv2",
+                "children": [
+                {
+                    "name": "zv lv3",
+                    "children": []
+                }
+            ]
+        }
+    ]
+}
+
+function losNombres(arbol){
+    console.log(arbol.name);
+    if (arbol.children!=[]) {
+        for(a of arbol.children) {
+            losNombres(a)
+        }
+    }
+}
+losNombres(arbol)
