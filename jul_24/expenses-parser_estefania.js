7a962fe7e6479c0e9edffd4d456e6cd424101b6f
estefaniacr <estefaniac.bts@gmail.com>
2019-07-25 20:53:06 -0500

    logre terminar el ejercicio de expenses-parser :)

--- jul_24/expenses-parser_estefania.js
+++ jul_24/expenses-parser_estefania.js
@@ -1,40 +1,53 @@
-function expensesParser(datos){
+var input = '1000.00 127 Video 7.45 128 Gasoline 16.10'
 
-JSON.stringify({initialBalance: Number, 
-expenses:[{'id':Number, 'description': String, 'cost':Number }]})
+function expensesParser(input){
+    var arr = input.split(" ");
+    console.log(arr)
+    var obj = new Object()
+    if (arr.length == 0) {
+        return "No hay datos"
+    } else {
+        obj.initialBalance = Number(arr[0])
+        obj.expenses = []
+        let y = 1
+        let resta = Number(arr[0])
+        for (i=0; i<(arr.length-1)/3; i++) {
+            obj.expenses[i] = new Object()
+            console.log(i)
+            for(x=1; x < 5; x++) {
+               
+                switch (x) {
+                    case 1: 
+                        obj.expenses[i].id = Number(arr[y])
+                        y++
+                        break
 
+                    case 2: 
+                        obj.expenses[i].description = (arr[y])
+                        y++
+                        break
+
+                    case 3:
+                        obj.expenses[i].cost = Number(arr[y])
+                        y++
+                        break
+
+                    case 4:
+                        resta = resta-Number(arr[y-1])
+                        obj.expenses[i].balance = resta;
+                        break
+                }
+            }
+        }
+        obj.totalExpenses = Number(arr[3]) +  Number(arr[6]);
+        obj.averageExpenses = (Number(arr[3]) +  Number(arr[6])) /2;
+        obj.finalBalance = resta
+        console.log(obj)
+        
+    }
 
 }
 
-
-var datos = new Object()
-datos.initialBalance = Number;
-datos.expenses = Array;
-datos.totalExpense = Number;
-datos.averageExpense = Number;
-datos. finalBalance = Number;
-
-
-
-const multi= `1000.00
-127 Video 7.45
-128 Gasoline 16.10`
-
-expensesParser(`1000.00
-127 Video 7.45
-128 Gasoline 16.10`)
-
-
-
-
-
-
-
-
-
-
-
-
-
+expensesParser(input)
 
 module.exports = expensesParser;
