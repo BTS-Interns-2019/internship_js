41542a6ccc89fc29eb863e213e49577a4abb331a
Miguel Angel <miguelv.bts@gmail.com>
2019-07-26 08:46:23 -0500

    Expenses Parser already passed one test, but still with failures

--- jul_24/expenses-parser_miguel.js
+++ jul_24/expenses-parser_miguel.js
@@ -1,7 +1,7 @@
 module.exports = expensesParser;
 
 function expensesParser(word){
-    let res = "";
+    let res;
     let temp = [];
     let auxw = word;
     /* Solo asigna valor para dividir el balance del resto */
@@ -15,48 +15,55 @@ function expensesParser(word){
     let palabras = auxw.split('^');
     balance = palabras[0];
     valor = palabras[1];
+    while(valor.charAt(0) == '\n'){
+        valor = valor.substring(1,valor.length);
+    }
     //console.log("balance = "+balance);
     //console.log("valor = "+valor);
     for(let s = 0, s1 = -1, s2 = 1;s<valor.length; s++, s1++, s2++){
         //console.log("v = "+valor.charAt(s)+" v-1 = "+valor.charAt(s1)+" v+1 = "+valor.charAt(s2));
-        if(valor.charAt(s) == '\n'){
+        if(valor.charAt(s) == '\n' && valor.charAt(s+1) != '\n'){
             valor = valor.replace(valor.charAt(s),'^');
         } 
     }
     //console.log("valor = "+valor);
     valores = valor.split("^");
     let costos = 0;
+    let b = parseInt(balance);
     let bal = parseFloat(balance);
+    let list=[];
     for(let i = 0; i<valores.length;i++){
         let elementos = valores[i].split(" ");
         let id = elementos[0];
         let idn = parseInt(id);
         let descripcion = elementos[1];
         let costo = elementos[2];
-        let cost = parseFloat(costo)
+        let cost = parseFloat(costo);
         costos = costos + cost;
         bal = bal - cost;
-        temp[i]="{\nid: "+idn+",\ndescription: "+descripcion+",\ncost: "+cost+",\nbalance: "+bal+"\n}"
+        temp[i]={id:idn,description:descripcion,cost:cost,balance:bal};
+        list.push(temp[i]);
     }
-    res="{\ninitialBalance: "+balance+",\nexpenses:[\n";
-    for(let j = 0; j<temp.length; j++){
-        res=res+temp[j];
-        if(j == temp.length-1){
-            res=res+"\n";
-        }else{
-            res=res+",\n";
-        }
+    res={initialBalance: b,
+    expenses: list,
+    totalExpense: costos,
+    averageExpense: costos/2,
+    finalBalance: bal
     }
-    res=res+"],\n";
-    res=res+"totalExpense: "+costos+",\naverageExpense: "+costos/2+",\nfinalBalance: "+bal+"\n";
-    res=res+"}";
     return res;
 }
 
 const input = `1000.00
 127 Video 7.45
 128 Gasoline 16.10`
+const blanc = `1000.00
+
+127 Video 7.45
+
+128 Gasoline 16.10`
+;
 console.log(expensesParser(input));
+console.log(expensesParser(blanc));
 //First balance: 992.55
 //Second balance: 976.44999999
 /*Despues de los corchetes
