ec5863ebb032c72410132aa56ecdf7b7cc6f2de3
Miguel Angel <miguelv.bts@gmail.com>
2019-07-26 08:46:23 -0500

    Expenses Parser already passed one test, but still with failures

---
+++ jul_24/expenses-parser_miguel.test.js
@@ -0,0 +1,15 @@
+const expensesParser = require("./expenses-parser_miguel.js");
+
+input1="";
+input2="";
+input3="";
+input4="";
+result1="";
+result2="";
+result3="";
+result4="";
+//PRUEBAS
+
+test("Prueba ",function(){
+    expect(expenserParser(input1)).toBe(result1);
+})
