2c4f8b6d76293fadbc80a088dcec4c524e06d41b
Erick Vazquez <erickbardo.bts@gmail.com>
2019-07-26 18:24:22 -0500

    Avance de flatten-array

---
+++ jul_26/flatten-array_erick.test.js
@@ -0,0 +1,25 @@
+const arr1 = [];
+const arr2 = [1, 2, ['a', 'b'],3 ,4];
+const arr3 = [1, 2, ['a', ['b', 'x']],3 ,4];
+const arr4 = [1, 2, ['a', ['b', ['x']]],3 ,4];
+const arr5 = [1, 2, [{foo: 'bar'}, [b]],3, 4];
+
+
+function flattenArray(arr){
+  const res = []
+  let pass = arr
+  function foo(){
+    console.log(res)
+      for(let i of pass){
+        if(Array.isArray(i)){
+          pass = i
+          foo(pass)         
+        }else{
+          res.push(i)
+        } 
+      }
+      console.log(res)
+    }
+    return foo()
+}
+console.log(flattenArray(arr4))
