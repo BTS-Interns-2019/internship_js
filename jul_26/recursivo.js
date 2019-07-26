37d8322fa250d4c8820993ee6aa746cb854f21d5
unknown <luiscarlosg.bts@gmail.com>
2019-07-26 12:23:29 -0500

    rvre

---
+++ jul_26/recursivo.js
@@ -0,0 +1,63 @@
+let chicorita = {
+    "name": "chicorita",
+    "children": [
+                    {
+                        "name": "lv 2",
+                        "children": [
+                                        {
+                                            "name": "lv 3",
+                                            "children": [
+                                                            {
+                                                                "name": "lv4",
+                                                                "children": [
+                                                                                {
+                                                                                    "name": "lv5",
+                                                                                    "children": []
+                                                                                }
+                                                                            ]
+                                                            }
+                                                        ]
+                                        }
+                                    ]
+                        },
+                        {
+                        "name": "x lv2",
+                        "children": [
+                                    {
+                                        "name": "xk l3",
+                                        "children": []
+                                    }
+                                    ]
+                        },
+                        {
+                        "name": "y lv2",
+                        "children": [
+                                        {
+                                        "name": "yb lv3",
+                                        "children": []
+                                        }
+                                    ]
+                        },
+                        {
+                        "name": "z lv2",
+                        "children": [
+                                        {
+                                         "name": "zv lv3",
+                                        "children": []
+                                        }
+                                    ]
+                        }
+                    ]
+                }
+
+function funcion(chicorita){
+    console.log(chicorita.name);
+    if(chicorita.children!=[]){
+        for(c of chicorita.children){
+            funcion(c);
+        }
+    }
+}
+
+funcion(chicorita);
+
