863ed00ba67b3935acfcd8fda390bfe67040e43b
Luis G <luicarlosg.bts@gmail.com>
2019-07-25 22:46:41 -0700

    correción y pruebas grouop by state

--- jul_24/group-by-state.js
+++ jul_24/group-by-state.js
@@ -1,22 +1,18 @@
 function groupByState(input){
 let output = {};
  let lineas = input.split("\n");
- let personas = [];
- for(let l of lineas){
-     personas.push({});
- }
+
  for(let l in lineas){
   let split = lineas[l].split(",");
   if(split.length==3){
       let estado = split[2].trim();
       let nombre = split[0].trim();
       let address = split[1].trim();
-      personas[l]={
+      let persona={
           'name':nombre,
           'address':address,
           'state': estado
       };
-      let personaadd = personas[l];
       let exi = Object.getOwnPropertyNames(output);
       if(exi.indexOf(estado)==-1){
         let newState = {};
@@ -40,18 +36,12 @@ let output = {};
             break;
         }
         newState.name=nombre;
-        newState.people=[{}];
-        console.log(personas[l]);
-        newState.people[0]={};
-        Object.assign(newState.people[0],personas[l]);
+        newState.people=[];
+        newState.people[0]=persona;
         output[estado]=newState;
 output[estado] = newState;
       }else{
-          console.log(personaadd);
-       // output[estado].people.push({});
-        //let pos = output[estado].people.length;
-        //Object.assign(output[estado].people[pos],personaadd);
-        output[estado].people.push(personas[l]);
+        output[estado].people.push(persona);
       }
   }
  }
@@ -59,7 +49,7 @@ output[estado] = newState;
  return output;
 }
 
-console.log(groupByState(`John Snow, 341 Kings Road, KL
+let res = groupByState(`John Snow, 341 Kings Road, KL
 Arya Stark, 22 No Where St, WF
-Jaime Lannister, 73 1st Street, KL`));
+Jaime Lannister, 73 1st Street, KL`);
 module.exports = groupByState;
