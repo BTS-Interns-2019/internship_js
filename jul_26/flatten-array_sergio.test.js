9d3ddcc20aa1c5b64b3906e954084a9f1da83e3c
sergio rivera <telesforociriaco@gmail.com>
2019-07-26 19:08:12 -0500

    flatten-array 1

--- jul_26/flatten-array_sergio.test.js
+++ jul_26/flatten-array_sergio.test.js
@@ -1,32 +1,30 @@
 function flattenArray(array) {
     if( array.length < 1){return []}
     let res = [];
+    for (let k = 0; k < array.length; k++) {
+        if( Array.isArray(array[k]) ){
+            res.push(sacar(array[k]))
+        }else if(typeof array[i] == "object"){
+            res.push(sacar(array[k]));
+
+        }else{
+            res.push(array[k]);
+        }
+    }
     function sacar(array){
 
         for (let i = 0; i < array.length; i++) {
-            if(Array.isArray(array[i])){ 
-                console.log(res);
-                return sacar(array[i]);
-                
-            }
-            try {
-                if(typeof array[i] == "object"){
-                    throw 'esto no es un arreglo es un objeto';
-                }
+            if(Array.isArray(array[i])){ sacar(array[i])
+            }else if(typeof array[i] == "object"){
                 res.push(array[i])
-                console.log(res);
-            } catch (error) { 
-                res.push(array[i])
-                console.log(error); 
-                console.log(res);    
+                   throw 'esto no es un arreglo es un objeto';
             }
+            res.push(array[i])
         }
-        
-        console.log(res);
         return res;
-        
     }
+
     return sacar(array)
 }
 
-console.log(flattenArray([1,2,[{foo:'bar'},['b']],3,4]));
+console.log(flattenArray([1,2,[{foo: 'bar'},['b']],3,4]));
