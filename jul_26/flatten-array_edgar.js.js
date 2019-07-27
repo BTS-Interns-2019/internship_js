10af39c647267053a723d7b5c5951dbf4d0e0a73
Edgar Peregrino <edgarpere.bts@gmail.com>
2019-07-26 20:28:41 -0500

    flatten array exercises

--- jul_26/flatten-array_edgar.js.js
+++ jul_26/flatten-array_edgar.js.js
@@ -1,20 +1,15 @@
 function flattenArray (cadena){
     var flat =[];
-    try{
-        for (i= 0; i < cadena.length; i++){
-           if (array.isArray(cadena(i))){
-            flat = flat.concat(flattenArray(cadena(i)));
-  } else { flat.push(cadena (i));
-    
-     throw  "error";
+        for (i in cadena){
+           if (cadena[i] instanceof Array){
+            flat = flat.concat(flattenArray(cadena [i]));                    
+      } else { 
+      flat.push(cadena[i]); 
     } 
+    
 }
-}  catch (error){
-            console.log ("Se esperaba un arreglo");
-}
-return flat;
+ return flat; 
 }
 
 
-var array = [1,2,['a','b'], 3,4, ['c','d']];
-console.log (flattenArray (array));
+console.log (flattenArray (array = [1, 2,['a',{lol:'b'}], 3, 4, ['c',['d', 'e', 'f']]]))
