3044014ac58e36a3c4127f47270424b525fea85c
Marlon Torres <marlont.bts@gmail.com>
2019-07-30 18:57:05 -0500

    mutation-methods incompleto

--- jul_30/mutation-methods_marlont.js
+++ jul_30/mutation-methods_marlont.js
@@ -1,12 +1,27 @@
 function sort(array, compare){
-  for(var i=0; i<array.length-1; i++){
-    for(var j=i+1; j < array.length; j++){
-      var result = compare(array[i], array[j]);
-      if(result > 0){
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-        console.log(array);
+  if(typeof compare != 'undefined'){
+    for(var i=0; i<array.length-1; i++){
+      for(var j=i+1; j < array.length; j++){
+        var result = compare(array[i], array[j]);
+        if(result > 0){
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+          console.log(array);
+        }
+      }
+    }
+  } else {
+    for(var i=0; i<array.length-1; i++){
+      for(var j=i+1; j < array.length; j++){
+        var prim = array[i].toString;
+        var segu = array[j].toString;
+        if(prim.localeCompare(segu) > 0){
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+          console.log(array);
+        }
       }
     }
   }
