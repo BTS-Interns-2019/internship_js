81d3de57f0b824f20ff81668f049145dcb1a9ef5
Edgar <edgarpere.bts@gmail.com>
2019-07-30 01:04:14 -0500

    iterators exercise and tests

--- jul_29/am_iterators_edgar.js
+++ jul_29/am_iterators_edgar.js
@@ -18,12 +18,14 @@ function maps (cad, cont){
     return "["+res+"]";
 }
 
-console.log(maps([1,2,3,4,5], 10));
+console.log(maps(["hey", 2,5,"lol"], 2));
+
+module.exports = maps;
 
 function filters (cad, cont){
     res = [];
     for (i = 0; i < cad.length; i++){
-        if (cad[i] || cad[i == cont]){
+        if (cad[i] || cad[i] == cont){
             if (i < cad.length - 1){
             res += cad[i]+",";
         } else if (cad[i] && i == cad.length - 1){
@@ -36,6 +38,8 @@ function filters (cad, cont){
 
 console.log(filters([NaN,null,"",4,"hola",6], true));
 
+module.exports = filters;
+
 function reduces (cad, acu){
     for (i = 0; i < cad.length; i++){
             acu = acu + cad[i];
@@ -43,7 +47,9 @@ function reduces (cad, acu){
     return acu
 }
 
-console.log(reduces([1,2,3,4,5,6], 0))
+console.log(reduces(["hey", 2, 5, "lol"], 2))
+
+module.exports = filters;
 
 
 function reducesright (cad, acu){
@@ -53,7 +59,7 @@ function reducesright (cad, acu){
     return acu
 }
 
-console.log(reducesright([1,2,3,4,5,6], 0))
+console.log(reducesright(["hey", 2, 5, "lol"], 2))
 
 
 
--- jul_29/am_iterators_edgar.test.js
+++ jul_29/am_iterators_edgar.test.js
@@ -1,7 +1,81 @@
 //tests foreach function
 
-test ('6 argumentos', () => {expected (foreach([10,11,23,21,56,98])).toBe(undefined)
+test ('6 argumentos', () => {
+    expect (foreach([10,11,23,21,56,98])).toBe(undefined)
+})
+
+test ('4 argumentos', () => {
+    expect (foreach(["hey",2,5,"lol"])).toBe(undefined)
+})
+
+test ('3 argumentos', () => {
+    expect (foreach(["hjj", "cmn", "jdf"])).toBe(undefined)
 })
 
 
 const foreach = require ('./am_iterators_edgar.js')
+
+//test for maps
+
+test ('6 argumentos', () => {
+    expect (maps([10,11,23,21,56,98], 2)).toEqual([12,13,25,23,58,100])
+})
+
+test ('4 argumentos', () => {
+    expect (maps(["hey", 2,5,"lol"], 2)).toEqual(["hey2",4,7,"lol2"])
+})
+
+test ('3 argumentos', () => {
+    expect (maps(["hjj","cmn","jdf"], 4)).toEqual(["hjj4","cmn4", "jdf4"])
+})
+
+const maps = require ('./am_iterators_edgar.js')
+
+//test for filters
+
+test ('6 argumentos', () => {
+    expect (filters([2,11,2,21,2,98], 2)).toEqual([12,13,25,23,58,100])
+})
+
+test ('4  argumentos', () => {
+    expect (filters(["hey", 2, 5, "lol"], 2)).toEqual([2])
+})
+
+test ('3 argumentos', () => {
+    expect (filters(["hjj","cmn","jdf"], "hjj")).toEqual(["hjj"])
+})
+
+const filters = require ('./am_iterators_edgar.js')
+
+//test reduce
+
+test ('6 argumentos', () => {
+    expect (reduces([2,11,2,21,2,98], 0)).toEqual([136])
+})
+
+test ('4  argumentos', () => {
+    expect (reduces(["hey", 2, 5, "lol"], 2)).toEqual(["2hey25lol"])
+})
+
+test ('3 argumentos', () => {
+    expect (reduces(["hjj","cmn","jdf"], "hjj")).toEqual(["hjjhjjcmnjdf"])
+})
+
+const reduces = require ('./am_iterators_edgar.js')
+
+
+//test reduce right
+
+test ('6 argumentos', () => {
+    expect (reducesright([2,11,2,21,2,98], 0)).toEqual([136])
+})
+
+test ('4  argumentos', () => {
+    expect (reducesright(["hey", 2, 5, "lol"], 2)).toEqual(["2lol52hey"])
+})
+
+test ('3 argumentos', () => {
+    expect (reducesright(["hjj","cmn","jdf"], "hjj")).toEqual(["hjjhjjcmnjdf"])
+})
+
+const reducesright = require ('./am_iterators_edgar.js')
