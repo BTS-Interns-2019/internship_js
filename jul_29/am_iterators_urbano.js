318711ff327b7f5233d249dac5ef74cc8a0a5d7f
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-07-29 22:26:55 -0500

    Tarea y pruebas Jul 29

--- jul_29/am_iterators_urbano.js
+++ jul_29/am_iterators_urbano.js
@@ -1,13 +1,19 @@
 let arr = [2, 4, 6, 8];
 let arry = ["sergio","rabanito","urbanus","pere","powers","marlon"]
 let arrFalsy=[false,null,0,true,1,"hola"]
+let arr2=[1,2,3,4];
+
 
 function forEach(callback, array) {
     for (let i = 0; i < array.length; i++) {
-        // console.log()
-         callback(arr[i])
+        callback(array[i])
     }
+
+    
 }
+module.exports=forEach;
+// console.log(forEach((i) => {  console.log(i+="-") }, arry));
+
 
 function map(callback, array) {
     newArray=[];
@@ -17,10 +23,9 @@ function map(callback, array) {
     }
     return newArray;
 }
+module.exports=map;
 console.log(map((i)=>{return i.toUpperCase()},arry))
 
-
-console.log(forEach((i) => { return i+="-" }, arry));
 
 function filter(callback, array) {
     let newArray = [];
@@ -32,13 +37,44 @@ function filter(callback, array) {
     }
     return newArray
 }
+module.exports=filter;
 
 console.log(filter((i) => { if (i) { return true } }, arrFalsy));
-let ac;
-function reduce(callback,array) {
+
+
+function reduce(callback,array, initial) {
+    let result;
+    let ac;
+    if (initial===undefined) {
+        ac=array[1]
+    }else{
+        ac= initial
+    }
     for (let i = 0; i < array.length; i++) {
-        callback(ac+=callback(),arr[i]);
+        ac= callback(ac,array[i]);
+        result=ac
     }
     return ac;
 }
-console.log(reduce((a,c)=>  a + c ),arr);
+module.exports=reduce;
+console.log(reduce(((acumulate, c)=> { return acumulate+c }),arr2, 0 ));
+
+
+function reduceRight(callback,array, initial) {
+    let result;
+    let ac;
+    if (initial===undefined) {
+        ac=array[1]
+    }else{
+        ac= initial
+    }
+    for (let i = array.length-1; i >= 0; i--) {
+        ac= callback(ac,array[i]);
+        result=ac
+    }
+    return ac;
+}
+module.exports=reduceRight;
+
+console.log(reduceRight(((a,c)=> { return (a + c)} ),arr,"2"));
+
