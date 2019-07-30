36ed57052c6b4362a3671cfa8af12ec8b1a51782
Marlon Torres <marlont.bts@gmail.com>
2019-07-30 14:00:17 -0500

    cp_exercices enviado

---
+++ jul_30/cp_exercices_marlont.js
@@ -0,0 +1,51 @@
+function login(name, password){
+  const users = [
+  {name:'marlon', password:'pass1996'},
+  {name:'miriam', password:'contra98'},
+  {name:'jorge' , password:'pw2008'},
+  {name:'marlem', password:'agost1997'}
+  ];
+  
+  return users.some((user) => 
+         user.name === name && user.password === password);
+}
+
+function onlyEven(array){
+  if(typeof array == 'undefined' || array.length === 0){
+    return false;
+  }
+
+  var result = array.every((item) =>
+               item % 2 === 0);
+  if(result) return array;
+  else return false;
+  
+}
+
+function google(str){
+  switch(str){
+      case 'find' : {
+          return {
+              name: 'find',
+              description: '',
+              params:
+          }
+      }
+  }
+}
+
+function evenNumbers(array){
+  var ind = [];
+  var it = array.entries();
+  for(var i=0; i<array.length; i++){
+    var actual = it.next().value;
+    if(actual[1]%2 === 0){
+      ind.push(actual[0]);
+    }
+  }
+return ind;
+}
+
+module.exports = {
+    login, onlyEven, google, evenNumbers
+}
---
+++ jul_30/cp_exercices_marlont.test.js
@@ -0,0 +1,49 @@
+//login() Function
+test('Valid Input', function(){
+    expect(login('marlon','pass1996')).toBe(true);
+})
+
+test('Invalid Input', function(){
+    expect(login('marlon','pass')).toBe(false);
+})
+
+test('Number Input', function(){
+    expect(login(20, 22)).toBe(false);
+})
+
+test('Boolean Input', function(){
+    expect(login(true,true)).toBe(false);
+})
+
+test('Undefined Input', function(){
+    expect(login()).toBe(false);
+})
+
+//onlyEven() Function
+test('Even Numbers', function(){
+    expect(onlyEven([2,4,6,8])).toBe([2,4,6,8]);
+})
+
+test('Odd Number', function(){
+    expect(onlyEven([2,4,3,8])).toBe(false);
+})
+
+test('Input String', function(){
+    expect(onlyEven([2,'4',6,8])).toBe(false);
+})
+
+test('Empty Array', function(){
+    expect(onlyEven([])).toBe(false);
+})
+
+test('Undefined Input', function(){
+    expect(onlyEven()).toBe(false);
+})
+
+//google() Function
+
+/////////////////////////////////////
+const {
+    login, onlyEven, google, evenNumbers
+} = require('./cp_exercices_marlont');
+
