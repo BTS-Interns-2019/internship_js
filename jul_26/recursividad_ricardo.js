8e6bd9a931e7590d13ecfc56bdb7dd11aa38fda3
Ricardo Martín del campo Loza <ricardom.bts@gmail.com>
2019-07-30 09:40:34 -0500

    ediion de tarea anterior

--- jul_26/recursividad_ricardo.js
+++ jul_26/recursividad_ricardo.js
@@ -1,62 +1,62 @@
-// obj = {
-//     name: "chicorita",
-//     children: [
-//         {
-//             name: "lv 2",
-//             children: [
-//                 {
-//                     name: "lv 3",
-//                     children: [
-//                         {
-//                             name: "lv4",
-//                             children: [
-//                                 {
-//                                     name: "lv5",
-//                                     children: [
+obj = {
+    name: "chicorita",
+    children: [
+        {
+            name: "lv 2",
+            children: [
+                {
+                    name: "lv 3",
+                    children: [
+                        {
+                            name: "lv4",
+                            children: [
+                                {
+                                    name: "lv5",
+                                    children: [
 
-//                                     ]
-//                                 }
-//                             ]
-//                         }
-//                     ]
-//                 }
-//             ]
-//         },
-//         {
-//             name: "x lv2",
-//             children: [
-//                 {
-//                     name: "xk l3",
-//                     children: [
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: "x lv2",
+            children: [
+                {
+                    name: "xk l3",
+                    children: [
 
-//                     ]
-//                 }
-//             ]
-//         },
-//         {
-//             name: "y lv2",
-//             children: [
-//                 {
-//                     name: "yb lv3",
-//                     children: [
+                    ]
+                }
+            ]
+        },
+        {
+            name: "y lv2",
+            children: [
+                {
+                    name: "yb lv3",
+                    children: [
 
-//                     ]
-//                 }
-//             ]
-//         },
-//         {
-//             name: "z lv2",
-//             children: [
-//                 {
-//                     name: "zv lv3",
-//                     children: [
+                    ]
+                }
+            ]
+        },
+        {
+            name: "z lv2",
+            children: [
+                {
+                    name: "zv lv3",
+                    children: [
 
-//                     ]
-//                 }
-//             ]
-//         }
-//     ]
-// }
+                    ]
+                }
+            ]
+        }
+    ]
+}
 
 
 // function foo(i){
@@ -70,16 +70,15 @@
 
 // foo()
 
+function outside(obj){
 
+  console.log(obj.name);
+  if(Array.isArray(obj.children)){
 
-function outside(x) {
-    function inside(y) {
-      return x + y;
-    }
-    return inside;
+  obj.children.forEach(foo => {outside(foo)});
+
   }
-  fn_inside = outside(3); // Think of it like: give me a function that adds 3 to whatever you give
-                          // it
-  result = fn_inside(5); // returns 8
-  
-  result1 = outside(3)(5); // returns 8
+}
+
+
+outside(obj);
