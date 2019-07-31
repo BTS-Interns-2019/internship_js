2d814eea6c7450dc3fc6874f0befc3616e29db05
Hector Soto <hectors.bts@gmail.com>
2019-07-31 18:00:49 -0500

    Separated functions' tests

--- jul_31/dates_exercises_hector.test.js
+++ jul_31/dates_exercises_hector.test.js
@@ -1,47 +1,49 @@
 const jsonTimes = require('./dates_exercises_hector');
 
-test(`Returns an object`, () => {
-  expect(typeof jsonTimes(2016)).toBe('object');
+describe('jsonTimes', () => {
+  test(`Returns an object`, () => {
+    expect(typeof jsonTimes(2016)).toBe('object');
+  });
+  test(`Returns an array with the dates`, () => {
+    const isArray = Array.isArray(jsonTimes(2016).dates);
+    expect(isArray).toBe(true);
+  });
+  test(`Returns a number with the Friday 13 count`, () => {
+    const isInteger = Number.isInteger(jsonTimes(2016).times);
+    expect(isInteger).toBe(true);
+  });
+  
+  // jsonTimes tests
+  const jsonCases = [
+    '2015',
+    2025,
+    '2014-07-31T21:50:26.432Z',
+    new Date()
+  ];
+  
+  const jsonCountResults = [
+    3,
+    1,
+    1,
+    2
+  ];
+  
+  const jsonDatesResults = [
+    ['2/13/2015', '3/13/2015', '11/13/2015'],
+    ['6/13/2025'],
+    ['6/13/2014'],
+    ['9/13/2019', '12/13/2019']
+  ];
+  
+  for (let i = 0; i < jsonCases.length; i++) {
+    test(`Getting the number of Friday 13 in ${jsonCases[i]}`, () => {
+      expect(jsonTimes(jsonCases[i]).times).toBe(jsonCountResults[i]);
+    })
+  }
+  
+  for (let i = 0; i < jsonCases.length; i++) {
+    test(`Getting the dates of Friday 13 in ${jsonCases[i]}`, () => {
+      expect(jsonTimes(jsonCases[i]).dates).toStrictEqual(jsonDatesResults[i]);
+    })
+  }
 });
-test(`Returns an array with the dates`, () => {
-  const isArray = Array.isArray(jsonTimes(2016).dates);
-  expect(isArray).toBe(true);
-});
-test(`Returns a number with the Friday 13 count`, () => {
-  const isInteger = Number.isInteger(jsonTimes(2016).times);
-  expect(isInteger).toBe(true);
-});
-
-// jsonTimes tests
-const jsonCases = [
-  '2015',
-  2025,
-  '2014-07-31T21:50:26.432Z',
-  new Date()
-];
-
-const jsonCountResults = [
-  3,
-  1,
-  1,
-  2
-];
-
-const jsonDatesResults = [
-  ['2/13/2015', '3/13/2015', '11/13/2015'],
-  ['6/13/2025'],
-  ['6/13/2014'],
-  ['9/13/2019', '12/13/2019']
-];
-
-for (let i = 0; i < jsonCases.length; i++) {
-  test(`Getting the number of Friday 13 in ${jsonCases[i]}`, () => {
-    expect(jsonTimes(jsonCases[i]).times).toBe(jsonCountResults[i]);
-  })
-}
-
-for (let i = 0; i < jsonCases.length; i++) {
-  test(`Getting the dates of Friday 13 in ${jsonCases[i]}`, () => {
-    expect(jsonTimes(jsonCases[i]).dates).toStrictEqual(jsonDatesResults[i]);
-  })
-}
