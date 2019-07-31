500e53ffcc8b79aef6ad5223542e57bf343b04eb
ViryDe <virid.bts@gmail.com>
2019-07-31 18:10:34 -0500

    avence actividad en clase

---
+++ jul_31/date_exercises_viridiana.js
@@ -0,0 +1,36 @@
+# Dates exercises
+Function definitions in the file `date_exercises_<name>.js`
+TDD in the file `date_exercises_<name>.test.js`
+
+## 1) Tell me when Json is coming in the given year
+* Function name `jsonTimes`
+
+
+function jsonTimes(year){
+    let date=new Date(2019,0)
+    let date=new Date(2019,11)
+
+    
+}
+
+
+jsonTimes()
+
+
+
+* Upload it before `17:30 07/31/2019`
+* Receives a single paramater
+* This paramater could be...
+  * a `number` represeting the year to watch for...
+  * a 4 characters `string` represeting the year to watch for...
+  * a timeString in `ISO 8601` representing a date
+  * a date object
+* For any case you need to return an object containing
+  * `times`: the number of times a friday 13 appears in the current year
+  * `dates`: an array of dates when Json is coming (firday 13)
+    * this date must be a string in the form `mm/dd/YYY`
+
+
+# TDD
+
+Write 2 test cases for each exercise.
