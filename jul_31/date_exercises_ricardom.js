09eeb71fc413728c2761c0fd78e3756d7db3d0a3
Ricardo Martin del Campo Loza <ricardom.bts@gmail.com>
2019-07-31 17:55:16 -0500

    ejercicio fechas corregido

--- jul_31/date_exercises_ricardom.js
+++ jul_31/date_exercises_ricardom.js
@@ -2,27 +2,31 @@ function jsonTimes(tiempo) {
 
 
     if (!isNaN(tiempo) && tiempo.toString().length == 4) {
-        console.log("es un año");
-        year = parseInt(tiempo,10);
-        
+        year = parseInt(tiempo, 10);
+
     } else if (!isNaN(tiempo)) {
         var date1 = new Date(tiempo);
         year = date1.getFullYear();
     } else {
-        console.log("es un objeto de fecha");
         year = tiempo.getFullYear();
     }
 
     // console.log(year);
 
     var cuenta = 0;
-    for (var month=0; month<12; month++) {
-        var d = new Date(year,month,13);
-        if(d.getDay() == 5){
-          cuenta++;
-       }
+    var days = [];
+    for (var month = 0; month < 12; month++) {
+        var d = new Date(year, month, 13);
+        if (d.getDay() == 5) {
+            cuenta++;
+            days.push(d.toDateString());
+        }
     }
-    return cuenta;
+    obj = {}
+    obj.visitas = cuenta;
+    obj.dias = days
+
+    return obj;
 
 
 }
