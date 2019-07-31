d085c3c7bebb239af93c8f9f260efd99022b0a28
Marlon Torres <marlont.bts@gmail.com>
2019-07-31 10:12:54 -0500

    actualizacion cp

--- jul_30/cp_exercices_marlont.js
+++ jul_30/cp_exercices_marlont.js
@@ -29,10 +29,30 @@ function google(str){
               name: 'find',
               description: 'Returns the value of the first element in the array that satisfies the provided testing function.\nOtherwise undefined is returned.',
               params:[
-              //{name:'callback', description: }
-              ]
+                {name:'callback', description:'Function to execute on each value in the array.'},
+                {name:'element', description:'The current element being processed in the array.'},
+                {name:'index', description:'Optional.\nThe index of the current element being processed in the array.'},
+                {name:'array', description:'Optional.\nThe array find was called upon.'},
+                {name:'thisArg', description:'Optional.\nOptional object to use as this when executing callback.'}
+              ],
+              returning:'The value of the first element in the array that satisfies the provided testing function. \nOtherwise, undefined is returned.'
           }
       }
+
+      case 'findIndex' : {
+        return {
+            name: 'findIndex',
+            description: 'executes the callback function once for every index 0..length-1 (inclusive) \nin the array until it finds the one where callback returns a truthy value (a value that coerces to true).',
+            params:[
+              {name:'callback', description:'A function to execute on each value in the array until the function returns true, indicating that the satisfying element was found.'},
+              {name:'element', description:'The current element being processed in the array.'},
+              {name:'index', description:'Optional.\nThe index of the current element being processed in the array.'},
+              {name:'array', description:'Optional.\nThe array findIndex was called upon.'},
+              {name:'thisArg', description:'Optional.\nOptional object to use as this when executing callback.'}
+            ],
+            returning:'The index of the first element in the array that passes the test. \nOtherwise, -1.'
+        }
+    }
   }
 }
 
