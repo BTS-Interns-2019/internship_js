2036691e23109a31c62158ec224657bab3f57aaa
unknown <cristianliljon@hotmail.com>
2019-07-31 11:17:55 -0500

    Arreglado al estilo caps

--- jul_31/cp_exercice1_cristian.js
+++ jul_31/cp_exercice1_cristian.js
@@ -1,7 +1,14 @@
 function avoidingFire(...arra){
+    /*console.log(...arra)
     arra = arra.sort((a,b)=> b-a);
-    result = arra.filter( (val, i)=> {return val <=arra[0] });
-    return result.length;
+    result = arra.filter( (val, i)=> { Math.max.apply(val) });
+    console.log(result + "gt")
+    return result.length;*/
+    var result = arra
+    .reduce( (acc, item) => acc.concat(item), [])
+    .filter( (item,i,array) => item === Math.max(...array))
+    .length;//*/
+    return result;
 }
 
-console.log(avoidingFire([1,2,3,4,5,6]));
+console.log(avoidingFire( [1,2,3,4],[2,6],[6] ) );
