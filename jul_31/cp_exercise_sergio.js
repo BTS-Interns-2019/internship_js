9d2b5432176feecfc1f26c34bd9f66c399024186
sergio rivera <telesforociriaco@gmail.com>
2019-07-31 10:57:34 -0500

    Ya funciona cp_exercise_sergio.js 10:58

--- jul_31/cp_exercise_sergio.js
+++ jul_31/cp_exercise_sergio.js
@@ -1,32 +1,44 @@
 function avoidngFire(arrays) {
     let temp = 0;
-    let int = 0;
+    let cont = 1;
     function sacar(params) {
         for (let i = 0; i < params.length; i++) {
+            if(params[i] == temp){
+                cont++;
+            }
             if(params[i] > temp ){
                 temp = params[i];
             }
+            
             
         }
         return temp;
     }
 
-    if(arrays[0] === Array.isArray()){ 
-        Sacar();
-    }else {
-        arrays.forEach(element => {
-            if(element > temp){
-                temp = element;
-            }
-        });
+    for (let k = 0; k < arrays.length; k++) {
+        console.log(arrays[k]);
+        
+        if(Array.isArray(arrays[k])){ 
+            sacar(arrays[k]);
+        }else {
+            arrays.forEach(element => {
+                if(temp === element){cont++;}
+                if(element > temp){
+                    temp = element;
+                }
+            });
+            break;
+        }
     }
 
     // falta
+
+    
     
 
-return int;
+return cont;
 }
 
-console.log(avoidngFire([1,2,3,1,2,1,2,3]));
+console.log(avoidngFire([[1,2,3,1,2,1,3,3,3,2,3],[,2,1,1,2,1,2,1,3],[,3,2,2,1,2,3,1,3,2]]));
 
 
