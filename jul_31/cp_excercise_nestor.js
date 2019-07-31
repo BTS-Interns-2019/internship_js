039a0d8cdb7e9f52e395867ded607c1ff6a3bff7
rabbin <rabbin_cbta@hotmail.com>
2019-07-31 10:37:56 -0500

    unfinish exercise

---
+++ jul_31/cp_excercise_nestor.js
@@ -0,0 +1,17 @@
+function avoidingFire(...arrays) {
+    console.log(arrays);
+    console.log(arrays.length);
+    for(let i = 0; i < arrays.length; i++){
+        if(Array.isArray(arrays[i])){
+            for(let x = 0;){
+                
+            }
+        }
+    }
+    
+    
+    
+    
+
+}
+console.log(avoidingFire([2,2,2,1], [6,9,2], [7,7,1,2], [8,8,1,7,3]));
