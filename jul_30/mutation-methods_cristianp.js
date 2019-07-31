d6eb32a466a0365a84f6cd48720ad0dae05f9a79
cristianliljon <cristianliljon@hotmail.com>
2019-07-31 08:25:13 -0500

    Tarea terminada, 2 pruebas no pasadas, 8 aprobadas

--- jul_30/mutation-methods_cristianp.js
+++ jul_30/mutation-methods_cristianp.js
@@ -1,21 +1,77 @@
 function sort(arre, callback){
     if(Array.isArray(arre))
     {
-        if(callback==null){
-            arre.forEach(()=>{
-                    arre.forEach((valor, i)=>{
-                        if(arre[i] > arre[i+1]){
-                            aux = arre[i];
-                            arre[i] = arre[i+1];
-                            arre[i+1]=aux;
-                        }
-                    })
-                })    
-            return arre;
+        if(callback==undefined){
+        arre.forEach(()=>{
+            arre.forEach((valor, i)=>{
+                    if(arre[i] > arre[i+1]){
+                        aux = arre[i];
+                        arre[i] = arre[i+1];
+                        arre[i+1]=aux;
+                    }
+                })
+            })
         }
+                else{
+                    arre.forEach(()=>{
+                        arre.forEach((valor, ia )=>{
+                            if(callback(arre[ia], arre[ia+1])>0 ){
+                                aux = arre[ia];
+                                arre.splice(ia, 1, arre[ia+1]);
+                                arre.splice(ia + 1,1,aux);
+                                /*arre[i] = arre[i+1];
+                                arre[i+1]=aux;*/
+                            }
+                        })
+                    }) 
+                    
+                }
+        return arre;
     }else{
         return "Favor de ingresar un arreglo";
     }
 }
 
-console.log(sort([10,1,2,3,30,4,40,5,5]));
+function generateSeed(arre){
+    arre2=[];
+    arre.forEach(val=>{
+        arre2.splice(Math.random, 0, val);
+    });
+    arre2.filter(v=> v===undefined)
+    return arre2;
+}
+//letras="abcdefghijklmnopqrst".split('');
+//console.log(generateSeed(letras));
+function encrypt(base,seed,message){
+    dev="";
+    message = message.split("");
+    message.forEach((v)=>{
+        i=base.indexOf(v);
+        if(i>=0){
+            dev+=seed[i];
+        }else{
+            dev+=v;
+        }
+    })
+    return dev;
+}
+
+function decrypt(base, seed, message){
+    dev="";
+    message = message.split("");
+    message.forEach((v)=>{
+        i=seed.indexOf(v);
+        if(i>=0){
+            dev+=base[i];
+        }else{
+            dev+=v;
+        }
+    })
+    return dev;
+}
+/*console.log(sort(["ab","asdf","asd","tr","ax"] ));
+letras="qazwsxedcrfvtgbyhnujmikolpñ".split('');
+console.log(generateSeed(letras));
+console.log(secreto=encrypt(letras, generateSeed(letras), 'hola amigos'));
+console.log(decrypt(letras, generateSeed(letras), secreto));//*/
+module.exports = {sort, generateSeed,encrypt,decrypt};
--- jul_30/mutation-methods_cristianp.test.js
+++ jul_30/mutation-methods_cristianp.test.js
@@ -1,13 +1,15 @@
-const sort = require ("");
+const {sort,generateSeed,
+encrypt,decrypt} = require ("./mutation-methods_cristianp.js");
 
 let originalArray;
 
 //realiza esto antes de cada test
-beforeEaxh(()=>{
+/*beforeEaCh(()=>{
     originalArray=[6,4,21,2];
-})
+})//*/
 
 test('returns an array', () =>{
+    originalArray=[6,4,21,2];
     let result = sort(originalArray);
     expect(Array.isArray(result)).toBe(true);
 })
@@ -23,13 +25,42 @@ test('returns return the same array', () =>{
 })
 
 test('order the array eithout params', () =>{
+    originalArray=[6,4,21,2];
     let control = originalArray.concat();
     sort(originalArray);
     expect(originalArray).toEqual(control.sort());
 })
 
 test('order the array with params', () =>{
+    originalArray=[6,4,21,2];
     let control = originalArray.concat();
     sort(originalArray, (a,b)=>a-b);
     expect(originalArray).toEqual(control.sort());
+})
+test('abiba abajo',()=>{
+    result = sort(originalArray.slice(0), (a,b) => b-a); 
+    expect(result).toEqual(originalArray.sort((a,b) => b-a));
+})
+test('Words nd string',()=>{
+    stringArray=["f","r","de","7","ñ"];
+    let result = sort(stringArray.slice(0));
+    expect(result).toEqual(stringArray.sort());
+})
+
+var letras="qazwsxedcrfvtgbyhnujmikolpñ".split('');
+
+test('prueba para generar seed',()=>{
+    expect(generateSeed(letras).length).toBe(letras.length);
+    expect(generateSeed(letras).sort()).toEqual(letras.sort());
+})
+
+var letras="abcdefg".split('');
+var msj = "hola amigos";
+var secreto = encrypt(letras,generateSeed(letras),msj);
+test('Mensaje encriptado',()=>{
+    expect(secreto.length).toBe(msj.length);
+})
+
+test('Mensaje desencriptado',()=>{
+    expect(decrypt(letras, generateSeed(letras), secreto)).toBe(msj);
 })
