19a2c5309894dc6c12aeb131cc3f17c401ecd4d4
sergio rivera <telesforociriaco@gmail.com>
2019-07-31 18:59:54 -0500

    avanzando date_exercises_Sergio  'modesta y triste mente'

--- jul_31/date_exercises_sergio.js
+++ jul_31/date_exercises_sergio.js
@@ -5,18 +5,18 @@ function jsonTimes(params) {
 
     }
     let dato = params;
-    console.log(dato);
-    console.log(dato.getDay());
+ //   console.log(dato);
+ //   console.log(dato.getDay());
     dato.setMonth(0);
     dato.setDate(1);
     dato.setDate(4);
-    console.log(dato);
+ //   console.log(dato);
     for (let i = 0; i < 52; i++) {
         dato.setDate(dato.getDate()+7);
-        console.log(dato);
+ //       console.log(dato);
         
         if(dato.getDate() == 13){
-            console.log("Aqui viene el jason");
+  //          console.log("Aqui viene el jason");
 
             
         }
@@ -31,4 +31,35 @@ function jsonTimes(params) {
 
 }
 
-console.log(jsonTimes(new Date()));
+// console.log(jsonTimes(new Date()));
+
+
+let obj = new Date("27 July 2018 13:30:00");
+
+
+function toLazyHuman(obj,params) {
+    if(!params){
+        params = Date.now();
+    }
+    console.log(params);
+    params = new Date(params);
+    console.log(params);
+    obj = obj.getTime();
+    console.log(obj);
+    params = params.getTime();
+    console.log(params);
+    let diferencia = params-obj;
+    console.log(diferencia);
+    let unoAño = 31556900000;
+    if(diferencia > unoAño){diferencia -= unoAño}
+    console.log(diferencia);
+    
+    
+    
+
+    
+
+}
+
+console.log(toLazyHuman(obj));
+
