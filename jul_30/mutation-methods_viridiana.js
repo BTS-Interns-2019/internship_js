0df7b18771226d007e9af276ee84442244435f12
ViryDe <virid.bts@gmail.com>
2019-07-30 19:10:09 -0500

    avance actividad en clase mutation methods

--- jul_30/mutation-methods_viridiana.js
+++ jul_30/mutation-methods_viridiana.js
@@ -1,1 +1,19 @@
+var originalArray = [6,4,21,2];
+ 
+function sort(originalArray, callback) {
+    var n, i, k, aux;
+    n = originalArray.length;
+    for (k = 1; k < n; k++) {
+        for (i = 0; i < (n - k); i++) {
+            if (originalArray[i] > originalArray[i + 1]) {
+                aux = originalArray[i];
+                originalArray[i] = originalArray[i + 1];
+                originalArray[i + 1] = aux;
+               
+            }
+        }
+    }
+    return originalArray;
+}
 
+console.log(sort(originalArray))
