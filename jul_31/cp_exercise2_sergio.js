51abff3fa59827391bca1e7b5a811d496bbee02a
sergio rivera <telesforociriaco@gmail.com>
2019-07-31 13:01:18 -0500

    deck : ok, suffle:ok hand: ok ,  jugadas: NOOOO  aun - falta diferenciar las jugadas

---
+++ jul_31/cp_exercise2_sergio.js
@@ -0,0 +1,64 @@
+
+
+let diferentes = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let tipos = ["spades", "diamonds", "clubs", "hearts"]
+
+
+
+function getDeck(suit, number){
+    let cartas = [];
+    for (let i = 0; i < suit.length; i++) {
+        for (let x = 0; x < number.length; x++) {
+            if(!isNaN(number[x])){
+                cartas.push({number: +number[x], suit: suit[i]});
+            }else {
+                cartas.push({number: number[x], suit: suit[i]});
+                }
+            }
+        }
+        
+        console.log(JSON.stringify(cartas, null));
+        return cartas;
+
+    
+}
+
+function shuffle(cartas) {
+    var i = cartas.length, temp, irandom;
+  
+    while (0 !== i) {
+  
+      irandom = Math.floor(Math.random() * i);
+      i -= 1;
+  
+      temp = cartas[i];
+      cartas[i] = cartas[irandom];
+      cartas[irandom] = temp;
+    }
+    console.log(JSON.stringify(cartas, null));
+    return cartas;
+    
+  }
+
+let cartas = getDeck(tipos,diferentes);
+let barajeada = shuffle(cartas);
+
+
+/// MyHand
+function myHand(barajeada) {
+    let mano = {hand:"",cards:[]};
+    for (let i = 0; i < 5; i++) {
+        mano['cards'] = barajeada[i];
+        
+    }
+    
+    for (let k = 0; k < mano['cards'].length; k++) {
+        if(mano['cards']['number'])
+        
+    }
+    return mano;
+
+}
+
+console.log(myHand(barajeada));
+
