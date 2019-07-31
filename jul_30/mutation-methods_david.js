f8047fde4e8737026adf37ff59cebbf6274558ec
silva-david <silvad.bts@gmail.com>
2019-07-31 00:22:34 -0500

    David subio sort

--- jul_30/mutation-methods_david.js
+++ jul_30/mutation-methods_david.js
@@ -1,63 +1,21 @@
-// array = [5,2,9,1];
-// n = array.length;
+// array = [2,3,5,6,1,4];
+array = [2,4,5,1,6,3];
+// array = [1,2,3,4,5,6];
 
-// for (j = 0; j < n; j++) {
-//     if(array[j]>array[j+1]) {
-//         a = array.slice(j,j+1);
-//         console.log(a);
-//         for (i = 0; i < n; i++) {
-//             array.copyWithin(i, i+1, i+2);
-//             array.splice(i+1,1,Number(a));
-//         }
-//     } 
-//     console.log(array);
-// }
+function sort(array) {
+    n = array.length;
+    k = n;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < k; j++) {
+            if(array[j]>array[j+1]) {
+                a = array.slice(j,j+1);
+                array.copyWithin(j, j+1, j+2);
+                array.splice(j+1,1,Number(a));
+            }
+        } 
+        k --;    
+    }
+    return array;
+}
+console.log(sort(array));
 
-//////////////////////////////////
-array = [5,2,6,1];
-if (array[0]>array[1]) {
-    a = array.slice(0,1);
-    console.log(a);
-    array.copyWithin(0, 1, 2);
-    console.log(array);
-    array.splice(1,1,Number(a));
-    console.log(array);
-}
-/////////////////////////////////
-if (array[1]>array[2]) {
-    a = array.slice(1,2);
-    console.log(a);
-    array.copyWithin(1, 2, 3);
-    console.log(array);
-    array.splice(2,1,Number(a));
-    console.log(array);
-} else {
-    a = array.slice(2,3);
-    console.log(a);
-    array.copyWithin(2, 3, 4);
-    console.log(array);
-    array.splice(3,1,Number(a));
-    console.log(array);
-}
-/////////////////////////////////
-if (array[2]>array[1]) {
-
-} else {
-    a = array.slice(1,2);
-    console.log(a);
-    array.copyWithin(1, 2, 3);
-    console.log(array);
-    array.splice(2,1,Number(a));
-    console.log(array);
-}
-/////////////////////////////////
-if (array[1]>array[0]) {
-
-} else {
-    a = array.slice(0,1);
-    console.log(a);
-    array.copyWithin(1, 0, 1);
-    console.log(array);
-    // array.splice(0,1,Number(a));
-    // console.log(array);
-}
