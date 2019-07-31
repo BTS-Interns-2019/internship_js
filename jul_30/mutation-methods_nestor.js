fb560aff43a7800e450c7a5c70010d41de3bea74
rabbin <rabbin_cbta@hotmail.com>
2019-07-30 19:57:14 -0500

    faltan las pruebas

--- jul_30/mutation-methods_nestor.js
+++ jul_30/mutation-methods_nestor.js
@@ -1,23 +1,28 @@
 function sort(arr, callback){
     if(!Array.isArray(arr)){
         return "Its not Array"
-    }
-    for(let i = 0; i < arr.length; i++){
-        var temp = i;
-        for(let x =temp+1; x < arr.length; x++){
-            if(arr[temp].toString() > arr[x].toString()){
-                arr.splice(temp, 0, arr[x]);
-                delete arr[x+1];
-                arr.splice(x+1, 1)
-                temp++
+    }else if(typeof callback == "function"){
+        return arr.sort(callback);
+    }else{
+        for(let i = 0, z = 0; z < arr.length; z++){
+            var temp = i;
+            for(let x =temp+1; x < arr.length; x++){
+                if(arr[temp].toString() > arr[x].toString()){
+                    arr.splice(temp, 0, arr[x]);
+                    delete arr[x+1];
+                    arr.splice(x+1, 1);
+                    temp++
+                }else if(arr[temp].toString() === arr[x].toString()){ 
+                    temp++;
+                }else{temp++}
             }
         }
+        return arr
     }
-    return arr
 }
 console.log([756,34,5,65,34,23,342,1,35,24,45].sort());
-
-console.log(sort([756,34,5,65,34,23,342,1,35,24,45]));
+var test = [756,34,5,65,34,23,342,1,35,24,45]
+console.log(sort(test, (a,b)=> b -a ));
 // var x = [756,34,5,65,34,23,342,1,35,24,45];
 // x.splice(0, 0, x[1]);
 // delete x[2];
