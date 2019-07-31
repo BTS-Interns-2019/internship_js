5239d5089374ca62d7e5a98ce0c00a68e976c244
sergio rivera <telesforociriaco@gmail.com>
2019-07-31 17:34:49 -0500

    solo encuentro los viernres 13 apenas

--- jul_31/date_exercises_sergio.js
+++ jul_31/date_exercises_sergio.js
@@ -1,5 +1,31 @@
 function jsonTimes(params) {
-    let dato = new Date(params);
+    if(typeof params == 'number'){
+        params.toString();
+    }else if(typeof params == 'object'){
+
+    }
+    let dato = params;
+    console.log(dato);
+    console.log(dato.getDay());
+    dato.setMonth(dato.getMonth(0));
+    console.log(dato);
+    for (let i = 0; i < 52; i++) {
+        dato.setDate(dato.getDate()+7);
+        console.log(dato);
+        
+        if(dato.getDate() == 13){
+            console.log("Aqui viene el jason");
+            
+        }
+        
+    }
+    
+    
+    
+    
     
 
+
 }
+
+console.log(jsonTimes(new Date()));
