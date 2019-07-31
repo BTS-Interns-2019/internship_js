93644e1ff0a9beed0ad921ba3b82c6f025985031
unknown <cristianliljon@hotmail.com>
2019-07-31 13:13:06 -0500

    cartas caps

---
+++ jul_31/cp_exercise2_cristianp.js
@@ -0,0 +1,48 @@
+var tipo = ["spades", "diamonds", "clubs", "hearts"];
+var carta = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function getDeck(tipo, carta){
+    cartas = [];
+    tipo.forEach( (tipo)=> {
+        carta.forEach( (card)=> {
+            var obj = {Tipo: tipo, Carta: card};
+			cartas.push(obj);
+        });
+    });
+    //console.log(cartas);
+    return cartas;
+   // return JSON.stringify(cartas, null)
+}
+//console.log(getDeck(tipo, carta));
+var Deck = getDeck(tipo, carta);
+
+function shuffle(desor){
+    var m= desor.length-1;
+    for (var i=m; i>1 ;i--){ 
+        alea=Math.floor(i*Math.random()); 
+        temp=desor[i]; 
+        desor[i]=desor[alea]; 
+        desor[alea]=temp; 
+    }
+    arr = [];
+    for (let i = 0; i <= 4; i++) {
+        arr.push(desor[i]);
+    }
+    //return JSON.stringify(arr, null)
+    return(arr);
+    //return deck;
+}
+//console.log(shuffle(Deck));
+
+function myHand(arr)
+{
+    console.log(JSON.stringify(arr, null));
+    arr.sort((a,b)=>a.Tipo.toString().localeCompare(b.Tipo.toString()));
+    console.log(JSON.stringify(arr, null));
+    arr.sort((a,b)=>a.Carta.toString().localeCompare(b.Carta.toString()));
+    console.log(JSON.stringify(arr, null));
+    
+
+}
+
+myHand(shuffle(Deck))
