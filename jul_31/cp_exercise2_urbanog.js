bb5d4b5a05469e0f42f28f8f08f94ae540a25338
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-07-31 16:50:47 -0500

    Added date exercises

--- jul_31/cp_exercise2_urbanog.js
+++ jul_31/cp_exercise2_urbanog.js
@@ -1,7 +1,7 @@
 let suites = ["spades", "diamonds", "clubs", "hearts"];
 let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let deck = [];
-let hand={};
+let hand = {};
 
 function getDeck(cards, suites) {
 
@@ -19,7 +19,7 @@ function getDeck(cards, suites) {
             // console.log(deck[i])
         }
     }
-return deck;
+    return deck;
 }
 
 console.log(getDeck(cards, suites));
@@ -29,8 +29,8 @@ console.log("Suffuling");
 // }
 function suffle(array) {
 
-    for (let i = 0; i <array.length ; i++) {
-        let rndm= Math.floor(Math.random()*i);
+    for (let i = 0; i < array.length; i++) {
+        let rndm = Math.floor(Math.random() * i);
         let aux = array[i];
         array[i] = array[rndm];
         array[rndm] = aux;
@@ -38,7 +38,23 @@ function suffle(array) {
     return array
 }
 
-function myHand(array){
-
-}
 console.log(suffle(deck));
+
+function myHand(array) {
+    if (Array.isArray(array) || (Array.isArray(array) && array.length > 5 || (Array.isArray(array) && array.length < 5))) {
+        return {
+            mensaje: "Error, solo deben ser 5 cartas"
+        };
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            let hand = {
+                hand: "",
+                cards: []
+            };
+            hand.cards[i] = array[i]
+        }
+        return hand
+    }
+}
+
+console.log(myHand(deck.slice(1,6)));
