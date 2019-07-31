815b791eb325ddd8797f56ccefae052a357e367b
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-07-31 18:59:17 -0500

    progreso de tarea dates

--- jul_31/date_exercises_urbanog.js
+++ jul_31/date_exercises_urbanog.js
@@ -16,5 +16,20 @@ function jsonTimes(year) {
         }
     }
 }
+// console.log(jsonTimes(2019))
 
-console.log(jsonTimes(2019))
+function toLazyHuman(date1,date2) {
+    let dias= 1000*60*60*24;
+    let hors= 1000*60*60;
+    let semana= 1000*60*60*24*7
+    let mes= 1000*60*60*24*7*4
+    if (date2===undefined){
+        date2=Date.now()
+        let milis=date1-date2;
+        console.log(Math.round(milis/dias))
+    }
+}
+
+let dateFuck= new Date(2019,06,13,6);
+let dateFuck2= new Date(2019,05 ,13);
+console.log(toLazyHuman(dateFuck))
