25629fbfa1d6b70a60b862ecaca5f8baaef2ed61
Edgar Peregrino <edgarpere.bts@gmail.com>
2019-07-31 17:35:07 -0500

    date exercises incomplete

---
+++ jul_31/date_exercises_edgar.js
@@ -0,0 +1,28 @@
+function JsonTimes (param) {
+           let year;
+       switch (typeof (param)){
+           case "number":
+               year = param;
+               break;
+            case "string":
+                if (param.length == 4){
+                    year = parseInt(param);
+                } else {
+                       let par = param.split("")
+                    year = par[0];
+                }
+                break;
+            case "object":
+
+                break;
+                       
+       }
+
+
+    }
+    
+
+
+
+param = 2019;
+console.log(JsonTimes (param))
