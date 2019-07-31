dbd0e576842ca7a919d360ad7099676c8cb5b1a6
unknown <cristianliljon@hotmail.com>
2019-07-31 17:35:07 -0500

    progreso de date ecercises

--- jul_31/date_excercises_cristian.js
+++ jul_31/date_excercises_cristian.js
@@ -1,3 +1,40 @@
 function jsonTimes(parameter){
-    
-}
+    a = typeof(parameter);
+    console.log(a);
+    var d;
+    switch (a){
+        case "number":
+                a= parameter;
+            break;
+
+        case "object":
+            a = parameter.getFullYear();
+            break;
+
+        case "string":
+            if(parameter.lenght ==4){
+                a = parseInt(parameter);
+            }else{
+                c = parameter.split("-");
+                a = c[0];
+            }
+            break;
+    }
+    var dates =[];
+    var friday=0;
+    month = 0;
+    do{
+        d = new Date(a, month);
+        d.setDate(13);
+        if(d.getDay()==5){
+            friday++;
+            //data = d.getY
+            dates.push();
+        }
+        month++;
+    }while(month<12);
+    var obj = {friday13:friday}
+    return obj;
+}   
+
+jsonTimes(1);
