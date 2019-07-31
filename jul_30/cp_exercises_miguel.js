0ab394fa0fffaac0bf3c21744a0251d3f6b1995d
Miguel Angel <miguelv.bts@gmail.com>
2019-07-31 10:04:45 -0500

    cp_exercises finished with a little delay

--- jul_30/cp_exercises_miguel.js
+++ jul_30/cp_exercises_miguel.js
@@ -10,7 +10,7 @@ module.exports = {
     login,
     onlyEven,
     google,
-    evenArray
+    evenIndex
 }
 users = [
     {
@@ -31,7 +31,7 @@ users = [
     }
 ];
 function login(arr, name, password){
-    if(arr.isArray==true){
+    if(Array.isArray(arr)==true){
         let resultado = arr.find(user=>user.name === name );
         if(resultado!=undefined){
             if(resultado.name == name && resultado.password == password){
@@ -46,27 +46,26 @@ function login(arr, name, password){
         return false;
     }
 }
-//console.log(login(users,"usuario1","usuario1"));
-//console.log(login(users,"leviatan","dorado"));
-//console.log(login(users,"usuario","contraseña"));
-//console.log(login(users,"usuario2","contraseña"));
 /*
     Given an array, returns an array with even elements if all are even or false if at least one is odd 
     onlyEven(arr){return []|false}
  */
 function onlyEven(arr){
-    let condicion = arr.every(num=> num%2 ==0);
-    if(condicion === true){
-        return arr;
+    if(Array.isArray(arr)==true){
+        if(arr.length<1){
+            return false;
+        }else{
+            let condicion = arr.every(num=> num%2 ==0);
+            if(condicion === true){
+                return arr;
+            }else{
+                return condicion;
+            }
+        }
     }else{
-        return condicion;
+        return false;
     }
-
 }
-//console.log(onlyEven([1,2,3,4,5,6,7,8]));
-//console.log(onlyEven([2,4,6,8,10,12,14,16]));
-//console.log(onlyEven([1,3,5,7,9,11,13,15]));
-//console.log(onlyEven([2,204,192,115,84,44,52,16]));
 /*  Given a string, returns an object with the required params and returning values of the following 
 functions ["find","findIndex","every","some","values","entries","keys"] 
 function google(str){
@@ -80,28 +79,166 @@ function google(str){
 /* Las cadenas de entradas seran "find", "findIndex", "every", "some", "values", "entries", "keys" 
 Va a existir un arreglo con la definicion de los objetos definido ya, y lo que se debe de hacer es que
 lo que mas coincida con la entrada es la definicion que retorna*/
-function google(str){ 
-    let res = [];
-    //find regresa su objeto
-    //findIndex regresa su objeto
-    //every regresa su objeto
-    //some regresa su objeto
-    //values regresa su objeto
-    //entries regresa su objeto
-    //keys regresa su objeto
-    return res;
+queries = [
+    {
+        name:"find",
+        description:"Retorna el primer elemento de un arreglo que cumpla con cierta condicion",
+        params:[
+            {
+                callback:[
+                    {
+                        name:"element", 
+                        description:"Valor actual",
+                        type:"necesario"
+                    },{
+                        name:"index",
+                        description:"Indice del valor actual",
+                        type:"opcional"
+                    },{
+                        name:"array",
+                        description:"Arreglo que esta manejando el metodo",
+                        type:"opcional"
+                    }
+                ]
+            },{
+                name:"thisArg",
+                description:"Parametro de invocacion del callback",
+                type:"opcional"
+            }
+        ],
+        returning: "Valor del primer elemento en el arreglo que satisfaga con la condicion de la funcion, de otro modo undefined"
+    },{
+        name:"findIndex",
+        description:"Retorna el indice del primer elemento de un arreglo que cumpla con cierta condicion",
+        params:[
+            {
+                callback:[
+                    {
+                        name:"element", 
+                        description:"Valor actual",
+                        type:"necesario"
+                    },{
+                        name:"index",
+                        description:"Indice del valor actual",
+                        type:"opcional"
+                    },{
+                        name:"array",
+                        description:"Arreglo que esta manejando el metodo",
+                        type:"opcional"
+                    }
+                ]
+            },{
+                name:"thisArg",
+                description:"Parametro de invocacion del callback",
+                type:"opcional"
+            }
+        ],
+        returning: "Entero de la posicion del arreglo donde se encuentra el elemento, si no lo encuentra imprime -1"
+    },{
+        name:"every",
+        description:"Retorna verdadero si todos los elementos de un arreglo cumplen con cierta condicion",
+        params:[
+            {
+                callback:[
+                    {
+                        name:"element", 
+                        description:"Valor actual",
+                        type:"necesario"
+                    },{
+                        name:"index",
+                        description:"Indice del valor actual",
+                        type:"opcional"
+                    },{
+                        name:"array",
+                        description:"Arreglo que esta manejando el metodo",
+                        type:"opcional"
+                    }
+                ]
+            },{
+                name:"thisArg",
+                description:"Parametro de invocacion del callback",
+                type:"opcional"
+            }
+        ],
+        returning: "Booleano"
+    },{
+        name:"some",
+        description:"Retorna verdadero si alguno de los elementos de un arreglo cumplen con cierta condicion",
+        params:[
+            {
+                callback:[
+                    {
+                        name:"element", 
+                        description:"Valor actual",
+                        type:"necesario"
+                    },{
+                        name:"index",
+                        description:"Indice del valor actual",
+                        type:"opcional"
+                    },{
+                        name:"array",
+                        description:"Arreglo que esta manejando el metodo",
+                        type:"opcional"
+                    }
+                ]
+            },{
+                name:"thisArg",
+                description:"Parametro de invocacion del callback",
+                type:"opcional"
+            }
+        ],
+        returning: "Booleano"
+    },{
+        name:"values",
+        description:"Retorna los valores de cada elemento",
+        returning: "Arreglo iterador"
+    },{
+        name:"entries",
+        description:"Retorna un objeto iterador con la clave y el valor de cada elemento",
+        returning: "Arreglo iterador"
+    },{
+        name:"keys",
+        description:"Retorna las claves de cada elemento",
+        returning: "Arreglo iteador"
+    }
+];
+function google(arr,str){ 
+    if(Array.isArray(arr)==true){
+        //console.log(arr.length);
+        if(arr.length>0){
+            //console.log("a");
+            let res = [];
+            //console.log("n="+arr.find(query=>query.name === str ));
+            if(arr.find(query=>query.name === str )==undefined){
+                return [];
+            }else{
+                res = arr.find(query=>query.name === str );
+                return res;
+            }
+        }else{
+            return [];
+        }
+    }else{
+        return [];
+    }
 }
+console.log(google(queries,"values"));
 //console.log(google("Hola mundo"));  
 /*Given an array of numbers, returns a new array of just the index of even numbers*/
-function evenArray(arr){
-    const evenones = arr.filter(num=>num%2==0);
-    let res = [];
-    for(let i=0; i<evenones.length;i++){
-        res[i] = arr.findIndex(num=>num == evenones[i]);
+function evenIndex(arr){
+    if(Array.isArray(arr)==true){
+        const evenones = arr.filter(num=>num%2==0);
+        let res = [];
+        for(let i=0; i<evenones.length;i++){
+            res[i] = arr.findIndex(num=>num == evenones[i]);
+        }
+        return res;
+    }else{
+        return [];
     }
-    return res;
+    
 }
-//console.log(evenArray([1,2,3,4,5,6,7,8,9]));  
-//console.log(evenArray([2,4,6,8,10,12,14,16])); 
-//console.log(evenArray([1,3,5,7,9,11,13,15])); 
-//console.log(evenArray([2,204,192,115,84,44,52,16])); 
+//console.log(evenIndex([1,2,3,4,5,6,7,8,9]));  
+//console.log(evenIndex([2,4,6,8,10,12,14,16])); 
+//console.log(evenIndex([1,3,5,7,9,11,13,15])); 
+//console.log(evenIndex([2,204,192,115,84,44,52,16])); 
--- jul_30/cp_exercises_miguel.test.js
+++ jul_30/cp_exercises_miguel.test.js
@@ -1,6 +1,7 @@
-const {login,onlyEven,google,evenArray} = require("./cp_exercises_miguel.js"); 
+const {login,onlyEven,google,evenIndex} = require("./cp_exercises_miguel.js"); 
 
 //PRUEBAS
+//PRUEBA DE LOGIN
 beforeEach(()=>{
     let users = [
         {
@@ -18,6 +19,129 @@ beforeEach(()=>{
         },{
             name: "yo",
             password: "notu"
+        }
+    ];
+    let queries = [
+        {
+            name:"find",
+            description:"Retorna el primer elemento de un arreglo que cumpla con cierta condicion",
+            params:[
+                {
+                    callback:[
+                        {
+                            name:"element", 
+                            description:"Valor actual",
+                            type:"necesario"
+                        },{
+                            name:"index",
+                            description:"Indice del valor actual",
+                            type:"opcional"
+                        },{
+                            name:"array",
+                            description:"Arreglo que esta manejando el metodo",
+                            type:"opcional"
+                        }
+                    ]
+                },{
+                    name:"thisArg",
+                    description:"Parametro de invocacion del callback",
+                    type:"opcional"
+                }
+            ],
+            returning: "Valor del primer elemento en el arreglo que satisfaga con la condicion de la funcion, de otro modo undefined"
+        },{
+            name:"findIndex",
+            description:"Retorna el indice del primer elemento de un arreglo que cumpla con cierta condicion",
+            params:[
+                {
+                    callback:[
+                        {
+                            name:"element", 
+                            description:"Valor actual",
+                            type:"necesario"
+                        },{
+                            name:"index",
+                            description:"Indice del valor actual",
+                            type:"opcional"
+                        },{
+                            name:"array",
+                            description:"Arreglo que esta manejando el metodo",
+                            type:"opcional"
+                        }
+                    ]
+                },{
+                    name:"thisArg",
+                    description:"Parametro de invocacion del callback",
+                    type:"opcional"
+                }
+            ],
+            returning: "Entero de la posicion del arreglo donde se encuentra el elemento, si no lo encuentra imprime -1"
+        },{
+            name:"every",
+            description:"Retorna verdadero si todos los elementos de un arreglo cumplen con cierta condicion",
+            params:[
+                {
+                    callback:[
+                        {
+                            name:"element", 
+                            description:"Valor actual",
+                            type:"necesario"
+                        },{
+                            name:"index",
+                            description:"Indice del valor actual",
+                            type:"opcional"
+                        },{
+                            name:"array",
+                            description:"Arreglo que esta manejando el metodo",
+                            type:"opcional"
+                        }
+                    ]
+                },{
+                    name:"thisArg",
+                    description:"Parametro de invocacion del callback",
+                    type:"opcional"
+                }
+            ],
+            returning: "Booleano"
+        },{
+            name:"some",
+            description:"Retorna verdadero si alguno de los elementos de un arreglo cumplen con cierta condicion",
+            params:[
+                {
+                    callback:[
+                        {
+                            name:"element", 
+                            description:"Valor actual",
+                            type:"necesario"
+                        },{
+                            name:"index",
+                            description:"Indice del valor actual",
+                            type:"opcional"
+                        },{
+                            name:"array",
+                            description:"Arreglo que esta manejando el metodo",
+                            type:"opcional"
+                        }
+                    ]
+                },{
+                    name:"thisArg",
+                    description:"Parametro de invocacion del callback",
+                    type:"opcional"
+                }
+            ],
+            returning: "Booleano"
+        },{
+            name:"values",
+            description:"Retorna los valores de cada elemento",
+            returning: "Arreglo iterador"
+        },{
+            name:"entries",
+            description:"Retorna un objeto iterador con la clave y el valor de cada elemento",
+            returning: "Arreglo iterador"
+        },{
+            name:"keys",
+            description:"Retorna las claves de cada elemento",
+            returning: "Arreglo iteador"
         }
     ];
 });
@@ -54,4 +178,98 @@ test("Prueba usuario y contraseña correctos pero no se relacionan entre si logi
 });
 test("Prueba sin nada login", function(){
     expect(login()).toBe(false);
+});
+//PRUEBA DE ONLYEVEN
+test("Prueba de pares onlyeven", function(){
+    expect(onlyEven([32,8,12,62,46,70,54,88])).toEqual([32,8,12,62,46,70,54,88]);
+});
+test("Prueba de nones onlyeven", function(){
+    expect(onlyEven([1,51,33,49,13,25,63,99])).toEqual(false);
+});
+test("Prueba combinada onlyeven", function(){
+    expect(onlyEven([64,55,32,74,21,76,18,65])).toEqual(false);
+});
+test("Prueba cadena pares onlyeven", function(){
+    expect(onlyEven(['18','54','80','46','4','32','84','22','50','24'])).toEqual(['18','54','80','46','4','32','84','22','50','24']);
+});
+test("Prueba cadena combinada pares onlyeven", function(){
+    expect(onlyEven([74,'6','52',82,'60',36,10])).toEqual([74,'6','52',82,'60',36,10]);
+});
+test("Prueba undefined onlyeven", function(){
+    expect(onlyEven([20,12,undefined,54,60,48,80])).toEqual(false);
+});
+test("Prueba arreglo vacio onlyeven", function(){
+    expect(onlyEven([])).toEqual(false);
+});
+test("Prueba vacia onlyeven", function(){
+    expect(onlyEven()).toEqual(false);
+});
+//PRUEBA DE GOOGLE
+test("Prueba verdadera google", function(){
+    result = {
+        name:"findIndex",
+        description:"Retorna el indice del primer elemento de un arreglo que cumpla con cierta condicion",
+        params:[
+            {
+                callback:[
+                    {
+                        name:"element", 
+                        description:"Valor actual",
+                        type:"necesario"
+                    },{
+                        name:"index",
+                        description:"Indice del valor actual",
+                        type:"opcional"
+                    },{
+                        name:"array",
+                        description:"Arreglo que esta manejando el metodo",
+                        type:"opcional"
+                    }
+                ]
+            },{
+                name:"thisArg",
+                description:"Parametro de invocacion del callback",
+                type:"opcional"
+            }
+        ],
+        returning: "Entero de la posicion del arreglo donde se encuentra el elemento, si no lo encuentra imprime -1"
+    };
+    expect(google(queries,"findIndex")).toEqual(result);
+});
+test("Prueba falsa google", function(){
+    expect(google(queries,"url")).toEqual([]);
+});
+test("Prueba sin cadena google", function(){
+    expect(google(queries,"")).toEqual([]);
+});
+test("Prueba arreglo vacio google", function(){
+    expect(google([],"values")).toEqual([]);
+});
+test("Prueba vacia google", function(){
+    expect(google()).toEqual([]);
+});
+//PRUEBA DE EVENINDEX
+test("Prueba todos pares evenindex", function(){
+    expect(evenIndex([72,64,10,58,34,2,14])).toEqual([0,1,2,3,4,5,6]);
+});
+test("Prueba todos nones evenindex", function(){
+    expect(evenIndex([11,55,7,23,89,41,75])).toEqual([]);
+});
+test("Prueba basica 1 evenindex", function(){
+    expect(evenIndex([78,33,59,45,99,87,6])).toEqual([0,6]);
+});
+test("Prueba basica 2 evenindex", function(){
+    expect(evenIndex([33,58,64,85,90,13,1,100])).toEqual([1,2,4,7]);
+});
+test("Prueba cadenas evenindex", function(){
+    expect(evenIndex(['66','11','33','44','99','88','22','55'])).toEqual([0,3,5,6]);
+});
+test("Prueba undefined evenindex", function(){
+    expect(evenIndex([1,2,3,undefined,4,5,6,8])).toEqual([1,4,6,7]);
+});
+test("Prueba arreglo vacio evenindex", function(){
+    expect(evenIndex([])).toEqual([]);
+});
+test("Prueba vacia evenindex", function(){
+    expect(evenIndex()).toEqual([]);
 });
