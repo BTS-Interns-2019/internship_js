83f3e5d51668a3343a60b5c0a9ce4a29e4253307
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-07-31 13:19:24 -0500

    cp ejercicio2 avances

--- jul_31/cp_exercise2_urbanog.js
+++ jul_31/cp_exercise2_urbanog.js
@@ -1,6 +1,7 @@
 let suites = ["spades", "diamonds", "clubs", "hearts"];
 let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let deck = [];
+let hand={};
 
 function getDeck(cards, suites) {
 
@@ -22,10 +23,22 @@ return deck;
 }
 
 console.log(getDeck(cards, suites));
-for (let i = 0; i < deck.length; i++) {
-    console.log(deck[i])
-}
+console.log("Suffuling");
+// for (let i = 0; i < deck.length; i++) {
+//      console.log(deck[i])
+// }
 function suffle(array) {
-    
+
+    for (let i = 0; i <array.length ; i++) {
+        let rndm= Math.floor(Math.random()*i);
+        let aux = array[i];
+        array[i] = array[rndm];
+        array[rndm] = aux;
+    }
+    return array
 }
 
+function myHand(array){
+
+}
+console.log(suffle(deck));
