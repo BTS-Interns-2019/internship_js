3c18a4580f3ad8d34de5483eb6f8bea7f7b02bfc
estefaniacr <estefaniac.bts@gmail.com>
2019-07-31 11:16:32 -0500

    ejercicio con pruebas final

--- jul_31/cp_exercise1_estefania.js
+++ jul_31/cp_exercise1_estefania.js
@@ -1,13 +1,10 @@
-function avoidingFire(arrays) {
-    arr2=Array.from(arrays)
-    console.log(arr2)
-    // arr2 = arr2.flat()
-    max = Math.max.apply(null,arr2)
-    console.log(max)
-    arrays.forEach(element => {
-        if(max === element) {
-        }
-    });
+function avoidingFire(...arr) {
+    var arr2 =arr.reduce(function(a,b) {
+        return a.concat(b);
+      }).filter((item, i, arrMax) => item ===  Math.max(...arrMax)).length
+        return arr2
 }
 
-avoidingFire([2,2,2,1], [6,9,2], [7,7,1,2], [8,8,1,7,3])
+console.log(avoidingFire([2,2,2,1], [6,9,2], [7,7,1,2], [8,8,1,7,3]))
+
+module.exports = avoidingFire;
