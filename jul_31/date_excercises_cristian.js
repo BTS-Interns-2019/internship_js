72a076d210b3be82418ffa47c0164e842ee9a072
unknown <cristianliljon@hotmail.com>
2019-07-31 19:00:07 -0500

    avances de segundo ejercicio

--- jul_31/date_excercises_cristian.js
+++ jul_31/date_excercises_cristian.js
@@ -29,7 +29,7 @@ function jsonTimes(parameter){
         if(d.getDay()==5){
             friday++;
             //data = d.getY
-            dates.push(d.getDay() + "/" + (d.getMonth()+1) + "/" + a);
+            dates.push((d.getMonth()+1).toString().padStart(2, 0) + "/" + d.getDay() + "/" + a);
         }
         month++;
     }while(month<12);
@@ -37,5 +37,34 @@ function jsonTimes(parameter){
                 viernes:dates};
     return obj;
 }   
+//console.log(jsonTimes(2019));
 
-console.log(jsonTimes(2019));
+function toLazyHuman(ob1, ob2){
+    if(ob2==undefined){
+        ob2 = new Date();
+    }
+    var diferencia = ob2 - ob1;
+    //var contdias = Math.round(diferencia/(1000*60*60*24));
+    var secondsToTime = as(diferencia);
+    console.log(diferencia);
+    return secondsToTime;
+}
+a = new Date(1980,00);
+b = new Date(1981,00);
+console.log(toLazyHuman(a,b))
+
+function as(s) {
+
+    function addZ(n) {
+      return (n<10? '0':'') + n;
+    }
+
+    var ms = s % 1000;
+    s = (s - ms) / 1000;
+    var secs = s % 60;
+    s = (s - secs) / 60;
+    var mins = s % 60;
+    var hrs = (s - mins) / 60;
+
+    return  addZ(hrs) + ':' + addZ(mins) + ':' + addZ(secs)+ '.' + addZ(ms);
+  }
