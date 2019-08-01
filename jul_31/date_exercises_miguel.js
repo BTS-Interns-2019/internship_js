48359b5e5a4bf2a88c79a56ff7a498685f38d5ab
Miguel Angel <miguelv.bts@gmail.com>
2019-08-01 00:42:17 -0500

    toLazyHuman function almost finished

--- jul_31/date_exercises_miguel.js
+++ jul_31/date_exercises_miguel.js
@@ -1,3 +1,5 @@
+module.exports = toLazyHuman;
+
 function jsonTimes(t){
     let aux;
     let date = new Date(0);
@@ -25,7 +27,7 @@ function toLazyHuman(todate,fromdate){
     let res = "";
     if(fromdate==undefined){fromdate.now()}
     let dif = todate-fromdate;
-    let rounded = Math.round(dif);
+    let rounded = Math.round(dif/1000);
     let val = Math.abs(rounded);
     let num,rnum,otherrnum;
     //rounded values 1, 2, 5, 10, 20, 30
@@ -35,8 +37,10 @@ function toLazyHuman(todate,fromdate){
     days 30
     months 12
     years ** */
-    if(val>59999 && val< 3600000){//Minute
-        num=val/60000;
+    if(val<60){
+        res=res+"less than 1 minute";
+    }else if(val>59 && val< 3600){//Minute
+        num=val/60;
         rnum = Math.round(num);
         if(rnum==1){
             res=res+rnum+" minute";
@@ -54,8 +58,8 @@ function toLazyHuman(todate,fromdate){
                 }
             }
         }
-    }else if(val>3599999 && val<86400000){//Hour
-        num=val/3600000;
+    }else if(val>3599 && val<86400){//Hour
+        num=val/3600;
         rnum = Math.round(num);
         if(rnum==1){
             res=res+rnum+" hour";
@@ -73,8 +77,8 @@ function toLazyHuman(todate,fromdate){
                 }
             }
         }
-    }else if(val>86399999 && val<2592000000){//Day
-        num=val/86400000;
+    }else if(val>86399 && val<2592000){//Day
+        num=val/86400;
         rnum = Math.round(num);
         if(rnum==1){
             res=res+rnum+" day";
@@ -92,6 +96,46 @@ function toLazyHuman(todate,fromdate){
                 }
             }
         }
+    }else if(val>2591999 && val<31104000) {//Month
+        num=val/2592000;
+        rnum = Math.round(num);
+        if(rnum==1){
+            res=res+rnum+" month";
+        }else{
+            if(rnum==2 || rnum==5 || rnum==10 || rnum==20 || rnum==30){
+                res=res+rnum+" months";
+            }else{
+                otherrnum=letsroundit12(rnum);
+                if(otherrnum.charAt(otherrnum.length-1)=='1'){
+                    res=res+otherrnum+" month";
+                }else if(otherrnum.charAt(otherrnum.length-1)=='y'){
+                    res=res+otherrnum+"ear";
+                }else{
+                    res=res+otherrnum+" months";
+                }
+            }
+        }
+    }else if(val>31103999 && val<933120000){//Year
+        num=val/31104000;
+        rnum = Math.round(num);
+        if(rnum==1){
+            res=res+rnum+" year";
+        }else{
+            if(rnum==2 || rnum==5 || rnum==10 || rnum==20 || rnum==30){
+                res=res+rnum+" years";
+            }else{
+                otherrnum=letsroundit30(rnum);
+                if(otherrnum.charAt(otherrnum.length-1)=='1'){
+                    res=res+otherrnum+" year";
+                }else if(otherrnum.charAt(otherrnum.length-1)=='m'){
+                    res=res+"more than 30 years";
+                }else{
+                    res=res+otherrnum+" years";
+                }
+            }
+        }
+    }else{
+        res=res+"more than 30 years";
     }
     //Positive or negative
     if(rounded<0){
@@ -246,6 +290,44 @@ function letsroundit30(val){
             return "less than 1 m";
         }else{
             return "more than 20";
+        }
+    }
+}
+function letsroundit12(val){
+    let goup, godown;
+    if(val>0 && val<1){
+        return "less than 1";
+    }else if(val>1 && val<2){
+        goup = 2 - val;
+        godown = val - 1;
+        if(goup<godown){
+            return "less than 2";
+        }else{
+            return "more than 1";
+        }
+    }else if(val>2 && val<5){
+        goup = 5 - val;
+        godown = val - 2;
+        if(goup<godown){
+            return "less than 5";
+        }else{
+            return "more than 2";
+        }
+    }else if(val>5 && val<10){
+        goup = 10 - val;
+        godown = val - 5;
+        if(goup<godown){
+            return "less than 10";
+        }else{
+            return "more than 5";
+        }
+    }else if(val>10 && val<12){
+        goup = 12 - val;
+        godown = val - 10;
+        if(goup<godown){
+            return "less than 1 y";
+        }else{
+            return "more than 10";
         }
     }
 }
