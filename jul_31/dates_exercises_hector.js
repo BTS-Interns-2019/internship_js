0a1b05adb9cdaf9014e68b90d7cf5ed72ca1a358
Hector Soto <hectors.bts@gmail.com>
2019-07-31 21:05:20 -0500

    Add lazyHuman function

--- jul_31/dates_exercises_hector.js
+++ jul_31/dates_exercises_hector.js
@@ -25,7 +25,6 @@ function jsonTimes(date) {
 
 function toLazyHuman(date1, date2 = new Date(Date.now())) {
   date1 = new Date(date1);
-  console.log(date1, date2);
   let diff = date1 - date2;
   const isDiffNegative = diff < 0;
   diff = Math.abs(diff);
@@ -37,43 +36,185 @@ function toLazyHuman(date1, date2 = new Date(Date.now())) {
   const diffYears = diffMonths / 12;
 
   const roundedValues = {
-    diffSecs: Math.round(diffSecs),
-    diffMins: Math.round(diffMins),
-    diffHours: Math.round(diffHours),
-    diffDays: Math.round(diffDays),
-    diffMonths: Math.round(diffMonths),
-    diffYears: Math.round(diffYears),
+    diffSecs: {
+      total: Math.floor(diffSecs),
+      roundedTop: false,
+      rounded: false
+    },
+    diffMins: {
+      total: Math.floor(diffMins),
+      roundedTop: false,
+      rounded: false,
+    },
+    diffHours: {
+      total: Math.floor(diffHours),
+      roundedTop: false,
+      rounded: false
+    },
+    diffDays: {
+      total: Math.floor(diffDays),
+      roundedTop: false,
+      rounded: false
+    },
+    diffMonths: {
+      total: Math.floor(diffMonths),
+      roundedTop: false,
+      rounded: false
+    },
+    diffYears: {
+      total: Math.floor(diffYears),
+      roundedTop: false,
+      rounded: false
+    },
   }
 
   for (let diff in roundedValues) {
-    // if (diff < 1 && diff > 0) {
-    //   roundedValues[diff] = 1;
-    // } else if (diff < 2 && diff > 1) {
-    //   roundedValues[diff] = 2;
-    // } else if (diff < 5 && diff > 2) {
-    //   roundedValues[diff] = 5;
-    // } else if (diff < 10 && diff > 5) {
-    //   roundedValues[diff] = 10;
-    // } else if (diff < 20 && diff > 10) {
-    //   roundedValues[diff] = 20;
-    // } else if (diff < 30 && diff > 20) {
-    //   roundedValues[diff] = 30;
-    // }
+    const extraction = roundedValues[diff] >= 10 ? -2 : -1;
+    let lastDigits = Number.parseInt(roundedValues[diff].total.toString().slice(extraction));
 
-    const lastDigit = roundedValues[diff] % 10;
-    if (lastDigit <= 5 && lastDigit > 0) {
-      roundedValues[diff] -= lastDigit;
-    } else if (lastDigit > 5) {
-      roundedValues[diff] += (10 - lastDigit);
+    if (lastDigits < 0.5 && lastDigits > 0) {
+      roundedValues[diff].total -= (lastDigits);
+      roundedValues[diff].roundedTop = false;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits > 0.5 && lastDigits < 1) {
+      roundedValues[diff].total += (1 - lastDigits);
+      roundedValues[diff].roundedTop = true;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits < 1.5 && lastDigits > 1) {
+      roundedValues[diff].total -= (lastDigits - 1);
+      roundedValues[diff].roundedTop = false;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits > 1.5 && lastDigits < 2) {
+      roundedValues[diff].total += (2 - lastDigits);
+      roundedValues[diff].roundedTop = true;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits < 3.5 && lastDigits > 2) {
+      roundedValues[diff].total -= (lastDigits - 2);
+      roundedValues[diff].roundedTop = false;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits < 5 && lastDigits > 3.5) {
+      roundedValues[diff].total += (5 - lastDigits);
+      roundedValues[diff].roundedTop = true;
+      roundedValues[diff].rounded = true;     
+    } else if (lastDigits < 7.5 && lastDigits > 5) {
+      roundedValues[diff].total -= (lastDigits - 5);
+      roundedValues[diff].roundedTop = false;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits < 10 && lastDigits > 5) {
+      roundedValues[diff].total += (10 - lastDigits);
+      roundedValues[diff].roundedTop = true;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits < 15 && lastDigits > 10) {
+      roundedValues[diff].total -= (lastDigits - 15);
+      roundedValues[diff].roundedTop = false;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits > 15 && lastDigits < 20) {
+      roundedValues[diff].total += (20 - lastDigits);
+      roundedValues[diff].roundedTop = true;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits < 25 && lastDigits > 20) {
+      roundedValues[diff].total -= (lastDigits - 20);
+      roundedValues[diff].roundedTop = false;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits > 25 && lastDigits < 30) {
+      roundedValues[diff].total += (30 - lastDigits);
+      roundedValues[diff].roundedTop = true;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits > 30 && lastDigits < 75) {
+      roundedValues[diff].total -= (lastDigits - 30);
+      roundedValues[diff].roundedTop = false;
+      roundedValues[diff].rounded = true;
+    } else if (lastDigits >= 75) {
+      roundedValues[diff].total += (100 - lastDigits);
+      roundedValues[diff].roundedTop = true;
+      roundedValues[diff].rounded = true;
+    } else {
+      roundedValues[diff].rounded = false;
     }
   }
 
-  if (roundedValues.diffYears)
+  let roundingString;
+  let timeUnit;
+  let resultString;
 
-  console.log(roundedValues);
+  if (roundedValues.diffYears.total) {
+    timeUnit = roundedValues.diffYears.total > 1 ? 'years' : 'year';
+    resultString = `${roundedValues.diffYears.total} ${timeUnit}`;
+    if (roundedValues.diffYears.rounded) {
+      roundingString = roundedValues.diffYears.roundedTop ? 'less than' : 'more than';
+      resultString = roundingString + ' ' + resultString;
+    }
+  } else if (roundedValues.diffMonths.total) {
+    if (roundedValues.diffMonths.total > 6 && roundedValues.diffMonths.total <= 12) {
+      timeUnit = 'year';
+      resultString = `${1} ${timeUnit}`;
+    } else {
+      timeUnit = roundedValues.diffMonths.total > 1 ? 'months' : 'month';
+      resultString = `${roundedValues.diffMonths.total} ${timeUnit}`;
+    }
+
+    if (roundedValues.diffMonths.rounded) {
+      roundingString = roundedValues.diffMonths.roundedTop ? 'less than' : 'more than';
+      resultString = roundingString + ' ' + resultString;
+    }
+  } else if (roundedValues.diffDays.total) {
+    if (roundedValues.diffDays.total > 15 && roundedValues.diffDays.total <= 31) {
+      timeUnit = 'month';
+      resultString = `${1} ${timeUnit}`;
+    } else {
+      timeUnit = roundedValues.diffDays.total > 1 ? 'days' : 'day';
+      resultString = `${roundedValues.diffDays.total} ${timeUnit}`;
+    }
+
+    if (roundedValues.diffDays.rounded) {
+      roundingString = roundedValues.diffDays.roundedTop ? 'less than' : 'more than';
+      resultString = roundingString + ' ' + resultString;
+    }
+  } else if (roundedValues.diffHours.total) {
+    if (roundedValues.diffHours.total > 15 && roundedValues.diffHours.total <= 24) {
+      timeUnit = 'day';
+      resultString = `${1} ${timeUnit}`;
+    } else {
+      timeUnit = roundedValues.diffHours.total > 1 ? 'hours' : 'hour';
+      resultString = `${roundedValues.diffHours.total} ${timeUnit}`;
+    }
+
+    if (roundedValues.diffHours.rounded) {
+      roundingString = roundedValues.diffHours.roundedTop ? 'less than' : 'more than';
+      resultString = roundingString + ' ' + resultString;
+    }
+  } else if (roundedValues.diffMins.total) {
+    if (roundedValues.diffMins.total > 30 && roundedValues.diffMins.total <= 60) {
+      timeUnit = 'hour';
+      resultString = `${1} ${timeUnit}`;
+    } else {
+      timeUnit = roundedValues.diffMins.total > 1 ? 'minutes' : 'minute';
+      resultString = `${roundedValues.diffMins.total} ${timeUnit}`;
+    }
+
+    if (roundedValues.diffMins.rounded) {
+      roundingString = roundedValues.diffMins.roundedTop ? 'less than' : 'more than';
+      resultString = roundingString + ' ' + resultString;
+    }
+  } else if (roundedValues.diffSecs.total) {
+    if (roundedValues.diffSecs.total > 30 && roundedValues.diffSecs.total <= 60) {
+      timeUnit = 'minute';
+      resultString = `${1} ${timeUnit}`;
+    } else {
+      timeUnit = roundedValues.diffSecs.total > 1 ? 'seconds' : 'second';
+      resultString = `${roundedValues.diffSecs.total} ${timeUnit}`;
+    }
+
+    if (roundedValues.diffSecs.rounded) {
+      roundingString = roundedValues.diffSecs.roundedTop ? 'less than' : 'more than';
+      resultString = roundingString + ' ' + resultString;
+    }
+  } else {
+    return 'Right now';
+  }
+
+  return isDiffNegative ? resultString + ' ago' : 'in ' + resultString;
 }
-
-toLazyHuman(new Date() - (24*3600*1000));
 
 module.exports = {
   jsonTimes,
