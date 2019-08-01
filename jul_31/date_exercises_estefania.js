cae2309a2820a9ed775eaab9ef3f72d11d48a15f
estefaniacr <estefaniac.bts@gmail.com>
2019-07-31 23:38:53 -0500

    hasta aqui mi avance actual en este ejercicio :(

--- jul_31/date_exercises_estefania.js
+++ jul_31/date_exercises_estefania.js
@@ -31,8 +31,47 @@ function toLazyHuman(toDate, fromDate) {
     date2= new Date(fromDate)
     console.log(date2)
     var diferencia = date1 - date2
-    return diferencia
+    
+    let segundos = diferencia/1000;
+    let minutos = segundos/60;
+    let horas = minutos/60;
+    let dias = horas/24;
+    let meses = dias/30;
+    let años = dias/365
+    
+    var rounded = {
+    segundos: {
+      total: Math.floor(segundos),
+      roundedTop: false,
+      rounded: false
+    },
+    minutos: {
+      total: Math.floor(minutos),
+      roundedTop: false,
+      rounded: false,
+    },
+    horas: {
+      total: Math.floor(horas),
+      roundedTop: false,
+      rounded: false
+    },
+    dias: {
+      total: Math.floor(dias),
+      roundedTop: false,
+      rounded: false
+    },
+    meses: {
+      total: Math.floor(meses),
+      roundedTop: false,
+      rounded: false
+    },
+    años: {
+      total: Math.floor(años),
+      roundedTop: false,
+      rounded: false
+    },
+  }
+  return diferencia
+
 }
-console.log(toLazyHuman("2019-07-31T12:00:00.000", "2019-07-31T12:01:00.000"))
-
-
+console.log(toLazyHuman("2019-07-31T12:00:00.000", "2019-07-31T12:01:00.000"));
