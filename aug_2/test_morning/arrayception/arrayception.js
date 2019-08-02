bc2f334e75b0d50b27eb712373522a558e36ead2
Ricardo Martin del Campo Loza <ricardom.bts@gmail.com>
2019-08-02 11:41:27 -0500

    copia de seguridad

--- aug_2/test_morning/arrayception/arrayception.js
+++ aug_2/test_morning/arrayception/arrayception.js
@@ -1,12 +1,27 @@
 
-/**
+/**Ricardo Martín del Campo Loza
  * arrayGenerator
  * @param {int} number
  * @returns {array}
  */
-function arrayGenerator(number) {
 
-  return [];
+function arrayGenerator(number) {
+  let finalArray = [];
+  arrayBase = [];
+  let j = 0;
+  for (i = 0; i <= number; i++) {
+    arrayBase.push(i.toString());
+  }
+  finalArray = arrayBase.slice();;
+while( j <= number){    //poner loop despues de las pruebas
+    finalArray[0]=arrayBase;
+    arrayBase = finalArray.slice()
+    j++
 }
+  return finalArray;
+}
+
+
+console.log(arrayGenerator(8))
 
 module.exports = arrayGenerator;
--- aug_2/test_morning/arrayception/arrayception.test.js
+++ aug_2/test_morning/arrayception/arrayception.test.js
@@ -5,7 +5,7 @@ describe('Arrayception', () => {
 
   test('Last number is n', ()=> {
     const result = arrayGenerator(8);
-    expect(result[result.length-1]).toBe(8);
+    expect(result[result.length-1]).toBe("8");
   });
 
   test('First element is an array', ()=> {
@@ -16,6 +16,11 @@ describe('Arrayception', () => {
   test('The penultimate element of the 15th inner array is number-1', ()=> {
     const result = arrayGenerator(79);
     const array15th = result[0][0][0][0][0][0][0][0][0][0][0][0][0][0][0];
-    expect(array15th[array15th.length-2]).toBe(78);
+    expect(array15th[array15th.length-2]).toBe("78");
   });
 });
+
+test('number is integer', ()=> {
+  expect(arrayGenerator("a")).toBe(false);
+});
+
