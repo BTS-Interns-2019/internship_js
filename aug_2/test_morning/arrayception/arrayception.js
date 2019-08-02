e1548ff6b897702fe9ee54fcdc50f432bce43c8b
Edgar Peregrino <edgarpere.bts@gmail.com>
2019-08-02 14:07:29 -0500

    tests exercises

--- aug_2/test_morning/arrayception/arrayception.js
+++ aug_2/test_morning/arrayception/arrayception.js
@@ -1,12 +1,24 @@
 
-/**
+/** Edgar David Peregrino Jimenez
  * arrayGenerator
  * @param {int} number
  * @returns {array}
  */
 function arrayGenerator(number) {
-
-  return [];
+    array = [];
+    for (i = 0; i < number; i++){
+      array [i] = i;
+     }
+     array.fill(array,0,1);
+      function fills (arr){ 
+      if (typeof (fills (arr[0])) == "object" ){
+        arr[0].fill(arr,0,1);
+      }
+    return arr;
+     }         
+  return fills (array, number);
 }
 
+
+console.log (arrayGenerator(3))
 module.exports = arrayGenerator;
