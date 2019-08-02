e60120ad0c5c5515f79fd9b3688a586868b9e188
Miguel <miguelv.bts@gmail.com>
2019-08-02 15:39:23 -0500

    Reverse function is finished

--- aug_2/test_afternoon/reverse/reverse.js
+++ aug_2/test_afternoon/reverse/reverse.js
@@ -1,5 +1,23 @@
 function reverse(str) {
-  return str;
+  if(typeof str == 'string'){
+    let div=str.split(' ');
+    let word = div.filter((e)=>{return e!=""});
+    let sentence = word.map((actual, i)=>{
+      if(i%2==0){
+        return actual;
+      }else{
+
+        return actual.split("").reverse().join("");
+      }
+    });
+    let res=sentence.join(" ");
+    return res;
+  }else{
+    return '';
+  }
+  //return str;
 }
 
 module.exports = reverse;
+
+console.log(reverse('     e ss a    d   e wrr   d r  s e  '));
--- aug_2/test_afternoon/reverse/reverse.test.js
+++ aug_2/test_afternoon/reverse/reverse.test.js
@@ -11,3 +11,11 @@ test('reverse words1', function(){
 test('reverse words2', function(){
   expect(reverse(' this is a second test :O ')).toEqual('this si a dnoces test O:');
 })
+
+test('reverse words3', function(){
+  expect(reverse('   this   word looks   like  was   been  separated too    much    ')).toEqual('this drow looks ekil was neeb separated oot much');
+})
+
+test('reverse none', function(){
+  expect(reverse()).toEqual('');
+})
