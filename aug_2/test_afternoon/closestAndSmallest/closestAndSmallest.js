b1ec05a11e5cef90d3e837bf49c2a0b27c028c2e
Miguel <miguelv.bts@gmail.com>
2019-08-02 18:59:27 -0500

    Closest and Smallest function was almost finished, only just one test fail, just one, 1

--- aug_2/test_afternoon/closestAndSmallest/closestAndSmallest.js
+++ aug_2/test_afternoon/closestAndSmallest/closestAndSmallest.js
@@ -1,6 +1,52 @@
 function closestAndSmallest(str) {
-  //Hardcoded for first test :)
-  return [[2, 4, 2000], [4, 0, 103]];
+  let value = [];
+  let div = str.split(' ');
+  let weight = div.map((actual)=>{
+    return actual.split('').reduce((base,numero)=>{
+      base = parseInt(base);
+      numero = parseInt(numero);
+      return base= base+numero;
+    });
+  });
+  let dif;
+  let smallest = [];
+  //console.log("weight = "+weight);
+  for(let i=0;i<weight.length;i++){
+    for(let j=0;j<weight.length;j++){
+      if(i!=j){
+        let aux=weight[i]-weight[j];
+        if(dif==undefined && aux>-1){
+          //console.log("Inicio");
+          dif=aux;
+          smallest[0]=weight[i];
+          smallest[1]=weight[j];
+        }else{
+          //console.log(i+" - "+j+" "+dif+"<"+aux);
+          if(aux<dif && aux>-1){
+            dif=aux;
+            smallest[0]=weight[i];
+            smallest[1]=weight[j];
+          }
+        }
+      }
+    }
+  }
+  if(smallest[0]>smallest[1]){
+    smallest.sort();
+  }
+  let d=div.concat();
+  let w=weight.concat();
+  value=smallest.map((actual)=>{
+      let j=w.indexOf(actual);
+      let r= [actual,j,parseInt(d[j])];
+      d.splice(j,1,null);
+      w.splice(j,1,null);
+    return r;
+  })
+  return value;
 }
 
 module.exports = closestAndSmallest;
+
+//console.log(closestAndSmallest('103 123 4444 99 2000'));//[[2, 4, 2000], [4, 0, 103]] w =[4,6,16,18,2]
+console.log(closestAndSmallest('101 200 101'));//[[2, 0, 101], [2, 1, 200]] w =[4,6,16,18,2]
--- aug_2/test_afternoon/closestAndSmallest/closestAndSmallest.test.js
+++ aug_2/test_afternoon/closestAndSmallest/closestAndSmallest.test.js
@@ -9,3 +9,39 @@ test('test closest1', function(){
   expect(response[1][1]).toEqual(0);
   expect(response[1][2]).toEqual(103);
 })
+test('test closest2', function(){
+  let response = closestAndSmallest('101 200 101');
+  expect(response[0][0]).toEqual(2);
+  expect(response[0][1]).toEqual(0);
+  expect(response[0][2]).toEqual(101);
+  expect(response[1][0]).toEqual(2);
+  expect(response[1][1]).toEqual(1);
+  expect(response[1][2]).toEqual(200);
+})
+test('test closest3', function(){
+  let response = closestAndSmallest('80 71 62 53');
+  expect(response[0][0]).toEqual(8);
+  expect(response[0][1]).toEqual(0);
+  expect(response[0][2]).toEqual(80);
+  expect(response[1][0]).toEqual(8);
+  expect(response[1][1]).toEqual(1);
+  expect(response[1][2]).toEqual(71);
+})
+test('test closest4', function(){
+  let response = closestAndSmallest('444 2000 445 544');
+  expect(response[0][0]).toEqual(13);
+  expect(response[0][1]).toEqual(2);
+  expect(response[0][2]).toEqual(445);
+  expect(response[1][0]).toEqual(13);
+  expect(response[1][1]).toEqual(3);
+  expect(response[1][2]).toEqual(554);
+})
+test('test closest5', function(){
+  let response = closestAndSmallest('239382 162 254765 182 485944 468751 49780 108 54');
+  expect(response[0][0]).toEqual(9);
+  expect(response[0][1]).toEqual(1);
+  expect(response[0][2]).toEqual(162);
+  expect(response[1][0]).toEqual(9);
+  expect(response[1][1]).toEqual(7);
+  expect(response[1][2]).toEqual(108);
+})
