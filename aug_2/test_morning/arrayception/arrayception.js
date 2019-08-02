f7f7428a53859bfe3f495fdcb7fd2aac44def288
unknown <luiscarlosg.bts@gmail.com>
2019-08-02 10:58:01 -0500

    arrayception luis

--- aug_2/test_morning/arrayception/arrayception.js
+++ aug_2/test_morning/arrayception/arrayception.js
@@ -1,12 +1,32 @@
 
 /**
+ * Luis Carlos Gallegos Rodriguez
  * arrayGenerator
  * @param {int} number
  * @returns {array}
  */
+
 function arrayGenerator(number) {
 
-  return [];
+  var inner = 0;
+  let ar = generator()
+  function generator() {
+    let arr = []
+    for (let i = 0; i <= number; i++) {
+      arr.push(i.toString())
+      inner++;
+      if (inner < 999) {
+        arr[0] = generator(number)
+      }
+    }
+    return arr
+
+  }
+  return ar;
 }
 
+const result = arrayGenerator(79);
+const array15th = result[0][0][0][0][0][0][0][0][0][0][0][0][0][0][0];
+console.log(array15th[array15th.length - 2])
+//console.log(result)
 module.exports = arrayGenerator;
--- aug_2/test_morning/arrayception/arrayception.test.js
+++ aug_2/test_morning/arrayception/arrayception.test.js
@@ -3,19 +3,31 @@ const arrayGenerator = require('./arrayception');
 
 describe('Arrayception', () => {
 
-  test('Last number is n', ()=> {
+  test('Last number is n', () => {
     const result = arrayGenerator(8);
-    expect(result[result.length-1]).toBe(8);
+    expect(result[result.length - 1]).toBe("8");
   });
 
-  test('First element is an array', ()=> {
+  test('First element is an array', () => {
     const result = arrayGenerator(8);
     expect(Array.isArray(result[0])).toBe(true);
   });
 
-  test('The penultimate element of the 15th inner array is number-1', ()=> {
+  test('The penultimate element of the 15th inner array is number-1', () => {
     const result = arrayGenerator(79);
     const array15th = result[0][0][0][0][0][0][0][0][0][0][0][0][0][0][0];
-    expect(array15th[array15th.length-2]).toBe(78);
+    expect(array15th[array15th.length - 2]).toBe("78");
   });
+});
+
+test('posicion 0 es diferente a posicion 0 0', () => {
+  const result = arrayGenerator(8);
+  const array0th = result[0]
+  const array1th = result[0][0]
+  expect(array0th == array1th).toBe(false);
+});
+
+test('Second element is not an array', () => {
+  const result = arrayGenerator(8);
+  expect(Array.isArray(result[1])).toBe(false);
 });
