7a4495d093cb430b4eab234eb9e2118ec4d5cd80
Hector Soto <hectors.bts@gmail.com>
2019-08-02 16:27:33 -0500

    Add exercise Cashier

--- aug_2/test_afternoon/cashier/cashier.js
+++ aug_2/test_afternoon/cashier/cashier.js
@@ -1,19 +1,112 @@
 function cashMeOut(input, quantity, cashierStatus) {
-  //Hardcoded for first test :)
-  response = {
-    bill500: 0,
-    bill200: 2,
-    bill100: 0,
-    bill50: 1,
-    bill20: 1,
-    coin50: 0,
-    coin20: 1,
-    coin10: 0,
-    coin5: 1,
-    coin2: 2,
-    coin1: 0,
+  if (typeof input !== 'object') {
+    return 'First input is not an object';
   }
-  return response;
+
+  if (typeof quantity !== 'number') {
+    return 'Second input is not a number';
+  }
+
+  if (typeof cashierStatus !== 'object') {
+    return 'Third input is not an object'
+  }
+
+  //Hardcoded for first test :)
+  let bills = [];
+  let coins = [];
+
+  let inputTotal = 0;
+  let cashierTotal = 0;
+  for (let money in input) {
+    const inputMoney = Number.parseInt(money.match(/(\d+)/));
+    for (let i = 0; i < input[money]; i++) {
+      inputTotal += inputMoney;
+    }
+  }
+
+  for (let money in cashierStatus) {
+    const cashierMoney = Number.parseInt(money.match(/(\d+)/));
+    if (money.startsWith('b')) {
+      bills.push([cashierMoney, cashierStatus[money]])
+    } else if (money.startsWith('c')) {
+      coins.push([cashierMoney, cashierStatus[money]]);
+    }
+    for (let i = 0; i < cashierStatus[money]; i++) {
+      cashierTotal += cashierMoney;
+    }
+  }
+
+  if (cashierTotal < quantity) {
+    return 'Not enough money to return';
+  }
+
+  if (quantity < inputTotal) {
+    return 'Not enough money for payment';
+  }
+
+  if (quantity === inputTotal) {
+    return 0;
+  }
+
+  let billsUsed = [];
+  let coinsUsed = [];
+  if (quantity > inputTotal) {
+    billsUsed = bills.map(bill => {
+      let amount = Number.parseInt(bill[0]);
+      bill[0] = 'bill' + bill[0];
+      let quantityOfBill = Number.parseInt(bill[1]);
+      bill[1] = 0;
+      for (let i = 0; i < quantityOfBill; i++) {
+        if (quantity - amount >= 0) {
+          quantity -= amount;
+          bill[1]++;
+        } else {
+          return bill;
+        }
+      }
+    }).filter(bill => bill);
+
+    if (quantity > 0) {
+      coinsUsed = coins.map(coin => {
+        let amount = Number.parseInt(coin[0]);
+        coin[0] = 'coin' + coin[0];
+        let quantityOfCoin = Number.parseInt(coin[1]); 
+        coin[1] = 0;
+        for (let i = 0; i < quantityOfCoin; i++) {
+          if (quantity - amount >= 0) {
+            quantity -= amount;
+            coin[1]++;            
+          } else {
+            return coin;
+          }
+        }
+      }).filter(coin => coin);
+    }
+  }
+
+  for (let money in cashierStatus) {
+    cashierStatus[money] = 0;
+  }
+
+  billsUsed.forEach(bill => {
+    for (let money in cashierStatus) {
+      if (money === bill[0]) {
+        cashierStatus[money] = bill[1];
+      }
+    }
+  });
+
+  if (coinsUsed.length > 0) {
+    coinsUsed.forEach(coin => {
+      for (let money in cashierStatus) {
+        if (money === coin[0]) {
+          cashierStatus[money] = coin[1];
+        }
+      }
+    });
+  }
+
+  return cashierStatus;
 }
 
 module.exports = cashMeOut;
--- aug_2/test_afternoon/cashier/cashier.test.js
+++ aug_2/test_afternoon/cashier/cashier.test.js
@@ -1,6 +1,78 @@
 const cashMeOut = require('./cashier');
 
-test('test cashMeOut 1', function(){
+test('Input 1 is not an object', function(){
+  obj1 = 'Not an object';
+  number = 501;
+  obj2 = {
+    bill500: 200,
+    bill200: 200,
+    bill100: 200,
+    bill50: 200,
+    bill20: 1,
+    coin50: 200,
+    coin20: 2000,
+    coin10: 2000,
+    coin5: 2000,
+    coin2: 2000,
+    coin1: 2000,
+  };
+
+  let response = cashMeOut(obj1, number, obj2);
+  expect(response).toBe('First input is not an object');
+});
+test('Input 2 is not a number', function(){
+  obj1 = {
+    bill500: 2,
+    bill200: 0,
+    bill100: 0,
+    bill50: 0,
+    bill20: 0,
+    coin50: 0,
+    coin20: 0,
+    coin10: 0,
+    coin5: 0,
+    coin2: 0,
+    coin1: 0,
+  };
+  number = '501';
+  obj2 = {
+    bill500: 200,
+    bill200: 200,
+    bill100: 200,
+    bill50: 200,
+    bill20: 1,
+    coin50: 200,
+    coin20: 2000,
+    coin10: 2000,
+    coin5: 2000,
+    coin2: 2000,
+    coin1: 2000,
+  };
+
+  let response = cashMeOut(obj1, number, obj2);
+  expect(response).toBe('Second input is not a number');
+});
+test('Input 3 is not an object', function(){
+  obj1 = {
+    bill500: 2,
+    bill200: 0,
+    bill100: 0,
+    bill50: 0,
+    bill20: 0,
+    coin50: 0,
+    coin20: 0,
+    coin10: 0,
+    coin5: 0,
+    coin2: 0,
+    coin1: 0,
+  };
+  number = 501;
+  obj2 = 'Not an object';
+
+  let response = cashMeOut(obj1, number, obj2);
+  expect(response).toBe('Third input is not an object');
+});
+test('Response says "Not enough money for payment" when input 2 is less than the sum of input 1', function(){
   obj1 = {
     bill500: 2,
     bill200: 0,
@@ -30,16 +102,157 @@ test('test cashMeOut 1', function(){
   };
 
   let response = cashMeOut(obj1, number, obj2);
-  expect(typeof response).toEqual("object");
-  expect(response.bill500).toEqual(0)
-  expect(response.bill200).toEqual(2)
-  expect(response.bill100).toEqual(0)
-  expect(response.bill50).toEqual(1)
-  expect(response.bill20).toEqual(1)
-  expect(response.coin50).toEqual(0)
-  expect(response.coin20).toEqual(1)
-  expect(response.coin10).toEqual(0)
-  expect(response.coin5).toEqual(1)
-  expect(response.coin2).toEqual(2)
-  expect(response.coin1).toEqual(0)
-})
+  expect(response).toBe('Not enough money for payment');
+});
+test('Response is 0 when input 2 is the same as the sum of input 1', function(){
+  obj1 = {
+    bill500: 2,
+    bill200: 0,
+    bill100: 0,
+    bill50: 0,
+    bill20: 0,
+    coin50: 0,
+    coin20: 0,
+    coin10: 0,
+    coin5: 0,
+    coin2: 0,
+    coin1: 0,
+  };
+  number = 1000;
+  obj2 = {
+    bill500: 200,
+    bill200: 200,
+    bill100: 200,
+    bill50: 200,
+    bill20: 1,
+    coin50: 200,
+    coin20: 2000,
+    coin10: 2000,
+    coin5: 2000,
+    coin2: 2000,
+    coin1: 2000,
+  };
+
+  let response = cashMeOut(obj1, number, obj2);
+  expect(response).toBe(0);
+});
+test('Response has more bills used than coins', function(){
+  obj1 = {
+    bill500: 2,
+    bill200: 0,
+    bill100: 0,
+    bill50: 0,
+    bill20: 0,
+    coin50: 0,
+    coin20: 0,
+    coin10: 0,
+    coin5: 0,
+    coin2: 0,
+    coin1: 0,
+  };
+  number = 1500;
+  obj2 = {
+    bill500: 200,
+    bill200: 200,
+    bill100: 200,
+    bill50: 200,
+    bill20: 1,
+    coin50: 200,
+    coin20: 2000,
+    coin10: 2000,
+    coin5: 2000,
+    coin2: 2000,
+    coin1: 2000,
+  };
+
+  let response = cashMeOut(obj1, number, obj2);
+  expect(response).toStrictEqual({
+    bill500: 3,
+    bill200: 0,
+    bill100: 0,
+    bill50: 0,
+    bill20: 0,
+    coin50: 0,
+    coin20: 0,
+    coin10: 0,
+    coin5: 0,
+    coin2: 0,
+    coin1: 0,
+  });
+});
+test('Response has more bills used than coins and it\'s the least amount of items possible', function(){
+  obj1 = {
+    bill500: 2,
+    bill200: 0,
+    bill100: 0,
+    bill50: 0,
+    bill20: 0,
+    coin50: 0,
+    coin20: 0,
+    coin10: 0,
+    coin5: 0,
+    coin2: 0,
+    coin1: 0,
+  };
+  number = 1500;
+  obj2 = {
+    bill500: 200,
+    bill200: 200,
+    bill100: 200,
+    bill50: 200,
+    bill20: 1,
+    coin50: 200,
+    coin20: 2000,
+    coin10: 2000,
+    coin5: 2000,
+    coin2: 2000,
+    coin1: 2000,
+  };
+
+  let response = cashMeOut(obj1, number, obj2);
+  expect(response).toStrictEqual({
+    bill500: 3,
+    bill200: 0,
+    bill100: 0,
+    bill50: 0,
+    bill20: 0,
+    coin50: 0,
+    coin20: 0,
+    coin10: 0,
+    coin5: 0,
+    coin2: 0,
+    coin1: 0,
+  });
+});
+test('Response says "Not enough money to return" when there isn\'t enough money in the cashier to return', function(){
+  obj1 = {
+    bill500: 2,
+    bill200: 0,
+    bill100: 0,
+    bill50: 0,
+    bill20: 0,
+    coin50: 0,
+    coin20: 0,
+    coin10: 0,
+    coin5: 0,
+    coin2: 0,
+    coin1: 0,
+  };
+  number = 1500;
+  obj2 = {
+    bill500: 0,
+    bill200: 0,
+    bill100: 0,
+    bill50: 0,
+    bill20: 0,
+    coin50: 0,
+    coin20: 0,
+    coin10: 0,
+    coin5: 0,
+    coin2: 0,
+    coin1: 0,
+  };
+
+  let response = cashMeOut(obj1, number, obj2);
+  expect(response).toBe('Not enough money to return');
+});
