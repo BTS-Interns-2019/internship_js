e45975b69fb088b77aeab59db0d0df1082e68885
estefaniacr <estefaniac.bts@gmail.com>
2019-08-02 11:41:08 -0500

    avance

--- aug_2/test_morning/xmaslist/xmas_list.js
+++ aug_2/test_morning/xmaslist/xmas_list.js
@@ -1,3 +1,4 @@
+//Estefanía Cano
 
 /**
  * xmasList
@@ -5,9 +6,63 @@
  * @param {Array} toys
  * @returns {Array}
  */
-function xmasList(kidsList, toys) {
 
-  return [];
+ xmasList(kids =`Cristian Fernandez, 12, 100
+  Sergio Piña , 4 , 50
+  Carlos Munguia, 6, 92
+  `, toys = [
+    {
+      description: 'Car',
+      quantity: 2,
+    }
+  ])
+function xmasList(kidsList, toys) {
+  let result = [];
+  let kids = kidsList.split("\n");
+  for(kid in kids){
+    let recorte = kids[kid].split(",");
+    
+    let toys = [
+    {
+    description: 'Car',
+    quantity: 2,
+    }
+    ];
+  if(recorte.length==3){
+    let theName = recorte[0];  
+    let theAge = recorte[1];
+    let theScore = recorte[2];
+    let entrega = {
+      name: theName,
+      age: theAge,
+      score: theScore,
+      gift: toys.description
+    } 
+
+    console.log(kids)
+    let exi = Object.getOwnPropertyNames(result);
+    if(exi.indexOf(gift)==-1){
+      newKid={}
+      switch(gift){
+          case "toy":
+          tipo= "toy";
+          break;
+          case "coal":
+          tipo = "coal";
+          break;
+         
+      }
+      newKid.name=theName;
+     
+    }else{
+      output[estado].people.push(persona);
+    }
 }
+}
+
+return output;
+}
+  }}
+
 
 module.exports = xmasList;
