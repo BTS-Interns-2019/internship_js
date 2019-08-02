184bc37f786a72f1fa5b2c86f9f5ffee0edb7d45
unknown <luiscarlosg.bts@gmail.com>
2019-08-02 18:14:34 -0500

    ahi nomas quedo

--- aug_2/test_afternoon/smallest/smallest.js
+++ aug_2/test_afternoon/smallest/smallest.js
@@ -1,6 +1,26 @@
-function smallest(str) {
-  //Hardcoded for first test :)
-  return [126235, 2, 0];
+function smallest(num) {
+  let min = Number.MAX_VALUE
+  let ind
+  let to
+  let split = num.toString()
+  for(let i=0;i<split.length;i++){
+   let car = split[i]
+   let def = split.slice(0,i) + split.slice(i+1,split.length)
+   for(let j=0;j<split.length;j++){
+     let ori = split[j]
+     //split[i] = ori
+     //split[j] = car
+     let num = def.slice(0,j)+car+def.slice(j,def.length)
+     //
+     if(num<min){
+       min = parseInt(num);
+       ind = i
+       to = j
+     }
+    // split[i] = car
+     //split[j] = ori
+   }
+  }
+  return [min, ind, to];
 }
-
 module.exports = smallest;
--- aug_2/test_afternoon/smallest/smallest.test.js
+++ aug_2/test_afternoon/smallest/smallest.test.js
@@ -1,8 +1,22 @@
 const smallest = require('./smallest');
 
-test('test closest1', function(){
-  let response = smallest('103 123 4444 99 2000');
+test('test smallest1', function(){
+  let response = smallest(261235);
   expect(response[0]).toEqual(126235);
   expect(response[1]).toEqual(2);
   expect(response[2]).toEqual(0);
+})
+
+test('test smallest2', function(){
+  let response = smallest(83425825);
+  expect(response[0]).toEqual(28342585);
+  expect(response[1]).toEqual(6);
+  expect(response[2]).toEqual(0);
+})
+
+test('test smallest3', function(){
+  let response = smallest(911111);
+  expect(response[0]).toEqual(111119);
+  expect(response[1]).toEqual(0);
+  expect(response[2]).toEqual(5);
 })
