14132658543652fa39c725400d01b39f27708440
Marlon Torres <marlont.bts@gmail.com>
2019-08-02 11:08:07 -0500

    guardar cambios para bajar tictactoe

--- aug_2/test_morning/arrayception/arrayception.js
+++ aug_2/test_morning/arrayception/arrayception.js
@@ -1,3 +1,4 @@
+//Marlon Esteban Torres Huerta
 
 /**
  * arrayGenerator
@@ -5,8 +6,16 @@
  * @returns {array}
  */
 function arrayGenerator(number) {
-
-  return [];
+  var arr = []
+  for(var i=0; i<=number; i++){
+    arr.push(i.toString());
+  }
+  if(arreglo.length == 9){
+    return arr;
+  }
+  var arreglo = [arr.splice(0,1,arrayGenerator(number)), arr];
+  console.log(arreglo);
+  return arr;
 }
 
 module.exports = arrayGenerator;
