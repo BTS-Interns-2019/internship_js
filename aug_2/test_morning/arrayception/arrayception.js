2ecc6c3d1a8de00b64c84e5e6d253480733996d5
silva-david <silvad.bts@gmail.com>
2019-08-02 10:38:26 -0500

    Commit para bajar test corregidos de master - David

--- aug_2/test_morning/arrayception/arrayception.js
+++ aug_2/test_morning/arrayception/arrayception.js
@@ -1,12 +1,24 @@
-
+// David Martin Silva Cruz
 /**
  * arrayGenerator
  * @param {int} number
  * @returns {array}
  */
-function arrayGenerator(number) {
 
-  return [];
+number = 3;
+arrayGenerator(3);
+
+function arrayGenerator(number) {
+  array = []
+  for (i = 0; i <= number; i++) {
+    if (i == 0) {
+      array[0] = arrayGenerator(number);
+    } else {
+      array[i] = String(i);
+    }
+    console.log(array);
+  }
+  return array
 }
 
 module.exports = arrayGenerator;
