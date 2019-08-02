3363480be8550e186161ed57803f7ac3599440da
BTS JM <jm@bts-mx-jm-mac-2.local>
2019-08-02 17:00:02 -0500

    fix smallest.test

--- aug_2/test_afternoon/smallest/smallest.test.js
+++ aug_2/test_afternoon/smallest/smallest.test.js
@@ -1,7 +1,7 @@
 const smallest = require('./smallest');
 
 test('test closest1', function(){
-  let response = smallest('103 123 4444 99 2000');
+  let response = smallest(261235);
   expect(response[0]).toEqual(126235);
   expect(response[1]).toEqual(2);
   expect(response[2]).toEqual(0);
