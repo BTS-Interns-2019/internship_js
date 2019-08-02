90d49c353f265b8266a64500a43302e61cddc688
Miguel <miguelv.bts@gmail.com>
2019-08-02 17:26:43 -0500

    Smallest function is finished

--- aug_2/test_afternoon/smallest/smallest.js
+++ aug_2/test_afternoon/smallest/smallest.js
@@ -1,6 +1,48 @@
-function smallest(str) {
-  //Hardcoded for first test :)
-  return [126235, 2, 0];
+function smallest(number) {
+  if(number === undefined)return [];
+  let str;
+  if(typeof number === "string"){
+    if(number == "")return [];
+    str=number;
+  }else{
+    str=number.toFixed();
+  }
+  let smallest = parseInt(str);
+  let begin = 0, end = 0;
+  for(let i=0;i<str.length;i++){
+    for(let f=0;f<str.length;f++){
+      if(i!=f){
+        //console.log("i = "+i+" f = "+f);
+        //console.log("Valor = "+str);
+        let div = str.split("");
+        let aux1 = div[i];
+        //console.log("aux1 = "+aux1);
+        //console.log("div = "+div.join());
+        let aux2 = div.concat();
+        aux2.splice(i,1);//Quita elemento
+        //console.log("aux2 = "+aux2.join());
+        let aux = aux2.concat();
+        aux.splice(f,0,aux1);//Coloca elemento
+        //console.log("aux = "+aux);
+        aux = aux.join("");
+        //console.log("aux = "+aux);
+        let n = parseInt(aux);
+        //console.log("NUMERO = "+n);
+        //console.log(smallest+">"+n);
+        if(n<smallest){
+          smallest=n;
+          begin = i;
+          end = f;
+        }
+      }
+    }
+  }
+  return [smallest,begin,end];
+  //Regresar menor indice tomado e indice colocado [126235, 2, 0];
 }
+
+/*console.log(smallest(261235));//126235
+console.log(smallest(83425825));
+console.log(smallest(911111));*/
 
 module.exports = smallest;
--- aug_2/test_afternoon/smallest/smallest.test.js
+++ aug_2/test_afternoon/smallest/smallest.test.js
@@ -1,8 +1,29 @@
 const smallest = require('./smallest');
 
 test('test closest1', function(){
-  let response = smallest('103 123 4444 99 2000');
+  let response = smallest(261235);
   expect(response[0]).toEqual(126235);
   expect(response[1]).toEqual(2);
   expect(response[2]).toEqual(0);
+})
+test('test closest2', function(){
+  expect(smallest(83425825)).toEqual([28342585, 6, 0]);
+})
+test('test closest3', function(){
+  expect(smallest(911111)[2]).toEqual(5);
+})
+test('test closest string', function(){
+  expect(smallest('911111')).toEqual([111119, 0, 5]);
+})
+test('test closest just one', function(){
+  expect(smallest(8)).toEqual([8,0,0]);
+})
+test('test closest empty', function(){
+  expect(smallest('')).toEqual([]);
+})
+test('test closest zero', function(){
+  expect(smallest(0)).toEqual([0,0,0]);
+})
+test('test closest none', function(){
+  expect(smallest()).toEqual([]);
 })
