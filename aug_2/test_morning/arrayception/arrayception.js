f329cc6114c7c0af02daee1af2ffb112df4e002b
Marlon Torres <marlont.bts@gmail.com>
2019-08-02 11:12:55 -0500

    d

--- aug_2/test_morning/arrayception/arrayception.js
+++ aug_2/test_morning/arrayception/arrayception.js
@@ -1,21 +1,11 @@
-//Marlon Esteban Torres Huerta
-
 /**
  * arrayGenerator
  * @param {int} number
  * @returns {array}
  */
 function arrayGenerator(number) {
-  var arr = []
-  for(var i=0; i<=number; i++){
-    arr.push(i.toString());
-  }
-  if(arreglo.length == 9){
-    return arr;
-  }
-  var arreglo = [arr.splice(0,1,arrayGenerator(number)), arr];
-  console.log(arreglo);
-  return arr;
+
+  return [];
 }
 
 module.exports = arrayGenerator;
