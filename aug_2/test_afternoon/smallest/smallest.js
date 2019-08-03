3780c286a336068ad48e41a39a851631f983ac9a
emmanuelr.bts@gmail.com <emmanuel.lv.97@gmail.com>
2019-08-02 19:00:13 -0500

    s

--- aug_2/test_afternoon/smallest/smallest.js
+++ aug_2/test_afternoon/smallest/smallest.js
@@ -1,6 +1,47 @@
-function smallest(str) {
-  //Hardcoded for first test :)
-  return [126235, 2, 0];
+function smallest(num) {
+try{
+  if (typeof(num)!=="number") throw new Error(message="Requiere un number")
+  let numbers=[];
+  num=num.toString().split("");
+  i=menor(num);
+  let minimo=parseInt(num[i]);
+  let primero;
+  for (let j = 0; j < num.length; j++) {
+    val=parseInt(num[j]);
+    if(val>minimo){
+      primero=j;
+      break;
+    }
+  }
+  valorPrimero=num[primero]; 
+  arr3=JSON.parse(JSON.stringify(num)) 
+  arr3.splice(i,1);
+  arr3.splice(primero,1,valPrimero);
+
+  if(primero==0){
+    num.splice(i,1);
+    num.unshift(minimo)
+  }
+  else{
+    num.splice(i,1,num[primero]);
+    num.splice(primero,1,minimo);
+  }
+  return [parseInt(arr2.join("")), i, 0]
+}catch(e){
+  return e.message;
 }
+}
+function menor(num2){
+  let minimo, k, l=num2.length;
+  for (let j = 0; j < l; j++) {
+    minimo=Math.min(...num2);
+    k=num2.lastIndexOf(minimo.toString());
+    if(k!=0)return k;
+    else num2=num2.slice(1);
+  }
+  return 0;
+}
+console.log(smallest(91111))
+//console.log(menor("12345".toString().split("")));
 
 module.exports = smallest;
--- aug_2/test_afternoon/smallest/smallest.test.js
+++ aug_2/test_afternoon/smallest/smallest.test.js
@@ -6,3 +6,16 @@ test('test closest1', function(){
   expect(response[1]).toEqual(2);
   expect(response[2]).toEqual(0);
 })
+test('tests examples', function(){
+  let pruebas=[
+    [261235,[126235, 2, 0]],
+    [83425825,[28342585, 6, 0]],
+    [9111111,[1111119, 0, 6]]
+  ]
+  pruebas.forEach((prueba,i) => {
+      expect(JSON.stringify(smallest(prueba[0]))).toBe(JSON.stringify(prueba[1]));
+  });
+})
+test('Another type of value', function(){
+  expect(smallest("hola")).toBe("Requiere un number");
+})
