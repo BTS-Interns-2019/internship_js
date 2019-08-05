0896413b1c1a3666d900cb19f567da4e558ce8e4
silva-david <silvad.bts@gmail.com>
2019-08-05 10:37:02 -0500

    Conflicto

--- aug_2/test_afternoon/closestAndSmallest/closestAndSmallest.js
+++ aug_2/test_afternoon/closestAndSmallest/closestAndSmallest.js
@@ -7,19 +7,20 @@
 
 function closestAndSmallest(str) {
   str = str.split(' ');
+
   w = [];
-  str.forEach((e, i, array) => w[i] = e.split('').reduce(function(a, b){
-    return Number(a) + Number(b); // Método forEach para calcular el 'weight' de cada número de la cadena  y los almacena en el arreglo w
-  }, 0));
+  str.forEach( (elemento, i, array) => w[i] = elemento.split('').reduce(function(acumulador, elemento){
+    return Number(acumulador) + Number(elemento); // Método forEach para calcular el 'weight' de cada número de la cadena  y los almacena en el arreglo w
+  }, 0) );
 
   wsorted = w.slice(); // Copia del arreglo w ('weight' de cada número)
-  wsorted.sort((a,b)=>a-b); // Arreglo copia con los weight's de mayor a menor
+  wsorted.sort( (a,b)=>a-b ); // Arreglo copia con los weight's de menor a mayor
 
   index = [];
-  for (let i = 0; i < wsorted.length; i++) {
+  for (let i = 0; i < w.length; i++) {
     for (let j = 0; j < w.length; j++) {
       if (wsorted[i] == w[j]) {
-        index[i] = j;  // Indice asociado al arreglo con los weight's de mayor a menor
+        index[i] = j;  // Indice asociado al arreglo 'w' de los elementos de 'wsorted'
       }
     } 
   }
@@ -28,7 +29,7 @@ function closestAndSmallest(str) {
   str.forEach( (e, i, array) => array[i] = Number(e) ) // Convierte cada número de la cadena de string a entero
   if (result) {
     a = [w[0], 0, str[0]]; // Resultado cuando todos los weight's son iguales
-    b = [w[0], 1, str[1]];
+    b = [w[1], 1, str[1]];
   } else {
     a = [wsorted[0], index[0], str[index[0]]]; // Resultado cuando no hay weight's repetidos
     b = [wsorted[1], index[1], str[index[1]]];
@@ -36,6 +37,7 @@ function closestAndSmallest(str) {
 
   ya:
   for (let i = 0; i < w.length; i++) {
+    
     for (let j = 0; j < w.length; j++) {
       if (w[i] == w[j] && i!=j ) {
         a = [w[i], i, str[i]]; // Resultado cuando uno o más weight's se repiten
@@ -44,7 +46,6 @@ function closestAndSmallest(str) {
       }
     } 
   }
-   
   return [a, b];
 }
 
