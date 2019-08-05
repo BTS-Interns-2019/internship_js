e5b79dc0ab7f716cd8d7c1f0617acc6c6c745a77
rabbin <rabbin_cbta@hotmail.com>
2019-08-05 13:40:22 -0500

    creating testing

--- project1/tres-mosqueteros_js/8-queens_tres-mosqueteros.test.js
+++ project1/tres-mosqueteros_js/8-queens_tres-mosqueteros.test.js
@@ -1,2 +1,30 @@
 const queenProblem = require("./8-queens_tres-mosqueteros.js");
-console.log(JSON.stringify(queenProblem(4)));
+// console.log(JSON.stringify(queenProblem(4)));
+// Test basic Cases
+test('array', () => {
+    let result = queenProblem(4, [0,1])
+    expect(Array.isArray(result)).toBe(true);
+})
+test('array length of 4', () => {
+    let result = queenProblem(4)
+    expect(result.length).toBe(4);
+})
+test('array length of 20', () => {
+    let result = queenProblem(20)
+    expect(result.length).toBe(20);
+})
+test('length 4, x 0, y 1', function(){
+    let result = queenProblem(4, [0,1]);
+    expect(result[0]).toEqual([0,  1,  0,  0]);
+    expect(result[1]).toEqual([0,  0,  0,  1]);
+    expect(result[2]).toEqual([1,  0,  0,  0]);
+    expect(result[3]).toEqual([0,  0,  1,  0]);
+})
+test('length 4, x 0, y 0', function(){
+    let result = queenProblem(4, [0,0]);
+    expect(result[0]).toEqual([0,  0,  0,  0]);
+    expect(result[1]).toEqual([0,  0,  0,  0]);
+    expect(result[2]).toEqual([0,  0,  0,  0]);
+    expect(result[3]).toEqual([0,  0,  0,  0]);
+})
+    
