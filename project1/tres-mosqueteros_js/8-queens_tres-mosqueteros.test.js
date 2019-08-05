3868a0d2d68769880352539a8132da99dce522db
rabbin <rabbin_cbta@hotmail.com>
2019-08-05 14:07:09 -0500

    undone test case

--- project1/tres-mosqueteros_js/8-queens_tres-mosqueteros.test.js
+++ project1/tres-mosqueteros_js/8-queens_tres-mosqueteros.test.js
@@ -11,21 +11,21 @@ test('array length of 20', () => {
     let result = queenProblem(20, [0,1])
     expect(result.length).toBe(20);
 })
-test('length 4, x 0, y 1', function(){
+test('size of mesh 4, x 0, y 1', function(){
     let result = queenProblem(4, [0,1]);
     expect(result[0]).toEqual([0,  1,  0,  0]);
     expect(result[1]).toEqual([0,  0,  0,  1]);
     expect(result[2]).toEqual([1,  0,  0,  0]);
     expect(result[3]).toEqual([0,  0,  1,  0]);
 })
-test('length 4, x 0, y 0', function(){
+test('size of mesh 4, x 0, y 0', function(){
     let result = queenProblem(4, [0,0]);
     expect(result[0]).toEqual([0,  0,  0,  0]);
     expect(result[1]).toEqual([0,  0,  0,  0]);
     expect(result[2]).toEqual([0,  0,  0,  0]);
     expect(result[3]).toEqual([0,  0,  0,  0]);
 })
-test('length 4, without cordinates', function(){
+test('size of mesh 4, without cordinates', function(){
     let result = queenProblem(4);
     expect(result[0]).toEqual([0,  1,  0,  0]);
     expect(result[1]).toEqual([0,  0,  0,  1]);
@@ -35,5 +35,14 @@ test('length 4, without cordinates', function(){
 test('array length of 4, without cordinates', () => {
     let result = queenProblem(4)
     expect(result.length).toBe(4);
+})
+test('size of mesh 4, cordinates out of mesh', function(){
+    let result = queenProblem(4, [0,10]);
+    expect(result).toBe("Coordenadas fuera del talbero");
+})
+test('size of mesh 4, cordinates out of mesh', function(){
+    let result = queenProblem(4, [0,4]);
+    expect(result).toBe("Coordenadas fuera del talbero");
+
 })
     
