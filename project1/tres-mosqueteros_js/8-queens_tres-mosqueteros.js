38e4de401f238788b5ff234cee7d0387503dcfc4
emmanuelr.bts@gmail.com <emmanuel.lv.97@gmail.com>
2019-08-05 11:44:43 -0500

    push

--- project1/tres-mosqueteros_js/8-queens_tres-mosqueteros.js
+++ project1/tres-mosqueteros_js/8-queens_tres-mosqueteros.js
@@ -1,11 +1,60 @@
 function queenProblem(size, coordinates) {
-    return [
-        [ 0,  1,  0,  0],
-        [ 0,  0,  0,  1],
-        [ 1,  0,  0,  0],
-        [ 0,  0,  1,  0]
-    ];
+    return(print_board(place_next_queen(size, size)))
+
+
 }
+    var iterations = 0
+
+    var print_board = function (columns) {
+        const board=[];
+        let n = columns.length, row = 0, col = 0
+        while (row < n) {
+            const arrRow=[];
+            while (col < n) {
+                arrRow.push(columns[row] === col ? 1 : 0)
+                col++
+            }    
+            board.push(arrRow);
+            col = 0
+            row++
+        }
+        return board;
+    }
+
+    var has_conflict = function (columns) {
+      var len = columns.length, last = columns[len - 1], previous = len - 2
+
+      while (previous >= 0) {
+        if (columns[previous] === last) return true
+        if (last - (len - 1) === columns[previous] - previous) return true
+        if (last + (len - 1) === columns[previous] + previous) return true
+        previous--
+      }
+
+      return false
+    }
+
+    var place_next_queen = function (total, queens, columns) {
+      if (queens === 0) return columns
+      columns = columns || []
+
+      for (var column = 0; column < total; column++) {
+        columns.push(column)
+        iterations++
+        if (!has_conflict(columns) &&
+            place_next_queen(total, queens - 1, columns)) {
+          return columns
+        }
+        columns.pop(column)
+      }
+
+      return null
+    }
+
+    
+    console.log('\niterations: ', iterations)
+
+
 
 
 module.exports=queenProblem;
--- project1/tres-mosqueteros_js/8-queens_tres-mosqueteros.test.js
+++ project1/tres-mosqueteros_js/8-queens_tres-mosqueteros.test.js
@@ -1,2 +1,2 @@
 const queenProblem = require("./8-queens_tres-mosqueteros.js");
-console.log( queenProblem());
+console.log(JSON.stringify(queenProblem(4)));
