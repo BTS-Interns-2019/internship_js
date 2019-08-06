cce1376c6547b2789f9cb3669b13248bca892acc
Erick Vazquez <erickbardo.bts@gmail.com>
2019-08-06 00:02:30 -0500

    Script y Test

--- project1/lumier_js/lumier.test.js
+++ project1/lumier_js/lumier.test.js
@@ -2,13 +2,13 @@ const queenProblem = require("./lumier.js")
 
 describe("Data types", () =>{ 
   test("Output is Array", () =>{
-      expect(Array.isArray(queenProblem(4, [0,0]))).toBe(true)
+      expect(Array.isArray(queenProblem(4, [2,0]))).toBe(true)
   });
   test("Is an object array", () =>{
-    expect(typeof queenProblem(5,[1,1])).toBe("object")
+    expect(typeof queenProblem(4,[2,0])).toBe("object")
   });
   test("Typeof of 'object' typeof is a string", () =>{
-    expect(typeof typeof queenProblem(5,[1,1])).toBe("string")
+    expect(typeof typeof queenProblem(4,[2,0])).toBe("string")
   });
 
 });
@@ -25,15 +25,13 @@ describe("Data types", () =>{
     test("Not calculatable size requirement", ()=>{
       let failSize = 0
       const error = () =>{
-        queenProblem(failSize, [0,0])
-        throw new TypeError 
-        
+        queenProblem(failSize, [0,0]) 
       }
-      expect(error).toThrow(TypeError)
+      expect(error).toThrow(RangeError)
     })
     test("Theres no solution on size (4, [0,0])",()=>{
       const noSol = () =>{
-        queenProblem(4, [0,0])
+        queenProblem(4, [2,0])
         throw new TypeError();
       }
       expect(noSol).toThrow(TypeError)
