a63036875bf4ba38b90e52e510962387b5662f81
ViryDe <virid.bts@gmail.com>
2019-08-05 19:14:40 -0500

    actualizacion

--- project1/Las_reynas/Las_reynas.test.js
+++ project1/Las_reynas/Las_reynas.test.js
@@ -5,24 +5,4 @@ test('validando si es un array:',()=>{
     expect(Array.isArray(result).toBe(true))
 })
 
-test('Crear una matriz de parametros correctos',()=>{
-    let result = queenProblem(6,[0,1])
-    expect(result.length).toBe(6)
-})
-
-test('El tamaño de la matriz es de 8, x0, y0',()=>{
-let result = queenProblem(8,[0,0]);
-expect(result[0]).toEqual([0,  0,  0,  0, 0,  0,  0]);
-expect(result[1]).toEqual([0,  0,  0,  0, 0,  0,  0]);
-expect(result[2]).toEqual([0,  0,  0,  0, 0,  0,  0]);
-expect(result[3]).toEqual([0,  0,  0,  0, 0,  0,  0]);
-expect(result[4]).toEqual([0,  0,  0,  0, 0,  0,  0]);
-expect(result[5]).toEqual([0,  0,  0,  0, 0,  0,  0]);
-expect(result[6]).toEqual([0,  0,  0,  0, 0,  0,  0]);
-expect(result[7]).toEqual([0,  0,  0,  0, 0,  0,  0]);
-
-})
-
-test('El array es un objeto:',()=>{
-    expect(typeof queenProblem(4,[0,1])),toBe('string')
-})
+test('')
