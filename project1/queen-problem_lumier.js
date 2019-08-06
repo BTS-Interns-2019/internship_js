5f6d98b382b8c5f39b06ffc5ff5222159b74d056
unknown <luiscarlosg.bts@gmail.com>
2019-08-05 23:46:44 -0500

    v2

--- project1/queen-problem_lumier.js
+++ project1/queen-problem_lumier.js
@@ -6,7 +6,7 @@ function queenProblem(size, ini) {
                 if (board[i][j] == 1) {
                     board[i][j] = 2
                     for (let fila_columna = 0; fila_columna < size; fila_columna++) {//revisa fila y columna
-                        if (board[fila_columna][j] == 1 || board[i][fila_columna] == 1){ board[i][j] = 1;return false;}
+                        if (board[fila_columna][j] == 1 || board[i][fila_columna] == 1) { board[i][j] = 1; return false; }
                     }
                     //revisa diagonales
                     for (let h = i, v = j; h < size && v < size;) {//diagonal 1
@@ -37,67 +37,72 @@ function queenProblem(size, ini) {
         return true;
     }
 
+    function intentarColocar(y) {
+        if (y == size) {
+            y = 0
+        }
+        if(y==-1){
+            y=size-1
+        }
+         if (y == [ini[1]]) {
+            for (let i = 0; i < size; i++) {
+                board[i] = [];
+                for (let j = 0; j < size; j++) {
+                    board[i][j] = 0
+                }
+            }
+            return board;
+        }
+        
+            //console.log("columan: "+y)
+            let bandera = false;
+            let exc = size
+            for (let i = 0; i < size; i++) {
+             if(board[i][y]==1){
+                exc = i;
+                board[i][y]=0
+                colocados--;
+             }
+            }
+            for(let i=0;i<size;i++){
+                if(i!=exc){
+                    board[i][y] = 1
+                    if(verificar()){
+                        bandera = true;
+                        colocados++
+                       // console.log(board)
+                    }else{
+                        board[i][y] = 0
+                    }
+                }
+            }
+            if(bandera){
+                //console.log("puestas "+colocados)
+                if(colocados==size){
+                    return board
+                }else{
+                    intentarColocar(y+1)
+                }
+            }else{
+                intentarColocar(y-1)
+            }
+
+        
+    }
+   
     for (let i = 0; i < size; i++) {
         board[i] = [];
         for (let j = 0; j < size; j++) {
             board[i][j] = 0
         }
     }
-    console.log(board)
     board[ini[0]][ini[1]] = 1
-    let colocados = 1
-    let intentos = 0
-    colocar(ini[0] + 2, ini[0] + 1)
-
-    function colocar(x, y) {
-        intentos++
-        if (x == size) {
-            x = 0
-        }
-        if (y >= size) {
-            y = 0
-        }
-        if (y == ini[1]) {
-            return false;
-        }
-        board[x][y] = 1
-        console.log(board)
-        if (!verificar()) {
-            console.log(x + " " + y + " false " + intentos)
-            board[x][y] = 0
-            if (intentos < size) {
-                colocar(x + 1, y)
-            } else {
-                let ya = y - 1
-                if (ya < 0) ya = size - 1
-                let xa
-                for (let i = 0; i < size; i++) {
-                    if (board[i][ya] == 1) {
-                        xa = i
-                        board[i][ya] = 0
-                        break;
-                    }
-                }
-                intentos=0
-                colocar(xa + 1, ya)
-            }
-            console.log(board)
-        } else {
-            //verificar dio true
-            console.log(x + " " + y + " true")
-            colocados++;
-            if (colocados < size) {
-                intentos = 0
-                colocar(x + 1, y + 1)
-            } else {
-                return board
-            }
-
-        }
-
-    }
+    let colocados = 1;
+    intentarColocar(ini[1] + 1)
+    return board;
 
 
 }
 
-queenProblem(4, [0, 0])
+
+queenProblem(4, [2, 0])
