1e42a5771d594cece09af063fc4b3c490825b091
Hector Soto <hectors.bts@gmail.com>
2019-08-07 14:01:12 -0500

    Refactoring kSum

--- project2/filthy_acts_for_a_reasonable_price/kSum.js
+++ project2/filthy_acts_for_a_reasonable_price/kSum.js
@@ -1,40 +1,38 @@
 function kSum(strNum1, strNum2) {
-    if ( strNum1.match(/[^0-9]/g) || strNum2.match(/[^0-9]/g))
-      return [strNum1, strNum2];
+  // Check if input has anything other than digits 0-9
+  if (strNum1.match(/[^0-9]/g) || strNum2.match(/[^0-9]/g)) {
+    return [strNum1, strNum2];
+  }
 
-    l1 = strNum1.length; // Longitud de la cadena strNum1
-    l2 = strNum2.length; // Longitud de la cadena strNum2
+  // Add zeros to the beginning of the string with smaller length to match the length of the other one
+  if (strNum1.length < strNum2.length) {
+    strNum1 = strNum1.padStart(strNum2.length, "0");
+  } else if (strNum2.length < strNum1.length) {
+    strNum2 = strNum2.padStart(strNum1.length, "0");
+  }
 
-    if (l1 < l2) {
-        strNum1 = strNum1.padStart(l2,"0"); // Añade ceros al inicio de la cadena strNum1 hasta tener la misma longitud que la cadena strNum2
-    } else if (l2 < l1) {
-        strNum2 = strNum2.padStart(l1,"0"); // Añade ceros al inicio de la cadena strNum2 hasta tener la misma longitud que la cadena strNum1
-    }
+  // Create arrays from both strings
+  strNum1 = strNum1.split("");
+  strNum2 = strNum2.split("");
 
-    strNum1 = strNum1.split(''); // Crea un arreglo donde cada elemento es un dígito de la cadena strNum1
-    strNum2 = strNum2.split(''); // Crea un arreglo donde cada elemento es un dígito de la cadena strNum2
+  const sum = new Array(strNum1.length); // Create array with the same length as both strings
+  let counter = 0; // Counter to keep track of the remainder if the sum of the digits is bigger than 9
+  let digitSum; // Variable to sum the digits of both strings
 
-    suma = new Array(l1); // Crea un arreglo vacio de la misma longitud que los arreglos strNum1 y strNum2
-    contador = 0; // Contador que se va a usar para llevar la cuenta cuando la suma de dos dígitos es mayor a 9
+  for (let i = (strNum1.length - 1); i >= 1; i--) { // Stops at 1, to not mess with the last digit of both strings
+    // Store the sum of the digits of each string plus the counter and turn it to string
+    digitSum = String(Number(strNum1[i]) + Number(strNum2[i]) + counter); 
 
-    for (i = l1-1; i >= 1; i--) {
-        presuma =  Number(strNum1[i]) + Number(strNum2[i]) + contador; // presuma almacena la suma de los digitos de cada cadena más el contador 
-        presuma = String(presuma); // Covierte la variable presuma en tipo cadena 
-
-        if (presuma.length > 1) {  // Si presuma es mayor a 9, almacena el dígito derecho de presuma en el arreglo suma y contador "lleva" 1
-            suma[i] = presuma[1]; 
-            contador = 1;
-        } else {
-            suma [i] = presuma[0]; // Si presuma es menor a 9, almacena el único dígito de presuma en el arreglo suma y contador "lleva" 0
-            contador = 0;
-        }
-    }
-    presuma = Number(strNum1[0]) + Number(strNum2[0]) + contador; // Se almacena la suma de los últimos dos dígitos de cada cadena más el contador 
-    presuma = String(presuma);
-    suma[0] = presuma 
-
-    suma = suma.join('') // Convierte el arreglo suma en un cadena
-    return suma
+    // If the digitSum is bigger than 9 (has more than one digit), assign remainder to counter
+    counter = digitSum.length > 1 ? 1 : 0;
+    sum[i] = digitSum[counter]; // Add the last digit of the digitSum to the sum array
+  }
+  /* Store the sum of the first to digits of both strings plus the counter
+  ** in the first position of the sum array and join the array to get a string.
+  ** Return the final string
+  */
+  sum[0] = String(Number(strNum1[0]) + Number(strNum2[0]) + counter);
+  return sum.join(''); 
 }
 
 module.exports = kSum;
