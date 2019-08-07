5485acecd2dfbb61b333bd5a6b4021c4d45cd7e4
emmanuelr.bts@gmail.com <emmanuel.lv.97@gmail.com>
2019-08-07 14:03:06 -0500

    algunos cambios

--- project2/tres_mosqueteros/fibonacci.js
+++ project2/tres_mosqueteros/fibonacci.js
@@ -1,5 +1,17 @@
+numbers=[0n, 1n];
 function fibonacci (nth) {
-    return total;
+    if(numbers[nth]!=null){
+        return numbers[nth];
+    }else {
+        numbers[nth]=(fibonacci(nth-2)+fibonacci(nth-1));
+        return numbers[nth];
+    }
 }
-
+console.log(fibonacci(6));
+console.log(fibonacci(16));
+console.log(fibonacci(56));
+console.log(fibonacci(96));
+console.log(fibonacci(106));
+console.log(fibonacci(200));
+console.log(fibonacci(2000));
 module.exports=fibonacci;
--- project2/tres_mosqueteros/ksum.js
+++ project2/tres_mosqueteros/ksum.js
@@ -1,6 +1,28 @@
 function kSum (strNum1, strNum2) {
-
-    return strSuma;
+    strNum1 = strNum1.split("").reverse();
+    strNum2 = strNum2.split("").reverse();
+    let mayor, menor;
+    if(strNum1.length>strNum2.length){
+        mayor = strNum1;
+        menor = strNum2;
+    } else {
+        mayor = strNum2;
+        menor = strNum1;
+    }        
+    let suma = new Array(mayor.length+1);
+    suma.fill(0);
+    for (let i = 0; i < menor.length; i++) {
+        suma[i] += parseInt(menor[i]) + parseInt(mayor[i]);
+        if (suma[i]>=10){
+            suma[i+1]+=1;
+            suma[i]=suma[i]%10;
+        }    
+    }
+    for (let i = menor.length; i < mayor.length; i++) {
+        suma[i]+=parseInt(mayor[i]);
+    }
+    if(suma[suma.length-1]===0)suma.splice(suma.length-1,1);
+    return suma.reverse().join("");
 }
 
 module.exports=kSum;
