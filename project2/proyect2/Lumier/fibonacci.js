370d4ec9a93426c0c793249247521d12f0283c6f
Erick Vazquez <erickbardo.bts@gmail.com>
2019-08-07 14:08:41 -0500

    changes done

--- project2/proyect2/Lumier/fibonacci.js
+++ project2/proyect2/Lumier/fibonacci.js
@@ -1,12 +1,21 @@
 function fibonacci(n){
-  if(n === 1){
-    return BigInt(n)
-  }
-  else if(n === 2){
-    return BigInt(fibonacci(n - 1))
-  }
-  else{
-    return BigInt((fibonacci(n-1) + fibonacci(n-2)))
+  let fibos = {}
+  if(fibos[n]){
+    return BigInt(fibos[n])
+  }else{
+
+    if(n === 1){
+       fibos[n] = BigInt(n)
+       return fibos[n]
+    }
+    else if(n === 2){
+      fibos[n] = BigInt(fibonacci(n - 1))
+      return fibos[n]
+    }
+    else{
+      fibos [n] = BigInt((fibonacci(n-1) + fibonacci(n-2)))
+      return fibos[n]
+    }
   }
 };
 
