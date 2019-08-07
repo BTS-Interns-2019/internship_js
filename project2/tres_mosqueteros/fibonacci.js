494542900904816157014b3328747b9f60b381ac
emmanuelr.bts@gmail.com <emmanuel.lv.97@gmail.com>
2019-08-07 16:03:03 -0500

    código comentado de fibonacci y ksum

--- project2/tres_mosqueteros/fibonacci.js
+++ project2/tres_mosqueteros/fibonacci.js
@@ -1,17 +1,15 @@
-numbers=[0n, 1n];
+numbers=[0n, 1n];   //arreglo para gurdar los fibonacci de n numeros
+/**
+ * Recibe un numero y devuelve el numero de la secuencia de fibonacci corerspondiente a esa posición
+ * @param {number} nth //numero del que se decea el fibonacci
+ */
 function fibonacci (nth) {
-    if(numbers[nth]!=null){
+    if(numbers[nth]!=null){ //si ya está calculado lo devuelve
         return numbers[nth];
-    }else {
+    }else {                 //si no, lo calcula a partir de los últimos 2 calculados
         numbers[nth]=(fibonacci(nth-2)+fibonacci(nth-1));
         return numbers[nth];
     }
 }
-console.log(fibonacci(6));
-console.log(fibonacci(16));
-console.log(fibonacci(56));
-console.log(fibonacci(96));
-console.log(fibonacci(106));
-console.log(fibonacci(200));
-console.log(fibonacci(2000));
+
 module.exports=fibonacci;
--- project2/tres_mosqueteros/ksum.js
+++ project2/tres_mosqueteros/ksum.js
@@ -1,16 +1,20 @@
+/**
+ * //Recibe dos strings con dos series de digitos y retorna otro string con la suma de estas series de digitos
+ * @param {string} strNum1 primer cadena de digitos
+ * @param {string} strNum2 segunda cadena de digitos
+ * @returns {string}        suma de las cadenas de digitos
+ */
 function kSum (strNum1, strNum2) {
-    strNum1 = strNum1.split("").reverse();
-    strNum2 = strNum2.split("").reverse();
-    let mayor, menor;
-    if(strNum1.length>strNum2.length){
-        mayor = strNum1;
-        menor = strNum2;
-    } else {
-        mayor = strNum2;
-        menor = strNum1;
-    }        
+    //obtener el string mas corto y el más largo e invertirlos para empezar a
+    //comparar desde los extremos derechos de ambos arreglos
+    const mayorMenor = greater(strNum1, strNum2);    
+    mayor=mayorMenor[0].split("").reverse();
+    menor=mayorMenor[1].split("").reverse();
+    //arreglo para guardar la suma inicializado con numeros "0"s del tamaño del
+    //numero más largo más uno, por si el restulatdo es un digito más grande
     let suma = new Array(mayor.length+1);
     suma.fill(0);
+    //hace la suma hasta el digito "n"th donde "n" es la longitud del numero más corto
     for (let i = 0; i < menor.length; i++) {
         suma[i] += parseInt(menor[i]) + parseInt(mayor[i]);
         if (suma[i]>=10){
@@ -18,11 +22,27 @@ function kSum (strNum1, strNum2) {
             suma[i]=suma[i]%10;
         }    
     }
+    //agrega el resto de digitos del numero más largo a la suma
     for (let i = menor.length; i < mayor.length; i++) {
         suma[i]+=parseInt(mayor[i]);
     }
+    //si el resultado de la suma tiene el mismo número de digitos que numero más grande
+    //se elimina el espacio agregado a la suma
     if(suma[suma.length-1]===0)suma.splice(suma.length-1,1);
-    return suma.reverse().join("");
+    //invierte la suma a su estado original, la une en un string y la retorna
+    return suma.reverse().join(""); 
+}
+
+/**
+ * Recive dos strings y devuelve un arreglo con el de mayor longitud
+ *  en la posicion 0 y y el de menor en la posicion 1
+ * @param {string} a 
+ * @param {string} b 
+ * @return {array}  arreglo con el string con más caracteres en la posición 0 y otro en la 1
+ */
+function greater(a, b){
+    if(a.length>b.length) return[a,b];
+    return [b,a];
 }
 
 module.exports=kSum;
