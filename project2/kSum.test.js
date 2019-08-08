09893d0ff646b51a5b7a4c0ee3a934a4f63cf5e9
CarlosPadilla <carlosp@bluetrailsoft.com>
2019-08-08 09:21:22 -0500

    problems example

--- project2/kSum.test.js
+++ project2/kSum.test.js
@@ -1,13 +1,11 @@
-const kSum = require('./ksum');
+const kSum = require('./kSum');
 
 const output = '131151201344081895336534324866';
 const output2 = '100';
 
 describe('Ksum Tests', () => {
-
-    test('expect HEY JUDE', () => {
-        expect(kSum('131151201344081895330000000000','0006534324866')).toBe(output);
-        expect(kSum('99','1')).toBe(output2);
-    });
-
+  test('expect HEY JUDE', () => {
+    expect(kSum('131151201344081895330000000000', '0006534324866')).toBe(output);
+    expect(kSum('99', '1')).toBe(output2);
+  });
 });
