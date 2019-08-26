9fca0a5ad88d73b867899c148298e9f34a3d5b3c
Marlon Torres <marlont.bts@gmail.com>
2019-08-26 15:55:30 -0500

    prism terminado

--- aug_26/prism_marlont.js
+++ aug_26/prism_marlont.js
@@ -1,20 +1,26 @@
 function prism(long, width, height) {
+  if(typeof long === 'undefined' || typeof width === 'undefined' || typeof height === 'undefined'){
+    this.long = 0;
+    this.width = 0;
+    this.height = 0;
+  } else {
     this.long = long;
     this.width = width;
     this.height = height;
+  }
     
-    this.perimeter = function(){
-      return (this.long + this.width) * 2;
-    }
+  this.perimeter = function(){
+    return (this.long + this.width) * 2;
+  }
   
-    this.area = function(){
-      return this.long * this.width;
-    }
+  this.area = function(){
+    return this.long * this.width;
+  }
   
-    this.volume = function() {
-      return this.long * this.width * this.height;
+  this.volume = function() {
+    return this.long * this.width * this.height;
     }
   
 }
   
-  module.exports = prism;
+module.exports = prism;
--- aug_26/prism_marlont.test.js
+++ aug_26/prism_marlont.test.js
@@ -3,12 +3,46 @@ const prism = require('./prism_marlont');
 test('Normal Input', function(){
     let p = new prism(2,2,2);
     console.log(p);
-    
+
     expect(p.long).toBe(2);
     expect(p.width).toBe(2);
     expect(p.height).toBe(2);
 
-    expect(p.parameter).toBe(8);
-    expect(p.area).toBe(4);
-    expect(p.volume).toBe(8);
+    expect(p.perimeter()).toBe(8);
+    expect(p.area()).toBe(4);
+    expect(p.volume()).toBe(8);
 }) 
+
+test('Modificacion de un valor', function(){
+    let p2 = new prism(2,5,8);
+
+    expect(p2.long).toBe(2);
+    expect(p2.width).toBe(5);
+    expect(p2.height).toBe(8);
+
+    expect(p2.perimeter()).toBe(14);
+    expect(p2.area()).toBe(10);
+    expect(p2.volume()).toBe(80);
+
+    p2.long = 4;
+
+    expect(p2.long).toBe(4);
+    
+    expect(p2.perimeter()).toBe(18);
+    expect(p2.area()).toBe(20);
+    expect(p2.volume()).toBe(160);
+}) 
+
+test('Falta de parametros', function(){
+    let p3 = new prism(2,3);
+
+    expect(p3.long).toBe(0);
+    expect(p3.width).toBe(0);
+    expect(p3.height).toBe(0);
+
+    expect(p3.perimeter()).toBe(0);
+    expect(p3.area()).toBe(0);
+    expect(p3.volume()).toBe(0);
+}) 
+
+
