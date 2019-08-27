1363a324f4650c7cbf06f1f51dbac10a155b6319
Erick Vazquez <erickbardo.bts@gmail.com>
2019-08-27 10:35:29 -0500

    muchos commits, lo siento

--- aug_26/person_erick.test.js
+++ aug_26/person_erick.test.js
@@ -1,6 +1,6 @@
-const person = require ( './person_erick.js' )
+const Person = require ( './person_erick.js' )
 
-let person1 = new person()
+let person1 = new Person()
 
 describe('Passing no values on contructor', () => {
   test('No name, No lastname', () => {
@@ -11,12 +11,17 @@ describe('Passing no values on contructor', () => {
   });
 });
 describe('no initial values, but setting a full name', () => {
-  let person2 = new person()
+  let person2 = new Person()
   person2.fullname = 'Kokoriko Okirokok'
   test('undefined to the new name', () => {
     expect(person2.name).toBe('Kokoriko')
   });
   test('Undefined to new lastname', () => {
     expect(person2.lastname).toBe('Okirokok')
+  });
+  let person3 = new Person('Gallo', 'Loco')
+  person3.fullname = 'Harry Po Po Potter'
+  test('Big last name', () => {
+    expect(person3.lastname).toBe('Po Po Potter')
   });
 });
