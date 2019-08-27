ef675ec987555dba0d1800c91c2f92c9885ebdb9
Erick Vazquez <erickbardo.bts@gmail.com>
2019-08-27 10:27:04 -0500

    test arreglado

--- aug_26/person_erick.test.js
+++ aug_26/person_erick.test.js
@@ -1,19 +1,22 @@
-const person = require ( './person_erick' )
+const person = require ( './person_erick.js' )
+
+let person1 = new person()
 
 describe('Passing no values on contructor', () => {
   test('No name, No lastname', () => {
-    expect(cosa.name).toBe(undefined)
+    expect(person1.name).toBe(undefined)
   });
   test('No name, No lastname', () => {
-    expect(cosa.lastname).toBe(undefined)
+    expect(person1.lastname).toBe(undefined)
   });
 });
 describe('no initial values, but setting a full name', () => {
-  cosa.fullname = 'Kokoriko Okirokok'
+  let person2 = new person()
+  person2.fullname = 'Kokoriko Okirokok'
   test('undefined to the new name', () => {
-    expect(cosa.name).toBe('Kokoriko')
+    expect(person2.name).toBe('Kokoriko')
   });
   test('Undefined to new lastname', () => {
-    expect(cosa.lastname).toBe('Okirokok')
+    expect(person2.lastname).toBe('Okirokok')
   });
 });
