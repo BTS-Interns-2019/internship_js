2f26068eb5750903b4b9bc32b6f538214fe15790
Erick Vazquez <erickbardo.bts@gmail.com>
2019-08-27 10:30:37 -0500

    script mejorado

--- aug_26/person_erick.js
+++ aug_26/person_erick.js
@@ -1,31 +1,24 @@
-function person(name, lastname){ 
+function Person(name, lastname){ 
   this.name = name,
   this.lastname = lastname,
 
   Object.defineProperty(this, 'fullname', {
     'get': function(){
       if(!this.name || !this.lastname) return undefined
-      return name + ' ' + lastname
+      return name + ' ' + lastname;
     },
     'set': function(string){
-      this.name = string.split(' ')[0]
-      this.lastname = string.split(' ')[1]
+      this.name = string.split(' ')[0];
+      this.lastname = string.split(' ')
+                            .slice(1)
+                            .join(' ');
     },
   })
 };
-let persona = new person()
-persona.fullname = 'Kokoriko okirokok'
-console.log(persona)
 
-module.exports = person;
+
+module.exports = Person;
 
   
 
-// let erick = new person('Erick', 'Vazquez')
-// erick.name = 'Israel'
-// console.log(erick)
-// erick.fullname = 'Israel Neri'
-// // erick.lastname = 'Neri'
-// console.log(erick.name)
-// console.log(erick.lastname)
 
