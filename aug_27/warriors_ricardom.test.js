cf66e5f45ab2bedeeda20b1120291f6f29334c04
Ricardo Martin del Campo Loza <ricardom.bts@gmail.com>
2019-08-28 02:06:29 -0500

    subo warriors v1 (con errores)

---
+++ aug_27/warriors_ricardom.test.js
@@ -0,0 +1,16 @@
+const {Ninja, Samurai} = require('./warriors_ricardom');
+
+const ninjaGaiden = new Ninja();
+test('values asigned Ninja',()=>{
+    expect(ninjaGaiden.healthPoints).toBe(100);
+    expect(ninjaGaiden.attackPoints).toBe(20);
+    expect(ninjaGaiden.isDead).toBe(false);
+});
+
+const samTanjiro = new Samurai();
+test('values asigned Samurai',()=>{
+    expect(samTanjiro.healthPoints).toBe(100);
+    expect(samTanjiro.attackPoints).toBe(10);
+    expect(samTanjiro.isDead).toBe(false);
+});
+
