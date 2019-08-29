7f513aee62247207cc93cdf91967406f1b77493b
nestorRabbin <rabbin_cbta@hotmail.com>
2019-08-29 09:38:44 -0500

    unfinish geometry

---
+++ aug_28/geometry_nestorl.js
@@ -0,0 +1,43 @@
+function Figure() {
+    this.base = 0,
+    this.height = 0,
+    this.radius = 0,
+    this.vheight = 0,
+    this.descriptionObj = {
+        type: "",
+    };
+    this.mesure = {
+        base: this.base,
+        height: this.height,
+        radius: this.radius,
+        vheight: this.vheight,
+    }
+}
+
+function Rectangle(b,h){
+    this.type = "Rectangle"
+    this.base = b;
+    this.height = h;
+    this.perimeter = 2 * (b + h);
+    this.area = b * h;
+}
+
+function Square(l){
+    this.type = "Square"
+    this.base = l;
+    this.height = l;
+    this.perimeter = l * 4;
+    this.area = l * l;
+}
+
+function Triangle(b,h){
+    this.type = "Triangle"
+    this.base = b;
+    this.height = h;
+    hp = Math.sqrt(  );
+    this.perimeter = (hp * 2) + b;
+    this.area = b * h /2 ;
+}
+
+let triangulo = new Triangle(10,42);
+console.log(triangulo);
