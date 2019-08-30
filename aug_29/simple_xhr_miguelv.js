5402f31a0aa500801df0e1812c47709b85c416e9
Miguel <miguelv.bts@gmail.com>
2019-08-30 13:44:30 -0500

    Unfinished promises

--- aug_29/simple_xhr_miguelv.js
+++ aug_29/simple_xhr_miguelv.js
@@ -23,17 +23,45 @@ function get(url, onsuccess, onerror){
     }
 }
 get("https://reqres.in/api/users/2",
-function(responseText){
-    console.log("respuesta: ",responseText)
-},
-function(status){
-    console.log("error: ",status)
-})
+    function(responseText){
+        console.log("respuesta1: ",responseText);
+    },
+    function(status){
+        console.log("error1: ",status);
+    }
+)
+
+
 function post(url,dataString,onsuccess,onerror){
     let http=new XMLHttpRequest();
     http.open("POST",url);
+    http.setRequestHeader("Content-type","aplication/json");
+    http.send(dataString);
+    http.onload=()=>{
+        if(http.status<400){
+            console.log("success");
+            onsuccess(http.responseText);
+        }else{
+            console.log("fail");
+            onerror(http.status)
+        }
+    }
+    http.onerror=(e)=>{
+        console.log("Error: ",e)
+    }
 }
-
+post("https://reqres.in/api/users/2",
+    '{"myData": "My data."}',
+    function(responseText){
+        console.log("respuesta2: ",responseText);
+    },
+    function(status){
+        console.log("error2: ",status);
+    })
 function request(method,url,dataString,onsuccess,onerror){
-    let http=new XMLHttpRequest();
+    if(method==="GET"){
+        get(url,onsuccess,onerror);
+    }else{
+        post(url,dataString,onsuccess,onerror);
+    }
 }
