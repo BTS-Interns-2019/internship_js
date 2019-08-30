32e4dec60c96e9a541d8d3e22b5539ca7b3eb13c
Miguel <miguelv.bts@gmail.com>
2019-08-30 09:56:43 -0500

    Unfinished functions post and request

--- aug_29/simple_xhr_miguelv.js
+++ aug_29/simple_xhr_miguelv.js
@@ -4,3 +4,34 @@ request.open('GET','https://reqres.in/api/users/2',false);
 request.send();
 //return request.responseText;
 console.log(request.responseText);
+
+function get(url, onsuccess, onerror){
+    let http=new XMLHttpRequest();
+    http.open("GET",url);
+    console.log(http.send());
+    http.onload=()=>{
+        if(http.status<400){
+            console.log("success");
+            onsuccess(http.responseText)
+        }else{
+            console.log("fail");
+            onerror(http.status)
+        }
+    }
+    http.onerror=(e)=>{
+        console.log("Error: ",e)
+    }
+}
+get("https://reqres.in/api/users/2",
+function(responseText){
+    console.log("respuesta: ",responseText)
+},
+function(status){
+    console.log("error: ",status)
+})
+function post(url,dataString,onsuccess,onerror){
+
+}
+function request(method,url,dataString,onsuccess,onerror){
+    
+}
