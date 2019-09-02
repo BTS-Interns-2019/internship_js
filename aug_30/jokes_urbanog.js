c7de2b3fa27fddfac5e756ac8730cc2cc5dd6114
Urbano <urbano0525@gmail.com>
2019-09-01 23:33:44 -0500

    jokes homework

---
+++ aug_30/jokes_urbanog.js
@@ -0,0 +1,59 @@
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function giveAJoke(apiUrl, category) {
+  if (category === '' || category === ' ') {
+    category = 'any'
+  }
+  return new Promise((resolve, reject) => {
+    request('GET', apiUrl +'/'+ category, resolve, reject);
+  });
+}
+
+//https://api.chucknorris.io/jokes/random
+//https://sv443.net/jokeapi/category/miscellaneous?type=twopart
+///category/miscellaneous?type=twopart
+giveAJoke('http://api.icndb.com/jokes', 'random')
+  .then(function (val) {
+      console.log(val.hasOwnProperty('saySetup'));
+      console.log(val.saySetup())
+      console.log(val.sayPunchLine())
+    }
+  ).catch(function (val) {
+  console.log(val);
+});
+
+
+function request(method, url, callback, error, body) {
+  if (typeof method !== 'string') {
+    return 'Method parameter, must be a String';
+  }
+  let meth = method.toUpperCase();
+  const postBody = JSON.stringify(body);
+  const request = new XMLHttpRequest();
+  request.open(meth, url, true);
+  request.onload = function () {
+    if (this.status >= 200 && this.status <= 299) {
+
+      console.log((this.status))
+      if (this.responseText.includes('twopart')) {
+        const result = JSON.parse(this.responseText);
+        result.saySetup = function () {
+          return this.setup;
+        }
+        result.sayPunchLine = function () {
+          return this.delivery;
+        }
+        callback(result);
+      } else {
+
+        error(`new Error (No jokes at url: ${url}`);
+      }
+
+    } else {
+      error(`new Error (No jokes at url: ${url}`);
+    }
+  };
+  request.send(postBody);
+}
+
+module.exports = {giveAJoke}
---
+++ aug_30/jokes_urbanog.test.js
@@ -0,0 +1,29 @@
+const {giveAJoke} = require('./jokes_urbanog');
+describe('Test for asyncronus function', () => {
+  test('Data is an object', done => {
+    function callback(data) {
+      expect(typeof data).toBe('object');
+      done();
+    }
+    giveAJoke('https://sv443.net/jokeapi/category', 'dark').then(callback(this))
+  });
+
+  test('Has a mothod called sayPunch line',  () => {
+    return giveAJoke('https://sv443.net/jokeapi/category', 'dark').then( function(data){
+      expect(data.hasOwnProperty('saySetup')).toBe(true);
+      expect(typeof data.saySetup()).toBe('string');
+    });
+  });
+  test('Punch Line return delivery',  () => {
+    return giveAJoke('https://sv443.net/jokeapi/category', 'dark').then(function (data) {
+      expect(data.delivery).toBe(data.sayPunchLine());
+    })
+  });
+  test('Is a promise', async () => {
+    const data = await giveAJoke('https://sv443.net/jokeapi/category', 'dark');
+    expect(typeof data).toBe('object');
+  });
+  test('Reject function', async () => {
+    await expect(giveAJoke('http://api.icndb.com/jokes','random')).rejects.toMatch('new Error (No jokes at url: http://api.icndb.com/jokes/random')
+  });
+});
