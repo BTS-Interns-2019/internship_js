fd912e265cebd458c0648ab6b843626dacab43d5
unknown <cristianliljon@hotmail.com>
2019-09-02 10:08:57 -0500

    jokes cristian aun con dudas

---
+++ aug_30/jokes_cristianp.js
@@ -0,0 +1,68 @@
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+//const url = 'https://sv443.net/jokeapi/category/miscellaneous?type=twopart';
+
+function giveAJoke(apiUrl, category) {
+  if (category === '' || category === ' ') {
+    category = 'Miscellaneous';
+  }
+  return new Promise((resolve, reject) => {
+    request('GET', apiUrl + '/' + category, resolve, reject);
+  });
+}
+
+function request(method, url, callback, error, body) {
+  if (typeof method !== 'string') {
+    return 'Need a string method';
+  }
+  const http = new XMLHttpRequest();
+  const pBody = JSON.stringify(body);
+  http.open(method, url, true);
+  http.onload = function() {
+    if (this.status >= 200 && this.status <= 299) {
+      console.log(this.status);
+      if (this.responseText.includes('twopart')) {
+        const result = JSON.parse(this.responseText);
+        result.saySetup = function() {
+          return this.setup;
+        };
+        result.sayPunchLine = function() {
+          return this.delivery;
+        };
+        callback(result);
+      } else {
+        error(`new Error (No jokes at url: ${url}`);
+      }
+    } else {
+      error(`new Error (No jokes at url: ${url}`);
+    }
+  };
+  http.send(pBody);
+  /*return new Promise((resolve, reject)=>{
+        const http = new XMLHttpRequest();
+        const pBody = JSON.stringify(body);
+        http.open(method, url,true);
+
+        http.onload = ()=>{
+            if(http.status >= 400){
+                error.send(pBody);
+            }
+            resolve(http.responseText);
+        }
+    });*/
+}
+giveAJoke('http://api.icndb.com/jokes', 'Miscellaneous')
+  .then(function(val) {
+    console.log(val.hasOwnProperty('saySetup'));
+    console.log(val.saySetup());
+    console.log(val.sayPunchLine());
+  })
+  .catch(function(val) {
+    console.log(val);
+  });
+
+/*function get(url){
+    return request("GET", url);
+}
+function post(url, data){
+    return request("POST", url, data);
+}*/
