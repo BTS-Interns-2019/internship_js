8216606c16dfd590e45fb436026d806b0457fb5c
rabbin <rabbin_cbta@hotmail.com>
2019-09-03 14:09:14 -0500

    unfinish more async

---
+++ sep_3/function.js
@@ -0,0 +1,37 @@
+var XMLHttpRequest = require('xmlhttpRequest').XMLHttpRequest;
+
+function get(url,cb,error){
+    const http = new XMLHttpRequest();
+    http.open("GET", url, true)
+    http.onreadystatechange = () =>{
+        if(http.readyState == 4){
+            if(http.status == 200){
+                const res = JSON.parse(http.responseText)
+                let wiki = res.data.results[0].urls[1].url;
+                let arr = wiki.split("?");           
+                let hero = {};
+                hero.wiki = arr[0];
+                hero.description = res.data.results[0].description;
+            return cb(hero);
+            } 
+            return error(`Error ${http.status} ${url}`);
+        }
+    };
+    http.send();
+}
+function post(url, body, cb, error){
+    const http = new XMLHttpRequest();
+    http.open("POST", url, true)
+    http.onreadystatechange = () =>{
+        if(http.readyState == 4){
+            if(http.status == 200){
+                var resultado = JSON.parse(http.responseText);
+                cb(http.responseText);
+            }else{
+                error(`Error ${http.status} ${url}`)
+            }
+        }
+    };
+    http.send(body);
+  }
+  module.exports = {get, post}
--- sep_3/more_async_await.test.js
+++ sep_3/more_async_await.test.js
@@ -1,5 +1,18 @@
-// const { ... } = require('path to file');
-
+const { get, post } = require('./function.js');
+// const publiKey = '9b04d37e84bdf94e5610a5caa34dd978';
+// const privateKey = '4f6177742eead504e4b74f182f8cee38d9a2ba5c';
+// async function findHero(hero){
+//   const hash =  'C2EA1985191BF9EBC56E3DBF95619284'
+//   const url = `https://gateway.marvel.com:443/v1/public/characters?name=${hero}&ts=1&apikey=9b04d37e84bdf94e5610a5caa34dd978&hash=${hash}`;
+//   get(url)
+// }
+function findHero(hero) {
+  const hash =  'c2ea1985191bf9ebc56e3dbf95619284'
+  const url = `https://gateway.marvel.com:443/v1/public/characters?name=${hero}&ts=1&apikey=9b04d37e84bdf94e5610a5caa34dd978&hash=${hash}`;
+  return new Promise((resolve, reject) => {
+  get(url, resolve, reject);
+  });  
+}
 
 describe('Find Super heroes', () => {
 
@@ -8,6 +21,7 @@ describe('Find Super heroes', () => {
     const spiderman = await findHero('Spider-man');
     // original -> "http://marvel.com/universe/Spider-Man_(Peter_Parker)?utm_campaign=apiRef&utm_source=a736913c879d22d50f62b4f286ca23bd"
     // remove traking keys from url
+
     expect(spiderman.wiki).toBe("http://marvel.com/universe/Spider-Man_(Peter_Parker)");
   });
 
