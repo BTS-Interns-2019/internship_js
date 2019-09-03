e0bcbae968bcad69721ba1028ec0298c396e2856
Miguel <miguelv.bts@gmail.com>
2019-09-03 18:52:36 -0500

    More changes

--- sep_3/more_async_await_miguelv.js
+++ sep_3/more_async_await_miguelv.js
@@ -26,10 +26,14 @@ function get(url){
                 console.log("status = "+http.status+"-->"+typeof http.status);
                 if(http.status<400){
                     console.log("success");
-                    console.log(typeof http.responseText);
-                    console.log(http.responseText);
-                    console.log("-"+JSON.parse(http.responseText).urls)
-                    resolve(http.responseText);
+                    //console.log(typeof http.responseText);
+                    //console.log(http.responseText);
+                    console.log("-"+JSON.parse(http.responseText).data.results[0].urls);
+                    console.log("--"+JSON.stringify(
+                        JSON.parse(http.responseText).data.results[0].urls.filter((c)=>{
+                            return c.type === "wiki";
+                        })));
+                    resolve(JSON.stringify(JSON.parse(http.responseText).etag));
                 }else{
                     console.log("fail");
                     reject(http.responseText);
@@ -43,15 +47,29 @@ function get(url){
         console.log(e);
     }
 }
-async function neo(){
+async function bwi(){
     let p = "3d439829a5351f94e9224b2c8a1f58126278381b";
     let p2 = "3ee825f31bd11f3d60b465f450f6eae6";
     let ts = Date.now();//ts es dinamico, numero de milisegundos obtenidos por date
     let link = "https://gateway.marvel.com:443/v1/public/characters?name=Black-Widow&ts="+ts+"&apikey=3ee825f31bd11f3d60b465f450f6eae6";
     let linkp = "&hash="+md5(ts+p+p2);
-    let a = await get(link+linkp);
-    return a;
+    l=link+linkp;
+    let a = await get(l).then((tag)=>{
+        console.log("..."+tag);
+    });
 }
-console.log(neo());
+async function thor(){
+    let p = "3d439829a5351f94e9224b2c8a1f58126278381b";
+    let p2 = "3ee825f31bd11f3d60b465f450f6eae6";
+    let ts = Date.now();
+    let link = "https://gateway.marvel.com:443/v1/public/characters?name=Thor&ts="+ts+"&apikey=3ee825f31bd11f3d60b465f450f6eae6";
+    let linkp = "&hash="+md5(ts+p+p2);
+    l=link+linkp;
+    let b = await get(l).then((tag)=>{
+        console.log("..."+tag);
+    });
+}
+//console.log(bwi());
+console.log(thor());
 
 //marvel.render();
