6801d5a031cf097ed1c5a71db16c816deb697c00
Miguel <miguelv.bts@gmail.com>
2019-09-05 01:00:08 -0500

    19 new events done

--- sep_4/events_miguelv.js
+++ sep_4/events_miguelv.js
@@ -9,6 +9,8 @@
     const lista = document.getElementById("lista");
     const screen = document.getElementById("screen");
     const inpt = document.getElementById("inpt");
+    const img = document.getElementById("img");
+    const custom1 = document.getElementById("custom1");
     function sendToDiv(ev){
         console.log('sendToDiv',ev);
         div.innerText=ev.target.value;
@@ -42,15 +44,18 @@
         console.log("Recibi un chingadazo", ev);
         div.style.background=ev.newColor;
     })
+    /* First 3 events */
     text.addEventListener('mousedown',()=>{
         text.style.fontWeight="bold";
     })
     text.addEventListener('mouseup',()=>{
         text.style.fontStyle="italic";
+        text.style.fontWeight="normal";
     })
     text.addEventListener('dblclick',()=>{
         text.style.textDecoration="line-through";
     })
+    /* Second 3 events */
     lista.addEventListener('click',()=>{
         lista.style.border="1px solid blue";
     })
@@ -62,6 +67,7 @@
         lista.classList.remove('inlineitem');
         lista.classList.add('blockitem');
     })
+    /* Third 3 events */
     screen.addEventListener("mousemove",(ev)=>{
         screen.classList.remove('hidden');
         screen.innerText=ev.screenX+","+ev.screenY;
@@ -72,6 +78,7 @@
     screen.addEventListener("click",(ev)=>{
         inpt.value=ev.screenX+","+ev.screenY;
     })
+    /* Fouth 3 events */
     inpt.addEventListener("focus",()=>{
         inpt.style.background="white";
         inpt.style.color="black";
@@ -82,5 +89,49 @@
     })
     inpt.addEventListener("paste",()=>{
         inpt.style.background="yellow";
+    })
+    /* Fifth 3 events */
+    img.addEventListener("mouseover",()=>{
+        img.style.opacity = "0.2";
+    })
+    img.addEventListener("mouseleave",()=>{
+        img.style.opacity = "1";
+        img.style.borderRadius="50%";
+    })
+    img.addEventListener("mousedown",()=>{
+        img.style.filter="blur(4px)";
+    })
+    /* Custom events */
+    text.addEventListener('engrandeceme',()=>{
+        text.style.fontSize="5em";
+    })
+    lista.addEventListener('subrayar-lista',()=>{
+        lista.classList.add('underlined')
+    })
+    inpt.addEventListener('nueva-fuente',(ev)=>{
+        inpt.style.fontFamily=ev.newFont;
+    })
+    screen.addEventListener('sombra',(color)=>{
+        alert("SOMBRA!!!");
+        screen.style.boxShadow="10px 10px "+color.newColor;
+    })
+    /* Custom handlers */
+    custom1.addEventListener("click",()=>{
+        const evento = new CustomEvent('engrandeceme');
+        text.dispatchEvent(evento);
+    })
+    lista.addEventListener('dblclick',()=>{
+        const evento = new CustomEvent('subrayar-lista');
+        lista.dispatchEvent(evento);
+    })
+    inpt.addEventListener("dblclick",()=>{
+        const evento = new CustomEvent('nueva-fuente');
+        evento.newFont="sans";
+        inpt.dispatchEvent(evento);
+    })
+    screen.addEventListener("dblclick",()=>{
+        const evento = new CustomEvent('sombra');
+        evento.newColor="gray";
+        screen.dispatchEvent(evento);
     })
 })();
