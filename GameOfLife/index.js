0b6828f31324c41f2121b1b2abdc1b384654ff9f
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-09-11 14:00:07 -0500

    Third try

--- GameOfLife/index.js
+++ GameOfLife/index.js
@@ -5,14 +5,37 @@ function Canvas(canvas, width, height, resolution) {
   this.resolution = resolution;
   this.canvas.width = this.width;
   this.canvas.height = this.height;
-  this.cols = this.width / this.resolution ;
-  this.rows = this.height / this.resolution;
   this.context = this.canvas.getContext('2d');
-  this.canvasGrid = new Array(this.cols).fill(null)
-    .map(() => new Array(this.rows).fill(null).map(() => 
-      Math.floor(Math.random() * 2)));
 }
+const btnClear = document.getElementById('clear');
+const canvas = document.querySelector('canvas');
+const world = new Canvas(canvas, 500, 400, 10);
+
+const cols = world.width / world.resolution;
+const rows = world.height / world.resolution;
+
+//Create the grid
+function canvasGrid (){
+  return  new Array(cols).fill(null)
+  .map(() => new Array(rows).fill(null));
+}
+//Fill the grid whit random cell
+function randPopulation() {
+  const blank = canvasGrid();
+  for (let col = 0; col < blank.length; col++) {
+    for (let row = 0; row < blank[col].length; row++) {
+      blank[col][row] = Math.floor(Math.random()*2)
+    }
+    
+  }
+
+  return blank;
+}
+
+//Paint the canvas
 function render(context, grid, resolution) {
+// console.log(grid);
+
   for (let col = 0; col < grid.length; col += 1) {
     for (let row = 0; row < grid[col].length; row += 1) {
       const cell = grid[col][row];
@@ -23,51 +46,75 @@ function render(context, grid, resolution) {
       context.stroke();
     }
   }
-};
+}
 
-update = function (grid) {
-  this.render();
-  window.setInterval(this.update, 300)
- }
-
-function newGen(grid, cols , rows) {
-  const newGen = grid.map( (arr) => [...arr]);
+//Creates a copy of the previous gen, and return a new Gen with the changes
+function newGen(grid) {
+  
+  const newGen = Array.of(grid);
   for (let col = 0; col < grid.length; col += 1) {
-    for (let row = 0; row < grid[col].length; row +=1) {
+    for (let row = 0; row < grid[col].length; row += 1) {
       const cell = [col][row];
-      let numNeigh =0;
+      let numNeigh = 0;
       for (let i = -1; i < 2; i += 1) {
         for (let j = -1; j < 2; j += 1) {
-          cornerX = col+i;
-          cornerY = col + j;         
-          if (cornerX >= 0 && cornerY >=0 && cornerX < rows && cornerY <cols) {
-            const currentNeigh = grid[col+ i][row+ j];
+          if (j === 0 && i === 0) {
+            continue;
+          }
+          cornerX = col + i;
+          cornerY = row + j;
+          if (cornerX >= 0 && cornerY >= 0 && cornerX < rows && cornerY < cols) {
+            const currentNeigh = grid[col + i][row + j];
             numNeigh += currentNeigh;
           }
-        }  
+        }
       }
-      //The rulez
-      if (cell === 1 && numNeigh <2) {
+      if (cell === 1 && numNeigh < 2) {
         newGen[col][row] === 0;
-      } else if (cell === 1 && numNeigh >3) {
+      } else if (cell === 1 && numNeigh > 3) {
         newGen[col][row] === 0;
-      }else if (cell === 0 && numNeigh === 3) {
+      } else if (cell === 0 && numNeigh === 3) {
         newGen[col][row] === 1;
       }
     }
   }
-return newGen;
+  // render(world.context, newGen, world.resolution);
+  // console.log(newGen);
+  
+  return newGen;
 }
-const canvas = document.querySelector('canvas');
-const world = new Canvas(canvas, 500, 400, 10);
-render(world.context,world.canvasGrid, world.resolution);
-update();
+let pop = randPopulation();
+console.log(pop);
+
+render(world.context, pop, world.resolution);
+// newGen(pop);
+refresh();
+function refresh() {  
+grid = newGen(pop);
+  render(world.context, grid, world.resolution);
+  setTimeout(refresh, 300);
+}
+// setTimeout(refresh, 300);
 // world.update(world.canvasGrid);
 // window.setInterval(world.update(world.canvasGrid), 300);
 // randomCells(world);
 
-function Cell(size, posx, posY) {
-  this.size = size;
-  this.posY = posY;
-  this.posx = posx;
+function clean(grid) {
+  for (let col = 0; col < grid.length; col++) {
+    for (let row = 0; row < grid[col].length; row++) {
+      grid= [col][row]= 0;
+    }
+    
+  }
 }
+
+function getPos(canvas) {
+  // let posCanvas = 
+
+}
+
+btnClear.addEventListener(('mousedown'), (ev)=> {
+  clean(newGen());
+});
+
+
